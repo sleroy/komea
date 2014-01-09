934ade25c69771f5ac9cb403d364ccb56eb3d87e
jguidoux <jeremie.guidoux@tocea.com>
2014-01-09 16:02:52 +0100

    # [MODEL] fix naming class criteria proble

--- komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/ProjectDAOTest.java
+++ komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/ProjectDAOTest.java
@@ -1,18 +1,14 @@
 package org.komea.product.database.dao;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.komea.product.database.model.Project;
-import org.komea.product.database.model.ProjectExample;
+import org.komea.product.database.model.ProjectCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.espertech.esper.client.annotation.Audit;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,7 +25,7 @@ public class ProjectDAOTest {
 	public void test() {
 		Project project = new Project();
 		project.setName("projet1");
-		ProjectExample example = new ProjectExample();
+		ProjectCriteria example = new ProjectCriteria();
 		example.createCriteria().andNameEqualTo("projet1");
 		Assert.assertTrue(projectDAO.selectByExample(example).isEmpty());
 		
