aa21c507468bcfc9489c5e14d0bc54a94a904a46
jguidoux <jeremie.guidoux@tocea.com>
2014-01-10 14:56:27 +0100

    * reseolved merge conflicts

--- komea-product/komea-database-model/src/main/resources/data/komea-h2-test.sql
+++ komea-product/komea-database-model/src/main/resources/data/komea-h2-test.sql
@@ -1,6 +1,5 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET MODE MySQL;
+
 
 
 -- -----------------------------------------------------
@@ -10,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `KOM_CUSTOMER` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -20,11 +19,15 @@ CREATE TABLE IF NOT EXISTS `KOM_PROJ` (
   `id` INT NOT NULL,
   `key` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(2048) NULL,
-  `idCustomer` INT NULL,
+  `description` TEXT NULL,
+  `idCustomer` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_Project_Customer1`
+    FOREIGN KEY (`idCustomer`)
+    REFERENCES `KOM_CUSTOMER` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -34,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `KOM_GRK` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -44,12 +47,21 @@ CREATE TABLE IF NOT EXISTS `KOM_PEGR` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `key` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(2048) NULL,
+  `description` TEXT NULL,
   `idPersonGroupParent` INT NULL,
-  `idGroupKind` INT NULL,
+  `idGroupKind` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_UserGroup_UserGroup1`
+    FOREIGN KEY (`idPersonGroupParent`)
+    REFERENCES `KOM_PEGR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Person_Group_Group_Kind1`
+    FOREIGN KEY (`idGroupKind`)
+    REFERENCES `KOM_GRK` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -58,9 +70,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_PERO` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`))
+;
 
 
 -- -----------------------------------------------------
@@ -69,14 +80,23 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_PE` (
   `id` INT NOT NULL,
   `idPersonGroup` INT NULL,
-  `idPersonRole` INT NULL,
+  `idPersonRole` INT NOT NULL,
   `firstName` VARCHAR(255) NOT NULL,
   `lastName` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `login` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `Personcol_UNIQUE` (`login` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_User_UserGroup1`
+    FOREIGN KEY (`idPersonGroup`)
+    REFERENCES `KOM_PEGR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_User_UserRole1`
+    FOREIGN KEY (`idPersonRole`)
+    REFERENCES `KOM_PERO` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -88,9 +108,8 @@ CREATE TABLE IF NOT EXISTS `KOM_PVD` (
   `name` VARCHAR(255) NOT NULL,
   `icon` VARCHAR(255) NOT NULL,
   `url` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`))
+;
 
 
 -- -----------------------------------------------------
@@ -100,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `KOM_KPI` (
   `id` INT NOT NULL,
   `key` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(2048) NULL,
+  `description` TEXT NULL,
   `idProvider` INT NOT NULL,
   `minValue` DOUBLE NULL,
   `maxValue` DOUBLE NULL,
@@ -109,8 +128,12 @@ CREATE TABLE IF NOT EXISTS `KOM_KPI` (
   `entityType` INT NOT NULL,
   `esperRequest` TEXT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_Metric_Plugin1`
+    FOREIGN KEY (`idProvider`)
+    REFERENCES `KOM_PVD` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -118,14 +141,34 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `KOM_MSR` (
   `id` INT NOT NULL,
-  `idKpi` INT NOT NULL,
+  `idMetric` INT NOT NULL,
   `date` DATE NOT NULL,
   `idPersonGroup` INT NULL,
   `idPerson` INT NULL,
   `idProject` INT NULL,
   `value` DOUBLE NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Measure_Metric1`
+    FOREIGN KEY (`idMetric`)
+    REFERENCES `KOM_KPI` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Measure_UserGroup1`
+    FOREIGN KEY (`idPersonGroup`)
+    REFERENCES `KOM_PEGR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Measure_User1`
+    FOREIGN KEY (`idPerson`)
+    REFERENCES `KOM_PE` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Measure_Project1`
+    FOREIGN KEY (`idProject`)
+    REFERENCES `KOM_PROJ` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -136,15 +179,19 @@ CREATE TABLE IF NOT EXISTS `KOM_KPIA` (
   `idKpi` INT NOT NULL,
   `key` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(2048) NULL,
+  `description` TEXT NULL,
   `severity` INT NOT NULL,
   `value` DOUBLE NOT NULL,
   `averageSince` DATE NULL,
   `operator` INT NOT NULL,
   `enabled` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_MetricAlert_Metric1`
+    FOREIGN KEY (`idKpi`)
+    REFERENCES `KOM_KPI` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -155,8 +202,13 @@ CREATE TABLE IF NOT EXISTS `KOM_LINK` (
   `name` VARCHAR(255) NOT NULL,
   `url` VARCHAR(255) NOT NULL,
   `idProject` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Tag_Project`
+    FOREIGN KEY (`idProject`)
+    REFERENCES `KOM_PROJ` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -169,12 +221,16 @@ CREATE TABLE IF NOT EXISTS `KOM_EVT` (
   `name` VARCHAR(255) NOT NULL,
   `severity` INT NOT NULL,
   `enabled` TINYINT(1) NOT NULL,
-  `description` VARCHAR(2048) NULL,
+  `description` TEXT NULL,
   `category` VARCHAR(255) NOT NULL,
   `entityType` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_Event_Plugin1`
+    FOREIGN KEY (`idProvider`)
+    REFERENCES `KOM_PVD` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -184,9 +240,8 @@ CREATE TABLE IF NOT EXISTS `KOM_SETTING` (
   `id` INT NOT NULL,
   `key` VARCHAR(255) NOT NULL,
   `value` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`))
+;
 
 
 -- -----------------------------------------------------
@@ -196,7 +251,7 @@ CREATE TABLE IF NOT EXISTS `KOM_TAG` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -205,8 +260,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_HAS_PROJ_PE` (
   `idProject` INT NOT NULL,
   `idPerson` INT NOT NULL,
-  PRIMARY KEY (`idProject`, `idPerson`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`idProject`, `idPerson`),
+  CONSTRAINT `fk_Project_has_User_Project1`
+    FOREIGN KEY (`idProject`)
+    REFERENCES `KOM_PROJ` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Project_has_User_User1`
+    FOREIGN KEY (`idPerson`)
+    REFERENCES `KOM_PE` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -217,9 +282,12 @@ CREATE TABLE IF NOT EXISTS `KOM_PVDS` (
   `key` VARCHAR(255) NOT NULL,
   `value` VARCHAR(255) NOT NULL,
   `idProvider` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC))
-ENGINE = InnoDB;
+  CONSTRAINT `fk_ProviderSetting_Provider1`
+    FOREIGN KEY (`idProvider`)
+    REFERENCES `KOM_PVD` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -228,8 +296,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_HAS_PROJ_TAG` (
   `idProject` INT NOT NULL,
   `idTag` INT NOT NULL,
-  PRIMARY KEY (`idProject`, `idTag`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`idProject`, `idTag`),
+  CONSTRAINT `fk_Project_has_Tag_Project1`
+    FOREIGN KEY (`idProject`)
+    REFERENCES `KOM_PROJ` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Project_has_Tag_Tag1`
+    FOREIGN KEY (`idTag`)
+    REFERENCES `KOM_TAG` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -238,8 +316,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_HAS_PROJ_KPIA` (
   `idProject` INT NOT NULL,
   `idKpiAlertType` INT NOT NULL,
-  PRIMARY KEY (`idProject`, `idKpiAlertType`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`idProject`, `idKpiAlertType`),
+  CONSTRAINT `fk_KOM_PROJ_has_KOM_KPIA_KOM_PROJ1`
+    FOREIGN KEY (`idProject`)
+    REFERENCES `KOM_PROJ` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_KOM_PROJ_has_KOM_KPIA_KOM_KPIA1`
+    FOREIGN KEY (`idKpiAlertType`)
+    REFERENCES `KOM_KPIA` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -248,8 +336,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_HAS_KPIA_PEGR` (
   `idKpiAlertType` INT NOT NULL,
   `idPersonGroup` INT NOT NULL,
-  PRIMARY KEY (`idKpiAlertType`, `idPersonGroup`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`idKpiAlertType`, `idPersonGroup`),
+  CONSTRAINT `fk_KOM_KPIA_has_KOM_PEGR_KOM_KPIA1`
+    FOREIGN KEY (`idKpiAlertType`)
+    REFERENCES `KOM_KPIA` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_KOM_KPIA_has_KOM_PEGR_KOM_PEGR1`
+    FOREIGN KEY (`idPersonGroup`)
+    REFERENCES `KOM_PEGR` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
@@ -258,10 +356,17 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `KOM_HAS_KPIA_PE` (
   `idKpiAlertType` INT NOT NULL,
   `idPerson` INT NOT NULL,
-  PRIMARY KEY (`idKpiAlertType`, `idPerson`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`idKpiAlertType`, `idPerson`),
+  CONSTRAINT `fk_KOM_KPIA_has_KOM_PE_KOM_KPIA1`
+    FOREIGN KEY (`idKpiAlertType`)
+    REFERENCES `KOM_KPIA` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_KOM_KPIA_has_KOM_PE_KOM_PE1`
+    FOREIGN KEY (`idPerson`)
+    REFERENCES `KOM_PE` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/ProjectDAOTest.java
+++ komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/ProjectDAOTest.java
@@ -1,15 +1,19 @@
 package org.komea.product.database.dao;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.komea.product.database.model.Customer;
+import org.komea.product.database.model.CustomerCriteria;
+import org.komea.product.database.model.Person;
 import org.komea.product.database.model.Project;
 import org.komea.product.database.model.ProjectCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -19,18 +23,40 @@ public class ProjectDAOTest {
 	@Autowired
 	private ProjectMapper projectDAO;
 
-	@Transactional
+	@Autowired
+	private CustomerMapper customerDAO;
+
+
+
 	@Test
 	public void test() {
-		final Project project = new Project();
+
+		ProjectCriteria request = new ProjectCriteria();
+		request.createCriteria().andNameEqualTo("projet1");
+		Assert.assertTrue(projectDAO.selectByExample(request).isEmpty());
+
+		
+ 
+		
+		Customer jguidoux = new Customer();
+		jguidoux.setName("jguidoux");
+		customerDAO.insert(jguidoux);
+		
+		
+		CustomerCriteria request2 = new CustomerCriteria();
+//		request2.createCriteria();
+		List<Customer> customers = customerDAO.selectByExample(request2);
+
+		
+		Project project = new Project();
+		project.setKey("TEST1");
 		project.setName("projet1");
-		final ProjectCriteria example = new ProjectCriteria();
-		example.createCriteria().andNameEqualTo("projet1");
-		Assert.assertTrue(projectDAO.selectByExample(example).isEmpty());
+		project.setIdCustomer(customers.get(0).getId());
+
 
 		projectDAO.insert(project);
 
-		Assert.assertFalse(projectDAO.selectByExample(example).isEmpty());
+		Assert.assertFalse(projectDAO.selectByExample(request).isEmpty());
 	}
 
 }
