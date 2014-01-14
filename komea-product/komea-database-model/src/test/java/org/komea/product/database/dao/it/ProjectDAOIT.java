9100ad98e5c57e4be8a6cc6ac5fa71965e5f2f7a
jguidoux <jeremie.guidoux@tocea.com>
2014-01-14 15:13:09 +0100

    + add integration test with failsafe plugin and add dbunit exemple test

---
+++ komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/it/ProjectDAOIT.java
@@ -0,0 +1,63 @@
+
+package org.komea.product.database.dao.it;
+
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.komea.product.database.dao.CustomerMapper;
+import org.komea.product.database.dao.ProjectMapper;
+import org.komea.product.database.model.Customer;
+import org.komea.product.database.model.CustomerCriteria;
+import org.komea.product.database.model.Project;
+import org.komea.product.database.model.ProjectCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:/spring/applicationContext-test.xml")
+@TransactionConfiguration(defaultRollback = true)
+public class ProjectDAOIT
+{
+    
+    @Autowired
+    private ProjectMapper  projectDAO;
+    
+    @Autowired
+    private CustomerMapper customerDAO;
+    
+    @Test
+    @Transactional
+    public void test() {
+    
+        ProjectCriteria request = new ProjectCriteria();
+        request.createCriteria().andNameEqualTo("projet1");
+        // Assert.assertTrue(projectDAO.selectByExample(request).isEmpty());
+        
+        Customer jguidoux = new Customer();
+        jguidoux.setName("jguidoux");
+        this.customerDAO.insert(jguidoux);
+        
+        CustomerCriteria request2 = new CustomerCriteria();
+        // request2.createCriteria();
+        List<Customer> customers = this.customerDAO.selectByExample(request2);
+        
+        System.out.println("client id = " + jguidoux.getId());
+        Project project = new Project();
+        project.setProjectKey("TEST1");
+        project.setName("projet1");
+        project.setIdCustomer(jguidoux.getId());
+        project.setDescription("");
+        
+        this.projectDAO.insert(project);
+        
+        Assert.assertFalse(this.projectDAO.selectByExample(request).isEmpty());
+        Assert.assertEquals(1, this.projectDAO.selectByExample(request).size());
+    }
+    
+}
--- komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/test/ProjectDAOTest.java
+++ komea-product/komea-database-model/src/test/java/org/komea/product/database/dao/test/ProjectDAOTest.java
@@ -1,25 +1,35 @@
 
-package org.komea.product.database.dao;
+package org.komea.product.database.dao.test;
 
-
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.komea.product.database.dao.CustomerMapper;
+import org.komea.product.database.dao.ProjectMapper;
 import org.komea.product.database.model.Customer;
-import org.komea.product.database.model.CustomerCriteria;
 import org.komea.product.database.model.Project;
 import org.komea.product.database.model.ProjectCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath*:/spring/applicationContext-test.xml")
 @TransactionConfiguration(defaultRollback = true)
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class })
 public class ProjectDAOTest
 {
     
@@ -31,19 +41,15 @@ public class ProjectDAOTest
     
     @Test
     @Transactional
+    @DatabaseSetup("database.xml")
+    @ExpectedDatabase(value = "addCustomer.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     public void test() {
     
         ProjectCriteria request = new ProjectCriteria();
         request.createCriteria().andNameEqualTo("projet1");
         // Assert.assertTrue(projectDAO.selectByExample(request).isEmpty());
         
-        Customer jguidoux = new Customer();
-        jguidoux.setName("jguidoux");
-        this.customerDAO.insert(jguidoux);
-        
-        CustomerCriteria request2 = new CustomerCriteria();
-        // request2.createCriteria();
-        List<Customer> customers = this.customerDAO.selectByExample(request2);
+        Customer jguidoux = customerDAO.selectByPrimaryKey(1);
         
         System.out.println("client id = " + jguidoux.getId());
         Project project = new Project();
@@ -52,10 +58,10 @@ public class ProjectDAOTest
         project.setIdCustomer(jguidoux.getId());
         project.setDescription("");
         
-        this.projectDAO.insert(project);
+        projectDAO.insert(project);
         
-        Assert.assertFalse(this.projectDAO.selectByExample(request).isEmpty());
-        Assert.assertEquals(1, this.projectDAO.selectByExample(request).size());
+        Assert.assertFalse(projectDAO.selectByExample(request).isEmpty());
+        Assert.assertEquals(1, projectDAO.selectByExample(request).size());
     }
     
 }
