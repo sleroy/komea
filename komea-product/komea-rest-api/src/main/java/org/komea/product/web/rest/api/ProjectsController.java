0d5648dbb7029f8316ef971f2c469c34466aa1ba
jguidoux <jeremie.guidoux@tocea.com>
2014-01-16 11:48:48 +0100

    # [REST] fix project/all

--- komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/ProjectsController.java
+++ komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/ProjectsController.java
@@ -2,30 +2,23 @@
 package org.komea.product.web.rest.api;
 
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.komea.product.database.model.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/projects")
 public class ProjectsController
 {
     
-    
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectsController.class);
-    
-    
     
     /**
      * This method return the project list
@@ -33,9 +26,8 @@ public class ProjectsController
      * @return the project list
      */
     @RequestMapping(method = RequestMethod.GET, value = "/all")
-    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
     public List<Project> allProjects() {
-    
     
         LOGGER.debug("call rest method /projects/all/");
         // TODO
