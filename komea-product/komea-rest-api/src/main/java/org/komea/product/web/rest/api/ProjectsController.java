40f89492b1f1d0d9428060a62cac60b4157c573a
Sylvain Leroy <sleroy0@gmail.com>
2014-01-19 11:00:51 +0100

    Basic Rest API Get

--- komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/ProjectsController.java
+++ komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/ProjectsController.java
@@ -2,23 +2,34 @@
 package org.komea.product.web.rest.api;
 
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import org.komea.product.database.dao.ProjectMapper;
 import org.komea.product.database.model.Project;
+import org.komea.product.database.model.ProjectCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 @RequestMapping(value = "/projects")
 public class ProjectsController
 {
     
+    
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectsController.class);
+    
+    @Autowired
+    private ProjectMapper       projectDAO;
+    
+    
     
     /**
      * This method return the project list
@@ -29,9 +40,9 @@ public class ProjectsController
     @ResponseBody
     public List<Project> allProjects() {
     
+    
         LOGGER.debug("call rest method /projects/all/");
-        // TODO
-        return new ArrayList<Project>();
+        return projectDAO.selectByExample(new ProjectCriteria());
     }
     
 }
