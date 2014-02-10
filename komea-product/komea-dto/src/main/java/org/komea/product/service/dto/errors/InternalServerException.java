479a51bdadc17295821d3d8a7360cf6a0c3d4688
jguidoux <jeremie.guidoux@tocea.com>
2014-02-10 12:53:52 +0100

    - [DTO] remove som contrctors in InternalServerException

--- komea-product/komea-dto/src/main/java/org/komea/product/service/dto/errors/InternalServerException.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/service/dto/errors/InternalServerException.java
@@ -2,25 +2,11 @@
 package org.komea.product.service.dto.errors;
 
 
-public class InternalServerException extends Exception
-{
+public class InternalServerException extends Exception {
     
     public InternalServerException() {
     
         super();
-        // TODO Auto-generated InternalServerException stub
-    }
-    
-    public InternalServerException(final String _message, final Throwable _cause, final boolean _enableSuppression,
-            final boolean _writableStackTrace) {
-    
-        super(_message, _cause, _enableSuppression, _writableStackTrace);
-        // TODO Auto-generated InternalServerException stub
-    }
-    
-    public InternalServerException(final String _message, final Throwable _cause) {
-    
-        super(_message, _cause);
         // TODO Auto-generated InternalServerException stub
     }
     
@@ -30,10 +16,4 @@ public class InternalServerException extends Exception
         // TODO Auto-generated InternalServerException stub
     }
     
-    public InternalServerException(final Throwable _cause) {
-    
-        super(_cause);
-        // TODO Auto-generated InternalServerException stub
-    }
-    //
 }
