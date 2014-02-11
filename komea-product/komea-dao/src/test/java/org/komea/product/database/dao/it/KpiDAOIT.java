143d80b0b088690062fcfb003551f28a34bfa31b
jguidoux <jeremie.guidoux@tocea.com>
2014-02-11 11:27:58 +0100

    * dix unit test (mising column

--- komea-product/komea-dao/src/test/java/org/komea/product/database/dao/it/KpiDAOIT.java
+++ komea-product/komea-dao/src/test/java/org/komea/product/database/dao/it/KpiDAOIT.java
@@ -1,9 +1,12 @@
+
 package org.komea.product.database.dao.it;
+
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.komea.product.database.dao.KpiDao;
 import org.komea.product.database.enums.EntityType;
+import org.komea.product.database.enums.EvictionType;
 import org.komea.product.database.enums.ValueDirection;
 import org.komea.product.database.enums.ValueType;
 import org.komea.product.database.model.Kpi;
@@ -13,19 +16,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 public class KpiDAOIT extends AbstractSpringIntegrationTestCase {
-
+    
     @Autowired
     private KpiDao kpiDAO;
-
+    
     @Test
     @Transactional
     public void test() {
-
+    
         final KpiCriteria request = new KpiCriteria();
         request.createCriteria().andEntityTypeEqualTo(EntityType.PROJECT);
-
+        
         Assert.assertTrue(kpiDAO.selectByExampleWithBLOBs(request).isEmpty());
-
+        
         final Kpi record = new Kpi();
         record.setDescription("");
         record.setEsperRequest("salut");
@@ -37,12 +40,14 @@ public class KpiDAOIT extends AbstractSpringIntegrationTestCase {
         record.setEntityID(0);
         record.setValueMax(0d);
         record.setValueMin(0d);
-
+        record.setEvictionRate(1);
+        record.setEvictionType(EvictionType.DAYS);
+        
         kpiDAO.insert(record);
-
+        
         Assert.assertEquals(1, kpiDAO.selectByCriteria(request).size());
         Assert.assertEquals("salut", kpiDAO.selectByExampleWithBLOBs(request).get(0).getEsperRequest());
-
+        
     }
-
+    
 }
