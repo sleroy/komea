07aeca3fc55a37a79432d2691ffe8a85c7f944c4
jguidoux <jeremie.guidoux@tocea.com>
2014-02-12 11:46:22 +0100

    * [DTO] add methods to projectDTO

--- komea-product/komea-dto/src/main/java/org/komea/product/database/dto/ProjectDto.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/database/dto/ProjectDto.java
@@ -3,13 +3,23 @@ package org.komea.product.database.dto;
 
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
+import org.komea.product.database.model.Link;
 import org.komea.product.database.model.Person;
 import org.komea.product.database.model.PersonGroup;
 
+/**
+ * decribe information on a project
+ * <p>
+ * 
+ * @author $Author: jguidoux $
+ * @since 12 févr. 2014
+ */
 public class ProjectDto implements Serializable {
     
     private static final Logger LOGGER           = Logger.getLogger(ProjectDto.class.getName());
@@ -18,8 +28,14 @@ public class ProjectDto implements Serializable {
     private String              name;
     private String              description;
     private String              customer;
+    
+    /**
+     * <name,url>
+     */
     private Map<String, String> links;
+    
     private List<String>        tags;
+    
     /**
      * <login, firstname lastname>
      */
@@ -48,84 +64,29 @@ public class ProjectDto implements Serializable {
         this.associatedTeams = associatedTeams;
     }
     
-    public String getProjectKey() {
+    /**
+     * This method associate a Link to this project
+     * 
+     * @param _link
+     *            th link to associate
+     */
+    public void addLink(final Link _link) {
     
-        return projectKey;
+        links.put(_link.getName(), _link.getUrl());
     }
     
-    public void setProjectKey(final String projectKey) {
+    /**
+     * This method associate a list a link to this project
+     * 
+     * @param _linkList
+     *            the link list to associate
+     */
+    public void addLinkList(final List<Link> _linkList) {
     
-        this.projectKey = projectKey;
-    }
-    
-    public String getName() {
-    
-        return name;
-    }
-    
-    public void setName(final String name) {
-    
-        this.name = name;
-    }
-    
-    public String getDescription() {
-    
-        return description;
-    }
-    
-    public void setDescription(final String description) {
-    
-        this.description = description;
-    }
-    
-    public String getCustomer() {
-    
-        return customer;
-    }
-    
-    public void setCustomer(final String customer) {
-    
-        this.customer = customer;
-    }
-    
-    public Map<String, String> getLinks() {
-    
-        return links;
-    }
-    
-    public void setLinks(final Map<String, String> links) {
-    
-        this.links = links;
-    }
-    
-    public List<String> getTags() {
-    
-        return tags;
-    }
-    
-    public void setTags(final List<String> tags) {
-    
-        this.tags = tags;
-    }
-    
-    public Map<String, String> getAssociatedPersons() {
-    
-        return associatedPersons;
-    }
-    
-    public void setAssociatedPersons(final Map<String, String> associatedPersons) {
-    
-        this.associatedPersons = associatedPersons;
-    }
-    
-    public Map<String, String> getAssociatedTeams() {
-    
-        return associatedTeams;
-    }
-    
-    public void setAssociatedTeams(final Map<String, String> associatedTeams) {
-    
-        this.associatedTeams = associatedTeams;
+        for (Link link : _linkList) {
+            
+            addLink(link);
+        }
     }
     
     public void associatePerson(final Person _person) {
@@ -150,6 +111,232 @@ public class ProjectDto implements Serializable {
         for (PersonGroup team : _teamlist) {
             associateTeam(team);
         }
+    }
+    public Map<String, String> getAssociatedPersons() {
+    
+        return associatedPersons;
+    }
+    
+    public Map<String, String> getAssociatedTeams() {
+    
+        return associatedTeams;
+    }
+    
+    /**
+     * This method
+     * 
+     * @return
+     */
+    
+    public String getCustomer() {
+    
+        return customer;
+    }
+    
+    /**
+     * project desciption
+     * 
+     * @return the project description
+     */
+    public String getDescription() {
+    
+        return description;
+    }
+    
+    public Map<String, String> getLinks() {
+    
+        return links;
+    }
+    
+    /**
+     * This method get the link url with this links name
+     * 
+     * @param _linkName
+     *            link name
+     * @return the link url
+     */
+    public String getLinksUrl(final String _linkName) {
+    
+        return links.get(_linkName);
+    }
+    
+    /**
+     * the project name
+     * 
+     * @return project name
+     */
+    public String getName() {
+    
+        return name;
+    }
+    
+    /**
+     * This method return a person name (firstname lastname)
+     * 
+     * @param _login
+     *            the person login
+     * @return the person name
+     */
+    public String getPersonName(final String _login) {
+    
+        return associatedPersons.get(_login);
+    }
+    
+    /**
+     * This method return the project key
+     * 
+     * @return the project key
+     */
+    public String getProjectKey() {
+    
+        return projectKey;
+    }
+    
+    /**
+     * the tags list
+     * 
+     * @return all tags
+     */
+    public List<String> getTags() {
+    
+        return tags;
+    }
+    
+    /**
+     * This method a team name
+     * 
+     * @param _key
+     *            the team key
+     * @return the team name
+     */
+    public String getTeamName(final String _key) {
+    
+        return associatedTeams.get(_key);
+    }
+    
+    /**
+     * This method return all links name associate to this projects
+     * 
+     * @return the links name list
+     */
+    public Set<String> linksNameList() {
+    
+        return links.keySet();
+    }
+    
+    /**
+     * This method return all the person login associate to this projects
+     * 
+     * @return
+     */
+    public Set<String> loginList() {
+    
+        return associatedPersons.keySet();
+    }
+    
+    public int nbAssociatedPerson() {
+    
+        return associatedPersons.size();
+    }
+    
+    /**
+     * nb links associate to this projects
+     * 
+     * @return
+     */
+    public int nbLinks() {
+    
+        return links.size();
+    }
+    
+    /**
+     * This method return the number of teams associate to this project
+     * 
+     * @return the number of team
+     */
+    public int nbTeams() {
+    
+        return associatedTeams.size();
+    }
+    
+    /**
+     * This method list all person (firstname name) working in this project
+     * 
+     * @return
+     */
+    public Collection<String> personNameList() {
+    
+        return associatedPersons.values();
+    }
+    
+    public void setAssociatedPersons(final Map<String, String> associatedPersons) {
+    
+        this.associatedPersons = associatedPersons;
+    }
+    
+    public void setAssociatedTeams(final Map<String, String> associatedTeams) {
+    
+        this.associatedTeams = associatedTeams;
+    }
+    
+    public void setCustomer(final String customer) {
+    
+        this.customer = customer;
+    }
+    
+    public void setDescription(final String description) {
+    
+        this.description = description;
+    }
+    
+    public void setLinks(final Map<String, String> links) {
+    
+        this.links = links;
+    }
+    
+    public void setName(final String name) {
+    
+        this.name = name;
+    }
+    
+    public void setProjectKey(final String projectKey) {
+    
+        this.projectKey = projectKey;
+    }
+    
+    public void setTags(final List<String> tags) {
+    
+        this.tags = tags;
+    }
+    
+    /**
+     * This method list all team keys associate to this project
+     * 
+     * @return the team key list
+     */
+    public Set<String> teamKeyList() {
+    
+        return associatedTeams.keySet();
+    }
+    
+    /**
+     * This method list all team name associate to this project
+     * 
+     * @return the team name list
+     */
+    public Collection<String> teamNameList() {
+    
+        return associatedTeams.values();
+    }
+    
+    /**
+     * This method list all url links associate to this project
+     * 
+     * @return the url list
+     */
+    public Collection<String> urlsLinkList() {
+    
+        return links.values();
     }
     
 }
