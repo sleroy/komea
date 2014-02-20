69d4e86b998a6b799ff35a91583ea6b8f4af2a74
Sebastien Carreau <akira.seb@gmail.com>
2014-02-20 15:29:02 +0100

    [rest] events

--- komea-product/komea-rest-client/src/main/java/org/komea/product/rest/client/api/IEventsAPI.java
+++ komea-product/komea-rest-client/src/main/java/org/komea/product/rest/client/api/IEventsAPI.java
@@ -1,80 +1,48 @@
-
 package org.komea.product.rest.client.api;
-
-
 
 import java.net.ConnectException;
 import java.util.List;
 
 import org.komea.product.database.alert.Event;
-import org.komea.product.database.alert.enums.Criticity;
 import org.komea.product.database.dto.EventSimpleDto;
 import org.komea.product.database.dto.SearchEventDto;
 import org.komea.product.service.dto.errors.InternalServerException;
 
-
-
 /**
- * Komea rest api client to manage events
- * the interface call the komea server via the rest api
+ * Komea rest api client to manage events the interface call the komea server
+ * via the rest api
  * <p>
- * 
+ *
  * @author $Author: jguidoux $
  * @since 15 janv. 2014
  */
-public interface IEventsAPI extends IRestClientAPI
-{
-    
-    
+public interface IEventsAPI extends IRestClientAPI {
+
     /**
      * This method find events which have been stored into komea
-     * 
-     * @param _searchEvent
-     *            looking for event of some entities and of dome type
+     *
+     * @param _searchEvent looking for event of some entities and of dome type
      * @return an event list
-     * @throws ConnectException
-     *             launch if it can't connect to the server
-     * @throws InternalServerException
-     *             launch if exception happened in server side
+     * @throws ConnectException launch if it can't connect to the server
+     * @throws InternalServerException launch if exception happened in server
+     * side
      */
     List<Event> findEvents(final SearchEventDto _searchEvent)
             throws ConnectException, InternalServerException;
-    
-    
+
     /**
-     * @return
-     * @throws InternalServerException
+     * @return @throws InternalServerException
      * @throws ConnectException
      */
     List<Event> getEvents() throws ConnectException, InternalServerException;
-    
-    
-    /**
-     * This method find events
-     * 
-     * @param _severityMin
-     *            the severity min of the events to return
-     * @param _number
-     *            the number of event to return
-     * @return an event list
-     * @throws ConnectException
-     *             launch if it can't connect to the server
-     * @throws InternalServerException
-     *             launch if exception happened in server side
-     */
-    List<Event> getEvents(final Criticity _severityMin, final int _number)
-            throws ConnectException, InternalServerException;
-    
-    
+
     /**
      * This method push a new event into komea
-     * 
-     * @param _event
-     *            the event to push
-     * @throws ConnectException
-     *             launch if it can't connect to the server
-     * @throws InternalServerException
-     *             launch if exception happened in server side
+     *
+     * @param _event the event to push
+     * @throws ConnectException launch if it can't connect to the server
+     * @throws InternalServerException launch if exception happened in server
+     * side
      */
     void pushEvent(final EventSimpleDto _event) throws ConnectException, InternalServerException;
 }
--- komea-product/komea-rest-client/src/test/java/org/komea/product/rest/client/EventsAPIIT.java
+++ komea-product/komea-rest-client/src/test/java/org/komea/product/rest/client/EventsAPIIT.java
@@ -1,7 +1,4 @@
-
 package org.komea.product.rest.client;
-
-
 
 import java.net.ConnectException;
 import java.net.URISyntaxException;
@@ -13,72 +10,55 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.komea.product.database.alert.Event;
-import org.komea.product.database.alert.enums.Criticity;
 import org.komea.product.database.dto.EventSimpleDto;
 import org.komea.product.database.dto.SearchEventDto;
 import org.komea.product.rest.client.api.IEventsAPI;
 import org.komea.product.rest.client.api.IProvidersAPI;
 import org.komea.product.service.dto.errors.InternalServerException;
 
+public class EventsAPIIT extends AbstractRestClientIntegrationTestCase {
 
-
-public class EventsAPIIT extends AbstractRestClientIntegrationTestCase
-{
-    
-    
     private IEventsAPI projectsAPI;
-    
-    
-    
+
     @Test
     @Ignore("Need to be review")
     public void findEvents() throws Exception {
-    
-    
+
         final SearchEventDto searchEvent = new SearchEventDto();
         searchEvent.getEntityKeys().add("1");
         searchEvent.getEventTypeKeys().add("dtc");
-        
+
         final List<Event> events = projectsAPI.findEvents(searchEvent);
         Assert.assertFalse(events.isEmpty());
-        
+
         // FIXME::Assert.assertEquals("romain", events.get(0).getEntityName());
-        
     }
-    
-    
+
     @Test
     @Ignore("Need to be review")
     public void getEvents() throws Exception {
-    
-    
-        final List<Event> events = projectsAPI.getEvents(Criticity.MINOR, 1);
+
+        final List<Event> events = projectsAPI.getEvents();
         Assert.assertFalse(events.isEmpty());
-        
+
         // FIXME:: Assert.assertEquals("romain", events.get(0).getEntityName());
-        
     }
-    
-    
+
     @Before
     public void setUp() throws Exception {
-    
-    
+
         projectsAPI = RestClientFactory.INSTANCE.createEventsAPI("http://localhost:8585/komea");
         Assert.assertNotNull(projectsAPI);
     }
-    
-    
+
     //
-    
     @Test
     public void testPush() throws ConnectException, URISyntaxException, InternalServerException {
-    
-    
-        final IProvidersAPI providersAPI =
-                RestClientFactory.INSTANCE.createProvidersAPI("http://localhost:8585/komea");
+
+        final IProvidersAPI providersAPI
+                = RestClientFactory.INSTANCE.createProvidersAPI("http://localhost:8585/komea");
         Assert.assertNotNull(providersAPI);
-        
+
         final EventSimpleDto event = new EventSimpleDto();
         event.setDate(new Date());
         event.setEventType("BUILD_LAUNCHED");
@@ -88,10 +68,9 @@ public class EventsAPIIT extends AbstractRestClientIntegrationTestCase
         event.setProject("komea");
         event.setUrl("http://komea.tocea.com/jenkins/event1");
         projectsAPI.pushEvent(event);
-        
+
         // List<EventDto> events = projectsAPI.getEvents("MINOR", 1);
         // Assert.assertEquals(1, events.size());
-        
     }
-    
+
 }
