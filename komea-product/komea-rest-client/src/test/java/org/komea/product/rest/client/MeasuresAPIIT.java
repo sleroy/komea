dc9ddb147963a28d52db08b66722133c20e172d3
Sylvain Leroy <sleroy0@gmail.com>
2014-02-24 17:32:56 +0100

    Fixed test

--- komea-product/komea-rest-client/src/test/java/org/komea/product/rest/client/MeasuresAPIIT.java
+++ komea-product/komea-rest-client/src/test/java/org/komea/product/rest/client/MeasuresAPIIT.java
@@ -1,23 +1,29 @@
+
 package org.komea.product.rest.client;
+
+
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.komea.product.database.enums.EntityType;
 import org.komea.product.rest.client.api.IMeasuresAPI;
 import org.komea.product.service.dto.KpiKey;
-import org.komea.product.service.dto.errors.InternalServerException;
 
-public class MeasuresAPIIT extends AbstractRestClientIntegrationTestCase {
 
-    @Test(expected = InternalServerException.class)
+
+public class MeasuresAPIIT extends AbstractRestClientIntegrationTestCase
+{
+    
+    
+    @Test()
     public void testLastListMeasure() throws Exception {
-
-        IMeasuresAPI measuresAPI = RestClientFactory.INSTANCE.createMeasuresAPI("http://localhost:8585/komea");
+    
+    
+        final IMeasuresAPI measuresAPI =
+                RestClientFactory.INSTANCE.createMeasuresAPI("http://localhost:8585/komea");
         Assert.assertNotNull(measuresAPI);
-        KpiKey kpiKey = KpiKey.ofKpiNameAndEntityDetails("lines", EntityType.PERSON, 1);
-        double val = measuresAPI.lastMeasuresForKpiKey(kpiKey);
-        // Assert.assertTrue(projects.get(0) instanceof PersonGroup);
-        // Assert.assertFalse(projects.isEmpty());
+        final KpiKey kpiKey = KpiKey.ofKpiNameAndEntityDetails("lines", EntityType.PERSON, 1);
+        measuresAPI.lastMeasuresForKpiKey(kpiKey);
     }
-
+    
 }
