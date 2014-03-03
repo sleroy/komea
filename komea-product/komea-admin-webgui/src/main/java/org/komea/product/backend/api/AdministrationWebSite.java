4642fc03af3fc634e003a8ace465e27752717abf
RaphaelTocea <rgalerme@Tera>
2014-03-03 17:40:21 +0100

    Added team and department pages to wicket application

--- komea-product/komea-admin-webgui/src/main/java/org/komea/product/backend/api/AdministrationWebSite.java
+++ komea-product/komea-admin-webgui/src/main/java/org/komea/product/backend/api/AdministrationWebSite.java
@@ -1,10 +1,7 @@
 /**
- * 
+ *
  */
-
 package org.komea.product.backend.api;
-
-
 
 import org.komea.product.wicket.LoginPage;
 import org.komea.product.wicket.UnauthorizedPage;
@@ -15,39 +12,43 @@ import org.komea.product.wicket.kpis.KpiEditPage;
 import org.komea.product.wicket.kpis.KpiPage;
 import org.komea.product.wicket.person.PersonAddPage;
 import org.komea.product.wicket.person.PersonPage;
+import org.komea.product.wicket.persongroup.department.DepartmentEditPage;
+import org.komea.product.wicket.persongroup.department.DepartmentPage;
+import org.komea.product.wicket.persongroup.team.TeamEditPage;
+import org.komea.product.wicket.persongroup.team.TeamPage;
 import org.komea.product.wicket.project.ProjectEditPage;
 import org.komea.product.wicket.project.ProjectPage;
 import org.komea.product.wicket.providers.ProviderPage;
 import org.komea.product.wicket.settings.SettingsPage;
 import org.komea.product.wicket.statistics.StatPage;
 
-
-
 /**
  * This class defines the web pages of the administration web site
- * 
+ *
  * @author sleroy
  */
 @MountAdminPages({
-        @MountPage(mount = "/kpis", page = KpiPage.class),
-        @MountPage(mount = "/kpiedit", page = KpiEditPage.class),
-        @MountPage(mount = "/projectedit", page = ProjectEditPage.class),
-        @MountPage(mount = "/projects", page = ProjectPage.class),
-        @MountPage(mount = "/settings", page = SettingsPage.class),
-        @MountPage(mount = "/viewlog", page = ConsolePage.class),
-        @MountPage(mount = "/stats", page = StatPage.class),
-        @MountPage(mount = "/login", page = LoginPage.class),
-        @MountPage(mount = "/cronpage", page = CronPage.class),
-        @MountPage(mount = "/logout", page = LoginPage.class),
-        @MountPage(mount = "/accessdenied", page = UnauthorizedPage.class),
-        @MountPage(mount = "/users", page = PersonPage.class),
-        @MountPage(mount = "/useradd", page = PersonAddPage.class),
-        @MountPage(mount = "/events", page = EventsPage.class),
-        @MountPage(mount = "/plugins", page = ProviderPage.class)
-
+    @MountPage(mount = "/departments", page = DepartmentPage.class),
+    @MountPage(mount = "/departmentedit", page = DepartmentEditPage.class),
+    @MountPage(mount = "/teams", page = TeamPage.class),
+    @MountPage(mount = "/teamedit", page = TeamEditPage.class),
+    @MountPage(mount = "/kpis", page = KpiPage.class),
+    @MountPage(mount = "/kpiedit", page = KpiEditPage.class),
+    @MountPage(mount = "/projectedit", page = ProjectEditPage.class),
+    @MountPage(mount = "/projects", page = ProjectPage.class),
+    @MountPage(mount = "/settings", page = SettingsPage.class),
+    @MountPage(mount = "/viewlog", page = ConsolePage.class),
+    @MountPage(mount = "/stats", page = StatPage.class),
+    @MountPage(mount = "/login", page = LoginPage.class),
+    @MountPage(mount = "/cronpage", page = CronPage.class),
+    @MountPage(mount = "/logout", page = LoginPage.class),
+    @MountPage(mount = "/accessdenied", page = UnauthorizedPage.class),
+    @MountPage(mount = "/users", page = PersonPage.class),
+    @MountPage(mount = "/useradd", page = PersonAddPage.class),
+    @MountPage(mount = "/events", page = EventsPage.class),
+    @MountPage(mount = "/plugins", page = ProviderPage.class)
 
 })
-public class AdministrationWebSite
-{
+public class AdministrationWebSite {
     //
 }
--- komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
+++ komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
@@ -1,50 +1,30 @@
+
 package org.komea.product.wicket;
 
-<<<<<<< HEAD:komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
 
 
 import java.util.Map.Entry;
 import java.util.Set;
 
-=======
->>>>>>> add department and teamé:komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/WicketApplication.java
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.settings.IExceptionSettings;
 import org.apache.wicket.settings.IExceptionSettings.ThreadDumpStrategy;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
-<<<<<<< HEAD:komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
 import org.komea.product.backend.api.IPluginAdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-=======
-import org.komea.product.wicket.console.ConsolePage;
-import org.komea.product.wicket.cronpage.CronPage;
-import org.komea.product.wicket.events.EventsPage;
-import org.komea.product.wicket.kpis.KpiEditPage;
-import org.komea.product.wicket.kpis.KpiPage;
-import org.komea.product.wicket.person.PersonAddPage;
-import org.komea.product.wicket.person.PersonPage;
-import org.komea.product.wicket.persongroup.department.DepartmentEditPage;
-import org.komea.product.wicket.persongroup.department.DepartmentPage;
-import org.komea.product.wicket.persongroup.team.TeamEditPage;
-import org.komea.product.wicket.persongroup.team.TeamPage;
-import org.komea.product.wicket.project.ProjectEditPage;
-import org.komea.product.wicket.project.ProjectPage;
-import org.komea.product.wicket.providers.ProviderPage;
-import org.komea.product.wicket.settings.SettingsPage;
-import org.komea.product.wicket.statistics.StatPage;
->>>>>>> add department and teamé:komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/WicketApplication.java
+
+
 
 /**
- * Application object for your web application. If you want to run this
- * application without deploying, run the Start class.
- *
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
+ * 
  * @see com.mycompany.Start#main(String[])
  */
-<<<<<<< HEAD:komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
 public class WicketApplication extends WebApplication
 {
     
@@ -65,33 +45,33 @@ public class WicketApplication extends WebApplication
     }
     
     
-=======
-public class WicketApplication extends WebApplication {
-
->>>>>>> add department and teamé:komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/WicketApplication.java
     /**
      * @see org.apache.wicket.Application#getHomePage()
      */
     @Override
     public Class<? extends WebPage> getHomePage() {
-
+    
+    
         return HomePage.class;
     }
-
+    
+    
     /**
      * @see org.apache.wicket.Application#init()
      */
     @Override
     public void init() {
-
+    
+    
         super.init();
-
+        
         // best place to do this is in Application#init()
         // Bootstrap.install(Application.get(), new BootstrapSettings());
+        
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
         // don't throw exceptions for missing translations
         getResourceSettings().setThrowExceptionOnMissingResource(false);
-
+        
         // enable ajax debug etc.
         getDebugSettings().setDevelopmentUtilitiesEnabled(true);
         // getDebugSettings().setComponentUseCheck(false);
@@ -100,41 +80,18 @@ public class WicketApplication extends WebApplication {
         
         getMarkupSettings().setStripWicketTags(true);
         // page mounts / SEO
-        mountPage("/teams", TeamPage.class);
-        mountPage("/teamedit", TeamEditPage.class);
-        mountPage("/departments", DepartmentPage.class);
-        mountPage("/departmentedit", DepartmentEditPage.class);
         mountPage("/home", HomePage.class);
-<<<<<<< HEAD:komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/WicketApplication.java
         LOGGER.info("#############################################################");
         final Set<Entry<String, Class<? extends WebPage>>> entrySet =
                 getPluginAdminPage().getRegisteredPages().entrySet();
         LOGGER.info("Wicket startup : {} pages", entrySet.size());
         for (final java.util.Map.Entry<String, Class<? extends WebPage>> entry : entrySet) {
-            LOGGER.info("> mounting {}->", entry.getKey(), entry.getValue().getName());
+            LOGGER.info("> mounting {}->{}", entry.getKey(), entry.getValue().getName());
             mountPage(entry.getKey(), entry.getValue());
         }
         LOGGER.info("#############################################################");
         
         
-=======
-        mountPage("/kpiedit", KpiEditPage.class);
-        mountPage("/kpis", KpiPage.class);
-        mountPage("/projectedit", ProjectEditPage.class);
-        mountPage("/projects", ProjectPage.class);
-        mountPage("/settings", SettingsPage.class);
-        mountPage("/viewlog", ConsolePage.class);
-        mountPage("/stats", StatPage.class);
-        mountPage("/login", LoginPage.class);
-        mountPage("/cronpage", CronPage.class);
-        mountPage("/logout", LoginPage.class);
-        mountPage("/accessdenied", UnauthorizedPage.class);
-        mountPage("/users", PersonPage.class);
-        mountPage("/useradd", PersonAddPage.class);
-        mountPage("/events", EventsPage.class);
-        mountPage("/plugins", ProviderPage.class);
-
->>>>>>> add department and teamé:komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/WicketApplication.java
         getExceptionSettings().setThreadDumpStrategy(ThreadDumpStrategy.ALL_THREADS);
         getExceptionSettings()
                 .setUnexpectedExceptionDisplay(IExceptionSettings.SHOW_EXCEPTION_PAGE);
