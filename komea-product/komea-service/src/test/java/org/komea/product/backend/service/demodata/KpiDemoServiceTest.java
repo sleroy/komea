87e34c9b7f20ef732c8bed314ae9d3f1b7f50bf3
Sebastien Carreau <akira.seb@gmail.com>
2014-03-05 18:42:22 +0100

    [service] KpiDemoServiceTest : repair tests

--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/demodata/KpiDemoServiceTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/demodata/KpiDemoServiceTest.java
@@ -1,10 +1,7 @@
 /**
- * 
+ *
  */
-
 package org.komea.product.backend.service.demodata;
-
-
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,159 +19,154 @@ import org.komea.product.service.dto.KpiKey;
 import org.komea.product.test.spring.AbstractSpringIntegrationTestCase;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-
 /**
  * @author sleroy
  */
-public class KpiDemoServiceTest extends AbstractSpringIntegrationTestCase
-{
-    
-    
+public class KpiDemoServiceTest extends AbstractSpringIntegrationTestCase {
+
     private static final JenkinsEventFactory jenkinsEventFactory = new JenkinsEventFactory();
-    
-    private static final EsperQueryTester    newTest             = EsperQueryTester
-                                                                         .newTest("BUILDER");
-    
-    private static final SonarEventFactory   sonarEventFactory   = new SonarEventFactory();
+
+    private static final EsperQueryTester newTest = EsperQueryTester
+            .newTest("BUILDER");
+
+    private static final SonarEventFactory sonarEventFactory = new SonarEventFactory();
     @Autowired
-    private IEsperEngine                     engine;
-    
-    
+    private IEsperEngine engine;
+
     @Autowired
-    private KpiDemoService                   kpiDemoService;
-    
-    
+    private KpiDemoService kpiDemoService;
+
     @Autowired
-    private IKPIService                      kpiService;
-    
-    
-    
+    private IKPIService kpiService;
+
     @Before
     public void setupBefore() {
-    
-    
-        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildComplete("SYSTEM", 1.0,
+
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildStarted("SYSTEM", 1.0,
                 "dev")));
         engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildComplete("SYSTEM", 1.0,
+                "dev")));
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildStarted("SYSTEM", 1.0,
+                "dev")));
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildComplete("SYSTEM", 1.0,
+                "dev")));
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildStarted("SCERTIFY", 1.0,
                 "dev")));
         engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildComplete("SCERTIFY", 1.0,
                 "dev")));
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildStarted("SCERTIFY", 1.0,
+                "dev")));
         engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildFailed("SCERTIFY", 1.0,
+                "dev")));
+        engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildStarted("SCERTIFY", 1.0,
                 "dev")));
         engine.sendEvent(newTest.convertDto(jenkinsEventFactory.sendBuildFailed("SCERTIFY", 1.0,
                 "dev")));
         engine.sendEvent(newTest.convertDto(sonarEventFactory.sendMetricValue("SCERTIFY", 12,
                 "line_coverage")));
-        
+
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testActualCoverage() throws KPINotFoundException {
-    
-    
+
         final Kpi kpi = kpiDemoService.actualLineCoverage();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
         kpiService.storeValueInHistory(KpiKey.ofKpi(kpi));
         Assert.assertEquals(1, valueTable.getNumberOfRecords());
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberBuildPerDay()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberBuildPerDay()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testNumberBuildPerDay() throws KPINotFoundException {
-    
-    
+
         final Kpi kpi = kpiDemoService.numberBuildPerDay();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
         kpiService.storeValueInHistory(KpiKey.ofKpi(kpi));
-        
+
         Assert.assertEquals(2, valueTable.getNumberOfRecords());
-        
+
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberBuildPerMonth()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberBuildPerMonth()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testNumberBuildPerMonth() throws KPINotFoundException {
-    
-    
+
         final Kpi kpi = kpiDemoService.numberBuildPerMonth();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
         kpiService.storeValueInHistory(KpiKey.ofKpi(kpi));
         Assert.assertEquals(2, valueTable.getNumberOfRecords());
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerDay()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerDay()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testNumberSuccessBuildPerDay() throws KPINotFoundException {
-    
-    
+
         final Kpi kpi = kpiDemoService.numberSuccessBuildPerDay();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
         kpiService.storeValueInHistory(KpiKey.ofKpi(kpi));
         Assert.assertEquals(2, valueTable.getNumberOfRecords());
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testNumberSuccessBuildPerWeek() throws KPINotFoundException {
-    
-    
+
         final Kpi kpi = kpiDemoService.numberSuccessBuildPerWeek();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
         kpiService.storeValueInHistory(KpiKey.ofKpi(kpi));
         Assert.assertEquals(2, valueTable.getNumberOfRecords());
     }
-    
-    
+
     /**
-     * Test method for {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
-     * 
+     * Test method for
+     * {@link org.komea.product.backend.service.demodata.KpiDemoService#numberSuccessBuildPerWeek()}.
+     *
      * @throws KPINotFoundException
      */
     @Test
     public final void testSuccessRateBuildPerWeek() throws KPINotFoundException {
-    
-    
+
         final Project mockProject = newTest.getMockProject("SCERTIFY");
-        final Double successScertify =
-                kpiService.getRealTimeValues(
+        final Double successScertify
+                = kpiService.getRealTimeValues(
                         KpiKey.ofKpi(kpiDemoService.numberSuccessBuildPerDay())).getValueOfEntity(
                         mockProject);
-        final Double nbBuildScertify =
-                kpiService.getRealTimeValues(KpiKey.ofKpi(kpiDemoService.numberBuildPerDay()))
-                        .getValueOfEntity(mockProject);
-        
+        final Double nbBuildScertify
+                = kpiService.getRealTimeValues(KpiKey.ofKpi(kpiDemoService.numberBuildPerDay()))
+                .getValueOfEntity(mockProject);
+
         final Kpi kpi = kpiDemoService.successRateJenkins();
         final KPIValueTable<Project> valueTable = kpiService.getRealTimeValues(KpiKey.ofKpi(kpi));
         valueTable.dump();
@@ -182,6 +174,5 @@ public class KpiDemoServiceTest extends AbstractSpringIntegrationTestCase
         Assert.assertEquals(100 * successScertify / nbBuildScertify,
                 valueTable.getValueOfEntity(mockProject), 0.1d);
     }
-    
-    
+
 }
