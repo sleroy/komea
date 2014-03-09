c05cc49a44ed433cde1e7283b17f2620c7aa32bc
Sylvain Leroy <sleroy0@gmail.com>
2014-03-09 20:38:16 +0100

    Separation of ldap configuration

--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/ldap/LdapUserServiceTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/ldap/LdapUserServiceTest.java
@@ -10,10 +10,12 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.komea.product.backend.service.ldap.LdapUser;
-import org.komea.product.backend.service.ldap.ILdapUserService;
+import org.junit.runner.RunWith;
 import org.komea.product.test.spring.AbstractSpringIntegrationTestCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 
 
@@ -22,6 +24,12 @@ import org.springframework.beans.factory.annotation.Autowired;
  * 
  * @author sleroy
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        locations = {
+                "classpath:/spring/application-context-test.xml",
+                "classpath:/spring/ldap-spring-test.xml" })
+@TransactionConfiguration(defaultRollback = true)
 public class LdapUserServiceTest extends AbstractSpringIntegrationTestCase
 {
     
