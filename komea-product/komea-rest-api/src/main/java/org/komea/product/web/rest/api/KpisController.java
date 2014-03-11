24dd948df40309762a415a801492962e7b96c04d
Sebastien Carreau <akira.seb@gmail.com>
2014-03-11 19:41:10 +0100

    [rest] KpisController : add method to get all Kpis for an entityType

--- komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/KpisController.java
+++ komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/KpisController.java
@@ -1,15 +1,15 @@
-
 package org.komea.product.web.rest.api;
 
-
+import java.util.Collections;
 import java.util.List;
-
 import org.komea.product.backend.service.kpi.IKPIService;
+import org.komea.product.database.enums.EntityType;
 import org.komea.product.database.model.Kpi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,23 +17,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/kpis")
 public class KpisController {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KpisController.class);
-    
+
     @Autowired
-    private IKPIService         service;
-    
+    private IKPIService service;
+
     /**
      * This method return the kpi list
-     * 
+     *
      * @return the kpi list
      */
     @RequestMapping(method = RequestMethod.GET, value = "/all")
     @ResponseBody
     public List<Kpi> allKpis() {
-    
+
         LOGGER.debug("call rest method /kpis/all/");
         return service.listAllKpis();
     }
-    
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get", produces = "application/json")
+    @ResponseBody
+    public List<Kpi> getKpis(@RequestBody EntityType entityType) {
+
+        LOGGER.debug("call rest method /kpis/get/");
+        return service.getKpis(entityType, Collections.EMPTY_LIST);
+    }
+
 }
