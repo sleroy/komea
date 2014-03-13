e7d16335fc97ea5272a0f20c5a4fb1f763822e6e
Sylvain Leroy <sleroy0@gmail.com>
2014-03-13 17:49:11 +0100

    Fixed bug with internationalization in User page

--- komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/person/PersonAddPage.java
+++ komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/person/PersonAddPage.java
@@ -1,9 +1,12 @@
+
 package org.komea.product.wicket.person;
 
-import com.googlecode.wicket.jquery.ui.widget.dialog.DialogButton;
+
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
+
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
@@ -16,168 +19,229 @@ import org.komea.product.backend.service.entities.IPersonGroupService;
 import org.komea.product.backend.service.entities.IPersonRoleService;
 import org.komea.product.backend.service.entities.IPersonService;
 import org.komea.product.backend.service.entities.IProjectService;
+import org.komea.product.backend.utils.KomeaEntry;
 import org.komea.product.database.model.Person;
 import org.komea.product.database.model.PersonGroup;
-import org.komea.product.wicket.KomeaEntry;
 import org.komea.product.wicket.LayoutPage;
 import org.komea.product.wicket.utils.SelectDialog;
 import org.komea.product.wicket.widget.builders.AjaxLinkLayout;
 
+import com.googlecode.wicket.jquery.ui.widget.dialog.DialogButton;
+
+
+
 /**
  * Person admin page
- *
+ * 
  * @author sleroy
  */
-public class PersonAddPage extends LayoutPage {
-
+public class PersonAddPage extends LayoutPage
+{
+    
+    
     @SpringBean
-    private IFormularService formularService;
-
+    private IFormularService    formularService;
+    
     @SpringBean
-    private IPersonService personDAO;
-
-    @SpringBean
-    private IPersonRoleService personRoleDAO;
-
-    @SpringBean
-    private IProjectService projectDAO;
-
+    private IPersonService      personDAO;
+    
     @SpringBean
     private IPersonGroupService personGroupService;
-
+    
+    @SpringBean
+    private IPersonRoleService  personRoleDAO;
+    
+    @SpringBean
+    private IProjectService     projectDAO;
+    
+    
+    
     public PersonAddPage(final PageParameters _parameters) {
-
+    
+    
         this(_parameters, new Person());
-
+        
     }
-
+    
+    
     public PersonAddPage(final PageParameters _parameters, final Person _person) {
-
+    
+    
         super(_parameters);
-
+        
         final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
         feedbackPanel.setOutputMarkupId(true);
         add(feedbackPanel);
         final PersonFormData newPersonForm = formularService.newPersonForm();
-        final PersonForm personForm
-                = new PersonForm(personDAO, newPersonForm, "form", feedbackPanel,
-                        new CompoundPropertyModel<Person>(_person), this, personGroupService, projectDAO);
+        final PersonForm personForm =
+                new PersonForm(personDAO, newPersonForm, "form", feedbackPanel,
+                        new CompoundPropertyModel<Person>(_person), this, personGroupService,
+                        projectDAO);
         add(personForm);
-
-        IChoiceRenderer<PersonGroup> iChoiceRenderer = new IChoiceRenderer<PersonGroup>() {
-
+        
+        final IChoiceRenderer<PersonGroup> iChoiceRenderer = new IChoiceRenderer<PersonGroup>()
+        {
+            
+            
             @Override
-            public Object getDisplayValue(PersonGroup t) {
+            public Object getDisplayValue(final PersonGroup t) {
+            
+            
                 return t.getName();
             }
-
+            
+            
             @Override
-            public String getIdValue(PersonGroup t, int i) {
+            public String getIdValue(final PersonGroup t, final int i) {
+            
+            
                 return String.valueOf(t.getId());
             }
-
+            
         };
-
-        final SelectDialog<PersonGroup> dialogPersonGroup = new SelectDialog<PersonGroup>("dialogGroup", "Choose a team or department", personGroupService, iChoiceRenderer) {
-
-            @Override
-            public void onClose(AjaxRequestTarget target, DialogButton button) {
-//                target.add(kpiForm);
-            }
-
-            @Override
-            protected void onSubmit(AjaxRequestTarget target) {
-                PersonGroup selectedPersonGroup = getSelectedProvider();
-                if (selectedPersonGroup != null) {
-                    personForm.getPerson().setIdPersonGroup(selectedPersonGroup.getId());
-                    personForm.getGroupName().setName(selectedPersonGroup.getName());
-                    target.add(personForm.getGroupField());
-                }
-            }
-
-        };
+        
+        final SelectDialog<PersonGroup> dialogPersonGroup =
+                new SelectDialog<PersonGroup>("dialogGroup", "Choose a team or department",
+                        personGroupService, iChoiceRenderer)
+                {
+                    
+                    
+                    @Override
+                    public void onClose(final AjaxRequestTarget target, final DialogButton button) {
+                    
+                    
+                        // target.add(kpiForm);
+                    }
+                    
+                    
+                    @Override
+                    protected void onSubmit(final AjaxRequestTarget target) {
+                    
+                    
+                        final PersonGroup selectedPersonGroup = getSelectedProvider();
+                        if (selectedPersonGroup != null) {
+                            personForm.getPerson().setIdPersonGroup(selectedPersonGroup.getId());
+                            personForm.getGroupName().setName(selectedPersonGroup.getName());
+                            target.add(personForm.getGroupField());
+                        }
+                    }
+                    
+                };
         add(dialogPersonGroup);
-        personForm.add(new AjaxLinkLayout<LayoutPage>("btnGroup", this) {
-
+        personForm.add(new AjaxLinkLayout<LayoutPage>("btnGroup", this)
+        {
+            
+            
             @Override
             public void onClick(final AjaxRequestTarget art) {
+            
+            
                 dialogPersonGroup.open(art);
-
+                
             }
         });
-
+        
     }
-
+    
+    
     /**
      * @return the formularService
      */
     public IFormularService getFormularService() {
-
+    
+    
         return formularService;
     }
-
+    
+    
     @Override
     public List<? extends Entry<String, Class>> getMiddleLevelPages() {
-
+    
+    
         return Collections.singletonList(new KomeaEntry<String, Class>(
                 getString("PersonPage.title"), PersonPage.class));
-
+        
     }
-
+    
+    
     /**
      * @return the personDAO
      */
     public IPersonService getPersonDAO() {
-
+    
+    
         return personDAO;
     }
-
+    
+    
     /**
      * @return the personRoleDAO
      */
     public IPersonRoleService getPersonRoleDAO() {
-
+    
+    
         return personRoleDAO;
     }
-
+    
+    
     /**
      * @return the projectDAO
      */
     public IProjectService getProjectDAO() {
-
+    
+    
         return projectDAO;
     }
-
+    
+    
+    @Override
+    public String getTitle() {
+    
+    
+        return getString("PersonAddPage.title");
+    }
+    
+    
     /**
-     * @param _formularService the formularService to set
+     * @param _formularService
+     *            the formularService to set
      */
     public void setFormularService(final IFormularService _formularService) {
-
+    
+    
         formularService = _formularService;
     }
-
+    
+    
     /**
-     * @param _personDAO the personDAO to set
+     * @param _personDAO
+     *            the personDAO to set
      */
     public void setPersonDAO(final IPersonService _personDAO) {
-
+    
+    
         personDAO = _personDAO;
     }
-
+    
+    
     /**
-     * @param _personRoleDAO the personRoleDAO to set
+     * @param _personRoleDAO
+     *            the personRoleDAO to set
      */
     public void setPersonRoleDAO(final IPersonRoleService _personRoleDAO) {
-
+    
+    
         personRoleDAO = _personRoleDAO;
     }
-
+    
+    
     /**
-     * @param _projectDAO the projectDAO to set
+     * @param _projectDAO
+     *            the projectDAO to set
      */
     public void setProjectDAO(final IProjectService _projectDAO) {
-
+    
+    
         projectDAO = _projectDAO;
     }
-
 }
--- komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/widget/GridViewEntities.java
+++ komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/widget/GridViewEntities.java
@@ -1,4 +1,7 @@
+
 package org.komea.product.wicket.widget;
+
+
 
 import java.util.List;
 import org.apache.wicket.markup.html.basic.Label;
@@ -7,28 +10,42 @@ import org.apache.wicket.markup.repeater.data.GridView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.komea.product.database.api.IEntity;
 
-public class GridViewEntities<T extends IEntity> extends GridView<T> {
 
+
+public class GridViewEntities<T extends IEntity> extends GridView<T>
+{
+    
+    
     private static final long serialVersionUID = 1L;
-    private final String id;
-
+    private final String      id;
+    
+    
+    
     public GridViewEntities(final String id, final List<T> data) {
+    
+    
         super(id, new ListDataProvider<T>(data));
         this.id = id;
-        this.setColumns(1);
+        setColumns(1);
         if (!data.isEmpty()) {
-            this.setRows(data.size());
+            setRows(data.size());
         }
     }
-
+    
+    
     @Override
-    protected void populateEmptyItem(Item<T> item) {
+    protected void populateEmptyItem(final Item<T> item) {//
+    
+    
     }
-
+    
+    
     @Override
-    protected void populateItem(Item<T> item) {
+    protected void populateItem(final Item<T> item) {
+    
+    
         final T modelObject = item.getModelObject();
         item.add(new Label(id + "Element", "- " + modelObject.getDisplayName()));
     }
-
+    
 }
