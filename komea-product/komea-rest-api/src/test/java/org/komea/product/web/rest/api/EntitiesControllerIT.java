6a811398aa9036649e34fe7b4d658dbd5e39e71f
Sylvain Leroy <sleroy0@gmail.com>
2014-03-13 17:46:47 +0100

    Updated test since no demo data are provided in Komea

--- komea-product/komea-rest-api/src/test/java/org/komea/product/web/rest/api/EntitiesControllerIT.java
+++ komea-product/komea-rest-api/src/test/java/org/komea/product/web/rest/api/EntitiesControllerIT.java
@@ -1,7 +1,8 @@
+
 package org.komea.product.web.rest.api;
 
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,67 +19,95 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+
+
 @TransactionConfiguration(defaultRollback = true)
 @TestExecutionListeners({
-    DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
-    TransactionDbUnitTestExecutionListener.class})
-public class EntitiesControllerIT extends AbstractSpringWebIntegrationTestCase {
-
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class })
+public class EntitiesControllerIT extends AbstractSpringWebIntegrationTestCase
+{
+    
+    
     @Autowired
     private WebApplicationContext context;
-
-    private MockMvc mockMvc;
-
+    
+    private MockMvc               mockMvc;
+    
+    
+    
     @Before
     public void setUp() {
-
+    
+    
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
-
+    
+    
     //
     @Test
     @DatabaseSetup("database.xml")
     public void testAllDepartments() throws Exception {
-
-        final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.get("/departments/all"));
-
+    
+    
+        final ResultActions httpRequest =
+                mockMvc.perform(MockMvcRequestBuilders.get("/departments/all"));
+        
         httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(1)));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.equalToIgnoringCase("Department ABC")));
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(0)));
+        // httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].name",
+        // Matchers.equalToIgnoringCase("Department ABC")));
     }
-
-    @Test
-    @DatabaseSetup("database.xml")
-    public void testAllTeams() throws Exception {
-
-        final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.get("/teams/all"));
-
-        httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.equalToIgnoringCase("Team 1")));
-    }
-
+    
+    
     @Test
     @DatabaseSetup("database.xml")
     public void testAllPersons() throws Exception {
-
-        final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.get("/persons/all"));
-
+    
+    
+        final ResultActions httpRequest =
+                mockMvc.perform(MockMvcRequestBuilders.get("/persons/all"));
+        
         httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
         httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].login", Matchers.equalToIgnoringCase("jguidoux")));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[1].login", Matchers.equalToIgnoringCase("sleroy")));
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].login",
+                Matchers.equalToIgnoringCase("jguidoux")));
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[1].login",
+                Matchers.equalToIgnoringCase("sleroy")));
     }
-
+    
+    
     @Test
     @DatabaseSetup("database.xml")
     public void testAllProjects() throws Exception {
-
-        final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.get("/projects/all"));
-
+    
+    
+        final ResultActions httpRequest =
+                mockMvc.perform(MockMvcRequestBuilders.get("/projects/all"));
+        
         httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
         httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].projectKey", Matchers.equalToIgnoringCase("komea")));
-        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[1].projectKey", Matchers.equalToIgnoringCase("techdebt")));
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].projectKey",
+                Matchers.equalToIgnoringCase("komea")));
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[1].projectKey",
+                Matchers.equalToIgnoringCase("techdebt")));
+    }
+    
+    
+    @Test
+    @DatabaseSetup("database.xml")
+    public void testAllTeams() throws Exception {
+    
+    
+        final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.get("/teams/all"));
+        
+        httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
+        httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(0)));
+        // httpRequest.andExpect(MockMvcResultMatchers.jsonPath("$[0].name",
+        // Matchers.equalToIgnoringCase("Team 1")));
     }
 }
