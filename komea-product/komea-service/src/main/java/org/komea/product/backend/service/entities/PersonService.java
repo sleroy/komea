a49b9bb7c9559eb9042f5686553c0c02b4488cfe
Sebastien Carreau <akira.seb@gmail.com>
2014-03-18 18:41:42 +0100

    PersonService : findOrCreatePersonByEmail -> find by login if it exists

--- komea-product/komea-service/src/main/java/org/komea/product/backend/service/entities/PersonService.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/service/entities/PersonService.java
@@ -1,14 +1,10 @@
-
 package org.komea.product.backend.service.entities;
 
-
-
+import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.apache.commons.lang.Validate;
 import org.komea.product.backend.genericservice.AbstractService;
 import org.komea.product.backend.service.esper.IEventConversionAndValidationService;
@@ -36,68 +32,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.common.collect.Lists;
-
-
-
 /**
  */
 @Service
 @Transactional
 public class PersonService extends AbstractService<Person, Integer, PersonCriteria> implements
-        IPersonService
-{
-    
-    
-    private static final Logger                  LOGGER = LoggerFactory
-                                                                .getLogger(PersonService.class);
-    
+        IPersonService {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(PersonService.class);
+
     @Autowired
     private IEventConversionAndValidationService eventConversionAndValidationService;
-    
+
     @Autowired
-    private IPersonGroupService                  groupService;
-    
+    private IPersonGroupService groupService;
+
     @Autowired
-    private IMeasureHistoryService               measureService;
-    
+    private IMeasureHistoryService measureService;
+
     @Autowired
-    private IPersonRoleService                   personRoleService;
-    
+    private IPersonRoleService personRoleService;
+
     @Autowired
-    private IProjectPersonService                projectPersonService;
-    
+    private IProjectPersonService projectPersonService;
+
     @Autowired
-    private IProjectService                      projectService;
-    
+    private IProjectService projectService;
+
     @Autowired
-    private PersonDao                            requiredDAO;
-    
-    
-    
+    private PersonDao requiredDAO;
+
     /**
      *
      */
     public PersonService() {
-    
-    
+
         super();
     }
-    
-    
+
     /**
      * (non-Javadoc)
-     * 
-     * @see org.komea.product.backend.service.entities.IPersonService#convertAllPersonsIntoPersonDTO()
+     *
+     * @see
+     * org.komea.product.backend.service.entities.IPersonService#convertAllPersonsIntoPersonDTO()
      */
     @Override
     public List<PersonDto> convertAllPersonsIntoPersonDTO() {
-    
-    
+
         // TOTO STUB
         final PersonCriteria request = new PersonCriteria();
         final List<Person> persons = requiredDAO.selectByCriteria(request);
-        
+
         final List<PersonDto> personDtos = Lists.newArrayList();
         for (final Person person : persons) {
             final PersonDto personDto = new PersonDto();
@@ -106,11 +92,11 @@ public class PersonService extends AbstractService<Person, Integer, PersonCriter
             personDto.setFirstName(person.getFirstName());
             personDto.setLastName(person.getLastName());
             personDto.setLogin(person.getLogin());
-            final PersonGroup personGroup =
-                    groupService.selectByPrimaryKey(person.getIdPersonGroup());
+            final PersonGroup personGroup
+                    = groupService.selectByPrimaryKey(person.getIdPersonGroup());
             if (personGroup != null) {
-                final Pair<String, String> group =
-                        Pair.create(personGroup.getPersonGroupKey(), personGroup.getDisplayName());
+                final Pair<String, String> group
+                        = Pair.create(personGroup.getPersonGroupKey(), personGroup.getDisplayName());
                 if (PersonGroupType.TEAM.equals(personGroup.getType())) {
                     personDto.setTeam(group);
                 } else {
@@ -123,141 +109,111 @@ public class PersonService extends AbstractService<Person, Integer, PersonCriter
             }
             personDto.associateToProjectList(projectService.getProjectsOfPerson(person.getId()));
             personDtos.add(personDto);
-            
+
         }
         return personDtos;
     }
-    
-    
+
     /**
      * Convert a person into base entity (its a dto)
-     * 
-     * @param persons
-     *            List<Person>
+     *
+     * @param persons List<Person>
      * @return List<BaseEntityDto>
      * @see
-     *      org.komea.product.backend.service.entities.IPersonService#personsToBaseEntities(List<Person>)
+     * org.komea.product.backend.service.entities.IPersonService#personsToBaseEntities(List<Person>)
      */
     @Override
     public List<BaseEntityDto> convertPersonsToBaseEntities(final List<Person> persons) {
-    
-    
+
         final List<BaseEntityDto> entities = new ArrayList<BaseEntityDto>(persons.size());
         for (final Person person : persons) {
-            final BaseEntityDto entity =
-                    new BaseEntityDto(EntityType.PERSON, person.getId(), person.getLogin(),
+            final BaseEntityDto entity
+                    = new BaseEntityDto(EntityType.PERSON, person.getId(), person.getLogin(),
                             person.getFirstName() + " " + person.getLastName(),
                             person.getPassword());
             entities.add(entity);
         }
         return entities;
     }
-    
-    
+
     @Override
     public void deletePerson(final Person _person) {
-    
-    
-        projectPersonService.updateProjectsOfPerson(Collections.<Project> emptyList(), _person);
+
+        projectPersonService.updateProjectsOfPerson(Collections.<Project>emptyList(), _person);
         final MeasureCriteria measureCriteria = new MeasureCriteria();
         measureCriteria.createCriteria().andIdPersonEqualTo(_person.getId());
         measureService.deleteByCriteria(measureCriteria);
         delete(_person);
     }
-    
-    
+
     /*
      * (non-Javadoc)
      * @see org.komea.product.backend.service.entities.IPersonService#existUserByEmail(java.lang.String)
      */
     @Override
     public boolean existUserByEmail(final String _email) {
-    
-    
+
         return findUserByEmail(_email) != null;
     }
-    
-    
-    /**
-     * Find a login without duplicate.
-     * 
-     * @param loginFromEmail
-     *            the original requested login.
-     */
-    public String findLoginWithoutDuplicate(final String loginFromEmail) {
-    
-    
-        int tries = 1;
-        String testLogin = loginFromEmail;
-        while (existLogin(testLogin)) {
-            testLogin = loginFromEmail + tries++;
-        }
-        return testLogin;
-    }
-    
-    
+
     /*
      * (non-Javadoc)
      * @see org.komea.product.backend.service.entities.IPersonService#findOrCreatePersonByEmail(java.lang.String)
      */
     @Override
     public Person findOrCreatePersonByEmail(final String _email) {
-    
-    
+
         final PersonCriteria criteria = new PersonCriteria();
         criteria.createCriteria().andEmailEqualTo(_email);
         Person personRequested = CollectionUtil.singleOrNull(selectByCriteria(criteria));
-        stripLoginFromEmail(_email);
+
         if (personRequested == null) {
-            LOGGER.debug("Create a person by its email since it does not exist : {}", _email);
-            personRequested = new Person();
+            final String login = stripLoginFromEmail(_email);
+            personRequested = selectByKey(login);
+
+            if (personRequested == null) {
+                LOGGER.debug("Create a person by its email since it does not exist : {}", _email);
+                personRequested = new Person();
+                personRequested.setLogin(login);
+                personRequested.setFirstName(personRequested.getLogin());
+                personRequested.setLastName("");
+                personRequested.setPassword("");
+                personRequested.setUserBdd(UserBdd.KOMEA);
+            }
+
             personRequested.setEmail(_email);
-            
-            personRequested.setLogin(findLoginWithoutDuplicate(_email));
-            personRequested.setFirstName(personRequested.getLogin());
-            personRequested.setLastName("");
-            personRequested.setPassword("");
-            personRequested.setUserBdd(UserBdd.KOMEA);
-            
             eventConversionAndValidationService.validateObject(personRequested);
-            
             saveOrUpdate(personRequested);
         }
-        
+
         return personRequested;
     }
-    
-    
+
     /*
      * (non-Javadoc)
      * @see org.komea.product.backend.service.entities.IPersonService#findUserByEmail(java.lang.String)
      */
     @Override
     public Person findUserByEmail(final String _email) {
-    
-    
+
         final PersonCriteria personCriteria = new PersonCriteria();
         personCriteria.createCriteria().andEmailEqualTo(_email);
         return CollectionUtil.singleOrNull(selectByCriteria(personCriteria));
     }
-    
-    
+
     @Override
     public List<Person> getPersonsOfPersonGroup(final Integer groupId) {
-    
-    
+
         final PersonCriteria criteria = new PersonCriteria();
         criteria.createCriteria().andIdPersonGroupEqualTo(groupId);
         return requiredDAO.selectByCriteria(criteria);
     }
-    
-    
+
     @Override
     public List<Person> getPersonsOfProject(final Integer _projectId) {
-    
-    
-        final List<HasProjectPersonKey> result =
-                projectPersonService.getPersonIdsOfProject(_projectId);
+
+        final List<HasProjectPersonKey> result
+                = projectPersonService.getPersonIdsOfProject(_projectId);
         final List<Person> persons = new ArrayList<Person>(result.size());
         for (final HasProjectPersonKey hasProjectPersonKey : result) {
             final Person person = selectByPrimaryKey(hasProjectPersonKey.getIdPerson());
@@ -267,20 +223,17 @@ public class PersonService extends AbstractService<Person, Integer, PersonCriter
         }
         return persons;
     }
-    
-    
+
     /*
      * (non-Javadoc)
      * @see org.komea.product.backend.genericservice.AbstractService#getRequiredDAO()
      */
     @Override
     public IGenericDAO<Person, Integer, PersonCriteria> getRequiredDAO() {
-    
-    
+
         return requiredDAO;
     }
-    
-    
+
     @Override
     public void saveOrUpdatePerson(
             @Valid
@@ -288,12 +241,11 @@ public class PersonService extends AbstractService<Person, Integer, PersonCriter
             final List<Project> _projects,
             final PersonRole _personRole,
             final PersonGroup _personGroup) {
-    
-    
+
         Validate.notNull(_person);
         Validate.notNull(_projects);
         new ObjectValidation().validateObject(_person);
-        
+
         getDaoEventRegistry().notifyUpdated(_person);
         for (final Project project : _projects) {
             getDaoEventRegistry().notifyUpdated(project);
@@ -306,89 +258,73 @@ public class PersonService extends AbstractService<Person, Integer, PersonCriter
         }
         _person.setIdPersonRoleOrNull(_personRole);
         _person.setIdPersonGroupOrNull(_personGroup);
-        
+
         //
         LOGGER.trace("Save or update person {}", _person);
         saveOrUpdate(_person);
         projectPersonService.updateProjectsOfPerson(_projects, _person);
-        
+
     }
-    
-    
+
     public void setEventConversionAndValidationService(
             final IEventConversionAndValidationService _eventConversionAndValidationService) {
-    
-    
+
         eventConversionAndValidationService = _eventConversionAndValidationService;
     }
-    
-    
+
     /**
-     * @param _groupService
-     *            the groupService to set
+     * @param _groupService the groupService to set
      */
     public void setGroupService(final IPersonGroupService _groupService) {
-    
-    
+
         groupService = _groupService;
     }
-    
-    
+
     /**
-     * @param _projectPersonService
-     *            the projectPersonService to set
+     * @param _projectPersonService the projectPersonService to set
      */
     public void setProjectPersonService(final IProjectPersonService _projectPersonService) {
-    
-    
+
         projectPersonService = _projectPersonService;
     }
-    
-    
+
     public void setRequiredDAO(final PersonDao _requiredDAO) {
-    
-    
+
         requiredDAO = _requiredDAO;
     }
-    
-    
+
     /**
      * Strip login from email.
-     * 
-     * @param email
-     *            the email
+     *
+     * @param email the email
      * @return the login.
      */
     public String stripLoginFromEmail(final String email) {
-    
-    
+
         final int indexOf = email.indexOf('@');
-        if (indexOf == -1) { return email; }
+        if (indexOf == -1) {
+            return email;
+        }
         return email.substring(0, indexOf);
     }
-    
-    
+
     /**
      * Tests if the login is already present
-     * 
-     * @param _loginFromEmail
-     *            the login
+     *
+     * @param _loginFromEmail the login
      * @return true if the login is alread present
      */
     private boolean existLogin(final String _loginFromEmail) {
-    
-    
+
         return !selectByCriteria(createPersonCriteriaOnLogin(_loginFromEmail)).isEmpty();
     }
-    
-    
+
     @Override
     protected PersonCriteria createPersonCriteriaOnLogin(final String key) {
-    
-    
+
         final PersonCriteria criteria = new PersonCriteria();
         criteria.createCriteria().andLoginEqualTo(key);
         return criteria;
     }
-    
+
 }
