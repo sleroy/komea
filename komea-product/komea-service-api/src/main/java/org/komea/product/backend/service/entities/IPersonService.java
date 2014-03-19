dcba822cd56dac21a6d93267a9f5badcc8f3492f
Sebastien Carreau <akira.seb@gmail.com>
2014-03-19 10:13:15 +0100

    IPersonService : remove method findLoginWithoutDuplicate

--- komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/entities/IPersonService.java
+++ komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/entities/IPersonService.java
@@ -1,10 +1,6 @@
-
 package org.komea.product.backend.service.entities;
 
-
-
 import java.util.List;
-
 import org.komea.product.backend.service.generic.IGenericService;
 import org.komea.product.database.dto.BaseEntityDto;
 import org.komea.product.database.dto.PersonDto;
@@ -14,108 +10,80 @@ import org.komea.product.database.model.PersonGroup;
 import org.komea.product.database.model.PersonRole;
 import org.komea.product.database.model.Project;
 
-
-
 /**
  * Komea service to manage person
  * <p>
- * 
+ *
  * @author $Author: jguidoux $
  * @since 12 févr. 2014
  * @version $Revision: 1.0 $
  */
-public interface IPersonService extends IGenericService<Person, Integer, PersonCriteria>
-{
-    
-    
+public interface IPersonService extends IGenericService<Person, Integer, PersonCriteria> {
+
     /**
      * Tests if the user is existing (from his email)
-     * 
-     * @param _email
-     *            the email
+     *
+     * @param _email the email
      * @return true if this user is present in the database
      */
     public boolean existUserByEmail(String _email);
-    
-    
-    /**
-     * Find a login without duplication (inserts a trailing number in case of duplication)
-     * 
-     * @param _loginName
-     * @return
-     */
-    public String findLoginWithoutDuplicate(String _loginName);
-    
-    
+
     /**
      * Find a user by its email.
-     * 
-     * @param _email
-     *            the email
+     *
+     * @param _email the email
      * @return null if not found.
      */
     public Person findUserByEmail(String _email);
-    
-    
+
     /**
      * get persons of a group
-     * 
-     * @param groupId
-     *            id of the group
+     *
+     * @param groupId id of the group
      * @return persons
      */
     public List<Person> getPersonsOfPersonGroup(Integer groupId);
-    
-    
+
     /**
      * This method list all person
-     * 
+     *
      * @return the person list
      */
     List<PersonDto> convertAllPersonsIntoPersonDTO();
-    
-    
+
     /**
      * Method personsToBaseEntities.
-     * 
-     * @param persons
-     *            List<Person>
+     *
+     * @param persons List<Person>
      * @return List<BaseEntityDto>
      */
     List<BaseEntityDto> convertPersonsToBaseEntities(List<Person> persons);
-    
-    
+
     /**
      * delete person
-     * 
-     * @param _person
-     *            person
+     *
+     * @param _person person
      */
     void deletePerson(final Person _person);
-    
-    
+
     /**
      * Find or create an user based on an email.
-     * 
-     * @param _email
-     *            the email.
+     *
+     * @param _email the email.
      */
     Person findOrCreatePersonByEmail(String _email);
-    
-    
+
     /**
      * get persons of a project
-     * 
-     * @param _projectId
-     *            id of the project
+     *
+     * @param _projectId id of the project
      * @return list of persons
      */
     List<Person> getPersonsOfProject(final Integer _projectId);
-    
-    
+
     /**
      * Save or update information of a person.
-     * 
+     *
      * @param _person
      * @param _projects
      * @param _selectedRole
@@ -126,5 +94,5 @@ public interface IPersonService extends IGenericService<Person, Integer, PersonC
             List<Project> _projects,
             PersonRole _selectedRole,
             PersonGroup _personGroup);
-    
+
 }
--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/entities/PersonServiceTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/entities/PersonServiceTest.java
@@ -1,11 +1,8 @@
-
 package org.komea.product.backend.service.entities;
 
-
-
+import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,68 +23,57 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.google.common.collect.Lists;
-
-
-
 /**
  */
 @RunWith(MockitoJUnitRunner.class)
-public class PersonServiceTest
-{
-    
-    
+public class PersonServiceTest {
+
     @Mock
-    private IPersonGroupService  groupService;
-    
+    private IPersonGroupService groupService;
+
     @Mock
-    private PersonDao            personDAOmock;
-    
+    private PersonDao personDAOmock;
+
     @Mock
-    private IPersonRoleService   personRoleService;
-    
+    private IPersonRoleService personRoleService;
+
     @Mock
-    private HasProjectPersonDao  projectPersonDaoMock;
+    private HasProjectPersonDao projectPersonDaoMock;
     @Mock
-    private IProjectService      projectService;
+    private IProjectService projectService;
     @InjectMocks
     private final IPersonService service = new PersonService();
-    
-    
-    
+
     //
     /**
      * Method getDepartment.
-     * 
+     *
      * @return PersonGroup
      */
     public PersonGroup getDepartment() {
-    
-    
+
         final PersonGroup group = new PersonGroup();
-        
+
         group.setDescription("research department");
         group.setId(1);
         group.setName("research department");
         group.setPersonGroupKey("RESEARCH_DEPARTMENT");
         group.setType(PersonGroupType.DEPARTMENT);
         return group;
-        
+
     }
-    
-    
+
     @Test
     public void testConvertAllPersonsIntoPersonDTO() {
-    
-    
+
         Mockito.when(personDAOmock.selectByCriteria(Matchers.any(PersonCriteria.class)))
                 .thenReturn(getPersons());
         Mockito.when(groupService.selectByPrimaryKey(2)).thenReturn(getTeam());
         Mockito.when(personRoleService.selectByPrimaryKey(1)).thenReturn(getRole());
         Mockito.when(projectService.getProjectsOfPerson(1)).thenReturn(Collections.EMPTY_LIST);
-        
+
         final List<PersonDto> personList = service.convertAllPersonsIntoPersonDTO();
-        
+
         Assert.assertEquals(1, personList.size());
         final PersonDto person = personList.get(0);
         Assert.assertEquals(1, person.getId().intValue());
@@ -97,37 +83,35 @@ public class PersonServiceTest
         Assert.assertTrue(person.hasTeam());
         Assert.assertEquals("KOMEA_TEAM", person.teamKey());
         Assert.assertEquals("dev", person.getRole());
-        
+
         Mockito.verify(personDAOmock, Mockito.times(1)).selectByCriteria(
                 Matchers.any(PersonCriteria.class));
-        
+
         Mockito.verify(projectService, Mockito.times(0)).selectByPrimaryKey(1);
-        
+
     }
-    
-    
+
     @Test
     public void testConvertAllPersonsIntoPersonDTOComplete() {
-    
-    
+
         Mockito.when(personDAOmock.selectByCriteria(Matchers.any(PersonCriteria.class)))
                 .thenReturn(getPersons());
-        
-        final List<HasProjectPersonKey> projectPersonList =
-                Lists.newArrayList(new HasProjectPersonKey(1, 1));
+
+        final List<HasProjectPersonKey> projectPersonList
+                = Lists.newArrayList(new HasProjectPersonKey(1, 1));
         Mockito.when(
                 projectPersonDaoMock.selectByCriteria(Matchers.any(HasProjectPersonCriteria.class)))
                 .thenReturn(projectPersonList);
-        
+
         Mockito.when(projectService.selectByPrimaryKey(1)).thenReturn(getProjects().get(0));
-        
+
         Mockito.when(groupService.selectByPrimaryKey(2)).thenReturn(getDepartment());
-        
+
         Mockito.when(personRoleService.selectByPrimaryKey(1)).thenReturn(getRole());
         Mockito.when(projectService.getProjectsOfPerson(1)).thenReturn(getProjects());
-        
+
         final List<PersonDto> personList = service.convertAllPersonsIntoPersonDTO();
-        
+
         Assert.assertEquals(1, personList.size());
         final PersonDto person = personList.get(0);
         Assert.assertEquals(1, person.getId().intValue());
@@ -136,61 +120,42 @@ public class PersonServiceTest
         Assert.assertEquals("RESEARCH_DEPARTMENT", person.departmentKey());
         Assert.assertEquals("research department", person.departmentName());
         Assert.assertEquals("dev", person.getRole());
-        
+
         Mockito.verify(personDAOmock, Mockito.times(1)).selectByCriteria(
                 Matchers.any(PersonCriteria.class));
-        
+
         Mockito.verify(personRoleService, Mockito.times(1)).selectByPrimaryKey(1);
-        
+
     }
-    
-    
-    @Test
-    public void testFindLoginWithoutDuplicate() throws Exception {
-    
-    
-        Assert.assertEquals("test", service.findLoginWithoutDuplicate("test"));
-        
-        // Assert.assertEquals("test.1", service.findLoginWithoutDuplicate("test"));
-        // Assert.assertEquals("test.2", service.findLoginWithoutDuplicate("test.1"));
-    }
-    
-    
+
     @Test
     public void testStripLoginFromEmail() throws Exception {
-    
-    
+
         final PersonService personService = new PersonService();
         Assert.assertEquals("truc", personService.stripLoginFromEmail("truc@gmail.com"));
     }
-    
-    
+
     @Test
     public void testStripLoginFromEmailWrong() throws Exception {
-    
-    
+
         final PersonService personService = new PersonService();
         Assert.assertEquals("", personService.stripLoginFromEmail("@gmail.com"));
     }
-    
-    
+
     @Test
     public void testStripLoginFromEmailWrong2() throws Exception {
-    
-    
+
         final PersonService personService = new PersonService();
         Assert.assertEquals("trucgmail.com", personService.stripLoginFromEmail("trucgmail.com"));
     }
-    
-    
+
     /**
      * Method getPersons.
-     * 
+     *
      * @return List<Person>
      */
     private List<Person> getPersons() {
-    
-    
+
         final Person person = new Person();
         person.setEmail("luke.skywailer@lighside.com");
         person.setFirstName("luke");
@@ -199,51 +164,45 @@ public class PersonServiceTest
         person.setLogin("lskywalker");
         person.setIdPersonGroup(2);
         person.setIdPersonRole(1);
-        
+
         return Lists.newArrayList(person);
     }
-    
-    
+
     /**
      * Method getProjects.
-     * 
+     *
      * @return List<Project>
      */
     private List<Project> getProjects() {
-    
-    
+
         final Project team = new Project();
         team.setId(1);
         team.setName("komea");
         team.setDescription("komea kpi");
         team.setProjectKey("KOMEA");
         team.setIdCustomer(1);
-        
+
         return Lists.newArrayList(team);
     }
-    
-    
+
     /**
      * Method getRole.
-     * 
+     *
      * @return PersonRole
      */
     private PersonRole getRole() {
-    
-    
+
         final PersonRole role = new PersonRole(1, "dev", "dev");
         return role;
     }
-    
-    
+
     /**
      * Method getTeam.
-     * 
+     *
      * @return PersonGroup
      */
     private PersonGroup getTeam() {
-    
-    
+
         final PersonGroup team = new PersonGroup();
         team.setId(2);
         team.setName("komea team");
@@ -252,6 +211,5 @@ public class PersonServiceTest
         team.setType(PersonGroupType.TEAM);
         return team;
     }
-    
-    
+
 }
