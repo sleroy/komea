9771267f55201498e647669726d250ec4e185e46
jguidoux <jeremie.guidoux@tocea.com>
2014-04-08 17:41:56 +0200

    * [REST] add logger in debug

--- komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/CorsFilter.java
+++ komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/CorsFilter.java
@@ -1,17 +1,27 @@
+
 package org.komea.product.web.rest.api;
 
+
 import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CorsFilter extends OncePerRequestFilter {
-
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
+    
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain)
             throws ServletException, IOException {
+    
+        LOGGER.debug("je passe dans le cors filter =============================");
         response.addHeader("Access-Control-Allow-Origin", "*");
         if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, OPTIONS");
