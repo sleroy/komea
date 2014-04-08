44b81d4bbe96e8417ce9ee40aa7f6eb385613622
Sylvain Leroy <sleroy0@gmail.com>
2014-04-08 13:52:29 +0200

    Fixed compilation error

--- komea-product/komea-packaging-war/src/main/java/org/komea/product/plugins/demodata/GitRepositoryExampleBean.java
+++ komea-product/komea-packaging-war/src/main/java/org/komea/product/plugins/demodata/GitRepositoryExampleBean.java
@@ -3,14 +3,6 @@ package org.komea.product.plugins.demodata;
 
 
 
-import javax.annotation.PostConstruct;
-
-import org.komea.product.backend.service.ISettingService;
-import org.komea.product.plugins.repository.model.ScmRepositoryDefinition;
-import org.komea.product.plugins.scm.api.IScmRepositoryService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
@@ -24,63 +16,63 @@ import org.springframework.stereotype.Component;
 public class GitRepositoryExampleBean
 {
     
-    
-    private static final String   DEFAULT_VALUE          = "0";
-    
-    private static final String   GIT_REPOSITORY_SAMPLES = "GIT_REPOSITORY_SAMPLES";
-    
-    private static Logger         LOGGER                 =
-                                                                 LoggerFactory
-                                                                         .getLogger(GitRepositoryExampleBean.class);
-    
-    @Autowired
-    private IScmRepositoryService repository;
-    
-    
-    @Autowired
-    private ISettingService       settingsService;
-    
-    
-    
-    @PostConstruct
-    public void initGitFeed() {
-    
-    
-        registerRepo("XStream Fedora GIT", "http://pkgs.fedoraproject.org/cgit/xstream.git",
-                "XSTREAM");
-        registerRepo("Maven GIT", "https://git-wip-us.apache.org/repos/asf/maven.git", "MAVEN");
-        registerRepo("Scertify GIT", "http://sleroy@zeus:8080/scm/git/scertify", "SCERTIFY");
-        registerRepo("SRA GIT", "http://sleroy@zeus:8080/scm/git/web-applications", "SRA");
-        registerRepo("CIFlow prototype", "http://sleroy@zeus:8080/scm/git/scertify-ciflow",
-                "CIFLOW");
-        
-    }
-    
-    
-    /**
-     * @param _feedName
-     * @param _url
-     * @return
-     */
-    private ScmRepositoryDefinition newGitRepository(final String _feedName, final String _url) {
-    
-    
-        final ScmRepositoryDefinition scmRepositoryDefinition = new ScmRepositoryDefinition();
-        scmRepositoryDefinition.setType("GIT");
-        scmRepositoryDefinition.setRepoName(_feedName);
-        scmRepositoryDefinition.setKey(ScmRepositoryDefinition.transformNameInKey(_feedName));
-        scmRepositoryDefinition.setUrl(_url);
-        return scmRepositoryDefinition;
-    }
-    
-    
-    private void registerRepo(final String _feedName, final String _url, final String _project) {
-    
-    
-        LOGGER.info("Registering demo GIT repositories {} {} {}", _feedName, _url, _project);
-        final ScmRepositoryDefinition gitRepository = newGitRepository(_feedName, _url);
-        gitRepository.setProjectForRepository(_project);
-        repository.saveOrUpdate(gitRepository);
-        
-    }
+    //
+    // private static final String DEFAULT_VALUE = "0";
+    //
+    // private static final String GIT_REPOSITORY_SAMPLES = "GIT_REPOSITORY_SAMPLES";
+    //
+    // private static Logger LOGGER =
+    // LoggerFactory
+    // .getLogger(GitRepositoryExampleBean.class);
+    //
+    // @Autowired
+    // private IScmRepositoryService repository;
+    //
+    //
+    // @Autowired
+    // private ISettingService settingsService;
+    //
+    //
+    //
+    // @PostConstruct
+    // public void initGitFeed() {
+    //
+    //
+    // registerRepo("XStream Fedora GIT", "http://pkgs.fedoraproject.org/cgit/xstream.git",
+    // "XSTREAM");
+    // registerRepo("Maven GIT", "https://git-wip-us.apache.org/repos/asf/maven.git", "MAVEN");
+    // registerRepo("Scertify GIT", "http://sleroy@zeus:8080/scm/git/scertify", "SCERTIFY");
+    // registerRepo("SRA GIT", "http://sleroy@zeus:8080/scm/git/web-applications", "SRA");
+    // registerRepo("CIFlow prototype", "http://sleroy@zeus:8080/scm/git/scertify-ciflow",
+    // "CIFLOW");
+    //
+    // }
+    //
+    //
+    // /**
+    // * @param _feedName
+    // * @param _url
+    // * @return
+    // */
+    // private ScmRepositoryDefinition newGitRepository(final String _feedName, final String _url) {
+    //
+    //
+    // final ScmRepositoryDefinition scmRepositoryDefinition = new ScmRepositoryDefinition();
+    // scmRepositoryDefinition.setType("GIT");
+    // scmRepositoryDefinition.setRepoName(_feedName);
+    // scmRepositoryDefinition.setKey(ScmRepositoryDefinition.transformNameInKey(_feedName));
+    // scmRepositoryDefinition.setUrl(_url);
+    // return scmRepositoryDefinition;
+    // }
+    //
+    //
+    // private void registerRepo(final String _feedName, final String _url, final String _project) {
+    //
+    //
+    // LOGGER.info("Registering demo GIT repositories {} {} {}", _feedName, _url, _project);
+    // final ScmRepositoryDefinition gitRepository = newGitRepository(_feedName, _url);
+    // gitRepository.setProjectForRepository(_project);
+    // repository.saveOrUpdate(gitRepository);
+    //
+    // }
 }
