a3efb2272377a263b5c4521ace3c1ba87f1ea94c
jguidoux <jeremie.guidoux@tocea.com>
2014-04-08 14:36:30 +0200

    * add many packaging type

--- komea-product/komea-admin-webgui/src/test/java/org/komea/product/web/dto/SpringConfigurationTest.java
+++ komea-product/komea-admin-webgui/src/test/java/org/komea/product/web/dto/SpringConfigurationTest.java
@@ -5,7 +5,7 @@
 package org.komea.product.web.dto;
 
 
-
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,35 +13,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-
-
 /**
  * @author sleroy
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = {
-        "classpath:/spring/application-context.xml",
-        "classpath:/spring/rest-servlet.xml",
-        "classpath:/spring/dispatcher-servlet.xml" })
+        "classpath:/spring/application-context.xml", "classpath:/spring/rest-servlet.xml", "classpath:/spring/dispatcher-servlet.xml" })
 @TransactionConfiguration(defaultRollback = true)
-public class SpringConfigurationTest
-{
-    
+public class SpringConfigurationTest {
     
     /**
      * 
      */
     public SpringConfigurationTest() {
     
-    
         // TODO Auto-generated constructor stub
     }
-    
-    
+    @Ignore(value = "To move")
     @Test
     public void test() {
-    
     
         //
         
