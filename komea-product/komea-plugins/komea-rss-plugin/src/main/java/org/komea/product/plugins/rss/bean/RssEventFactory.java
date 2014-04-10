a5ccf931bdd205aa82d8901d28d4dc0cf2e437b9
Sylvain Leroy <sleroy0@gmail.com>
2014-04-10 13:02:38 +0200

    Fixed regression in rss plugin

--- komea-product/komea-plugins/komea-rss-plugin/src/main/java/org/komea/product/plugins/rss/bean/RssEventFactory.java
+++ komea-product/komea-plugins/komea-rss-plugin/src/main/java/org/komea/product/plugins/rss/bean/RssEventFactory.java
@@ -1,11 +1,43 @@
-package org.komea.event.factory;
-import org.komea.product.database.dto.EventSimpleDto;
-public class RssEventFactory {
-public EventSimpleDto sendRssNews(URL _url) {EventSimpleDto event = new EventSimpleDto();
-event.setEventType("rss-news");
-event.setProvider("/rssnews");
-event.setURL(_url.toString());
-return event;
-}
 
+package org.komea.product.plugins.rss.bean;
+
+
+
+import java.util.Date;
+
+import org.komea.product.database.dto.EventSimpleDto;
+
+
+
+public class RssEventFactory
+{
+    
+    
+    /**
+     * Send rss news.
+     * 
+     * @param _message
+     *            the message
+     * @param _publishedDate
+     *            the date
+     * @param _uri
+     *            the uri
+     * @return
+     */
+    public EventSimpleDto sendRssNews(
+            final String _message,
+            final Date _publishedDate,
+            final String _uri) {
+    
+    
+        final EventSimpleDto event = new EventSimpleDto();
+        event.setEventType("rss-news");
+        event.setProvider("/rssnews");
+        event.setUrl(_uri);
+        event.setMessage(_message.isEmpty() ? " " : _message);
+        event.setDate(_publishedDate);
+        return event;
+        
+    }
+    
 }
