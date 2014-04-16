f686f7131fc7e62b264d51776c59e0d2ed5fd0a4
jguidoux <jeremie.guidoux@tocea.com>
2014-04-16 16:26:20 +0200

    * add dbunit test

---
+++ komea-product/komea-testspring/src/main/java/org/komea/product/test/spring/AbstractSpringDBunitIntegrationTest.java
@@ -0,0 +1,19 @@
+
+package org.komea.product.test.spring;
+
+
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@DatabaseSetup("/dbunit/database_complete.xml")
+public class AbstractSpringDBunitIntegrationTest extends AbstractSpringIntegrationTestCase {
+    //
+}
--- komea-product/komea-testspring/src/main/java/org/komea/product/test/spring/AbstractSpringIntegrationTestCase.java
+++ komea-product/komea-testspring/src/main/java/org/komea/product/test/spring/AbstractSpringIntegrationTestCase.java
@@ -2,15 +2,11 @@
 package org.komea.product.test.spring;
 
 
-
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-
-
 
 /**
  * This class defines the abstract class to perform integration tests on Spring
@@ -21,10 +17,8 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
     "classpath:/spring/application-context-test.xml" })
-@TransactionConfiguration(defaultRollback = true)
-public abstract class AbstractSpringIntegrationTestCase
-{
-    
+// @TransactionConfiguration(defaultRollback = true)
+public abstract class AbstractSpringIntegrationTestCase {
     
     protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
     
