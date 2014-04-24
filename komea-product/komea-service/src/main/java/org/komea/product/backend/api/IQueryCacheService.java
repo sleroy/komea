4c0079559e470707610dfb3a7cc135d25e24a605
Sebastien Carreau <akira.seb@gmail.com>
2014-04-24 13:46:32 +0200

    [service] QueryCacheService : add method refreshAll() and call it in QueryCacheCron

--- komea-product/komea-service/src/main/java/org/komea/product/backend/api/IQueryCacheService.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/api/IQueryCacheService.java
@@ -1,38 +1,30 @@
-
 package org.komea.product.backend.api;
 
-
-
 import java.util.List;
-
 import org.komea.cep.dynamicdata.IDynamicDataQuery;
-
-
 
 /**
  * This interface defines the service to manipulate the cache query.
- * 
+ *
  * @author sleroy
  */
-public interface IQueryCacheService
-{
-    
-    
+public interface IQueryCacheService {
+
     /**
      * Add a cache on a dynamic query.
-     * 
-     * @param dynamicDataQuery
-     *            the dynamic query
+     *
+     * @param dynamicDataQuery the dynamic query
      * @return the dynamic queyr with the cache.
      */
     IDynamicDataQuery addCacheOnDynamicQuery(final IDynamicDataQuery dynamicDataQuery);
-    
-    
+
     /**
      * Returns the stored query names.
-     * 
+     *
      * @return the stored query names
      */
     List<String> getStoredQueryNames();
-    
+
+    public void refreshAll();
+
 }
