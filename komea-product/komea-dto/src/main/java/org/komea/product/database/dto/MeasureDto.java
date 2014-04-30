40be14e35faf8ccd06cc9fb1527c766f056a9514
Sylvain Leroy <sleroy0@gmail.com>
2014-04-30 11:42:49 +0200

    [dto] measure dto : removed useless interface

--- komea-product/komea-dto/src/main/java/org/komea/product/database/dto/MeasureDto.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/database/dto/MeasureDto.java
@@ -1,53 +1,93 @@
+
 package org.komea.product.database.dto;
 
-import com.google.common.collect.Lists;
-import java.io.Serializable;
+
+
 import java.util.Date;
 import java.util.List;
+
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.komea.product.database.model.Measure;
 
-public class MeasureDto extends Measure implements Serializable {
+import com.google.common.collect.Lists;
 
+
+
+public class MeasureDto extends Measure
+{
+    
+    
     private static final long serialVersionUID = 1L;
-
-    private String kpiKey;
-
-    public MeasureDto() {
-    }
-
-    public MeasureDto(String kpiKey, Integer id, Integer idKpi, Date date, Integer idPersonGroup, Integer idPerson, Integer idProject, Double value) {
-        super(id, idKpi, date, idPersonGroup, idPerson, idProject, value);
-        this.kpiKey = kpiKey;
-    }
-
-    public MeasureDto(final Measure measure, String kpiKey) {
-        super(measure.getId(), measure.getIdKpi(), measure.getDate(),
-                measure.getIdPersonGroup(), measure.getIdPerson(),
-                measure.getIdProject(), measure.getValue());
-        this.kpiKey = kpiKey;
-    }
-
-    public String getKpiKey() {
-        return kpiKey;
-    }
-
-    public void setKpiKey(String kpiKey) {
-        this.kpiKey = kpiKey;
-    }
-
-    @Override
-    public String toString() {
-        return "MeasureDto{" + super.toString() + ", kpiKey=" + kpiKey + '}';
-    }
-
+    
+    
+    
     @JsonIgnore
     public static List<MeasureDto> convert(final List<Measure> measures, final String kpiKey) {
+    
+    
         final List<MeasureDto> measureDtos = Lists.newArrayList();
         for (final Measure measure : measures) {
             measureDtos.add(new MeasureDto(measure, kpiKey));
         }
         return measureDtos;
     }
-
+    
+    
+    
+    private String kpiKey;
+    
+    
+    
+    public MeasureDto() {
+    
+    
+    }
+    
+    
+    public MeasureDto(final Measure measure, final String kpiKey) {
+    
+    
+        super(measure.getId(), measure.getIdKpi(), measure.getDate(), measure.getIdPersonGroup(),
+                measure.getIdPerson(), measure.getIdProject(), measure.getValue());
+        this.kpiKey = kpiKey;
+    }
+    
+    
+    public MeasureDto(
+            final String kpiKey,
+            final Integer id,
+            final Integer idKpi,
+            final Date date,
+            final Integer idPersonGroup,
+            final Integer idPerson,
+            final Integer idProject,
+            final Double value) {
+    
+    
+        super(id, idKpi, date, idPersonGroup, idPerson, idProject, value);
+        this.kpiKey = kpiKey;
+    }
+    
+    
+    public String getKpiKey() {
+    
+    
+        return kpiKey;
+    }
+    
+    
+    public void setKpiKey(final String kpiKey) {
+    
+    
+        this.kpiKey = kpiKey;
+    }
+    
+    
+    @Override
+    public String toString() {
+    
+    
+        return "MeasureDto{" + super.toString() + ", kpiKey=" + kpiKey + '}';
+    }
+    
 }
