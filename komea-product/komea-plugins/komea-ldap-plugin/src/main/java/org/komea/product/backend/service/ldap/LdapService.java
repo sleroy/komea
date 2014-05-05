8537238c8e635eacf09551e311f1bc7ba8307f38
jguidoux <jeremie.guidoux@tocea.com>
2014-05-05 17:32:04 +0200

    * change ldap service to have onlt one ldap configuration

--- komea-product/komea-plugins/komea-ldap-plugin/src/main/java/org/komea/product/backend/service/ldap/LdapService.java
+++ komea-product/komea-plugins/komea-ldap-plugin/src/main/java/org/komea/product/backend/service/ldap/LdapService.java
@@ -9,7 +9,6 @@ package org.komea.product.backend.service.ldap;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 
@@ -17,10 +16,10 @@ import org.apache.commons.lang.Validate;
 import org.komea.product.api.service.ldap.ILdapConnector;
 import org.komea.product.api.service.ldap.ILdapService;
 import org.komea.product.api.service.ldap.LdapUser;
-import org.komea.product.backend.business.IDAOObjectStorage;
 import org.komea.product.backend.service.entities.IPersonGroupService;
 import org.komea.product.backend.service.entities.IPersonRoleService;
 import org.komea.product.backend.service.entities.IPersonService;
+import org.komea.product.backend.service.fs.IObjectStorage;
 import org.komea.product.backend.service.plugins.IPluginStorageService;
 import org.komea.product.database.enums.PersonGroupType;
 import org.komea.product.database.enums.UserBdd;
@@ -41,24 +40,24 @@ import com.google.common.base.Strings;
 @Service
 public class LdapService implements ILdapService {
     
-    private static final Logger           LOGGER = LoggerFactory.getLogger("ldap-plugin");
+    private static final Logger        LOGGER = LoggerFactory.getLogger("ldap-plugin");
     
     @Autowired
-    private IPersonGroupService           personGroupService;
+    private IPersonGroupService        personGroupService;
     
     @Autowired
-    private IPersonRoleService            personRoleService;
+    private IPersonRoleService         personRoleService;
     
     @Autowired
-    private IPersonService                personService;
+    private IPersonService             personService;
     
-    private IDAOObjectStorage<LdapServer> configurationStorage;
-    
-    @Autowired
-    private IPluginStorageService         pluginStorage;
+    private IObjectStorage<LdapServer> configurationStorage;
     
     @Autowired
-    private ILdapConnector                ldapConnector;
+    private IPluginStorageService      pluginStorage;
+    
+    @Autowired
+    private ILdapConnector             ldapConnector;
     
     /**
      * Generate missing department.
@@ -154,7 +153,7 @@ public class LdapService implements ILdapService {
     @PostConstruct
     public void init() {
     
-        configurationStorage = pluginStorage.registerDAOStorage("LDAP", LdapServer.class);
+        configurationStorage = pluginStorage.registerStorage("LDAP", LdapServer.class);
         
     }
     
@@ -168,15 +167,14 @@ public class LdapService implements ILdapService {
     @Override
     public LdapServer load() {
     
-        List<LdapServer> ldapConfigurations = configurationStorage.selectAll();
-        return ldapConfigurations.isEmpty() ? new LdapServer() : configurationStorage.selectAll().get(0);
+        return configurationStorage.get();
     }
     
     @Override
     public void saveOrUpdate(final LdapServer _ldapServer) {
     
         Validate.notNull(_ldapServer);
-        configurationStorage.saveOrUpdate(_ldapServer);
+        configurationStorage.set(_ldapServer);
     }
     
 }
