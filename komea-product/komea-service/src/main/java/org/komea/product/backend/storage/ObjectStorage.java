29419e23d3b2e894811f56aeb5280d228a111d1d
jguidoux <jeremie.guidoux@tocea.com>
2014-05-05 17:45:25 +0200

    # ObjectStorage: mettho het return an emtty ovjent and not null if not find

--- komea-product/komea-service/src/main/java/org/komea/product/backend/storage/ObjectStorage.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/storage/ObjectStorage.java
@@ -2,12 +2,12 @@
 package org.komea.product.backend.storage;
 
 
-
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import org.apache.commons.lang.SerializationException;
 import org.apache.commons.lang.Validate;
+import org.komea.eventory.utils.ClassUtils;
 import org.komea.product.backend.service.fs.IObjectStorage;
 import org.komea.product.backend.service.fs.IPluginFileSystem;
 import org.slf4j.Logger;
@@ -16,24 +16,18 @@ import org.slf4j.LoggerFactory;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.XppDriver;
 
-
-
 /**
  * This class implements an Object Storage.
  * 
  * @author sleroy
  * @version $Revision: 1.0 $
  */
-public class ObjectStorage<T> implements IObjectStorage<T>
-{
-    
+public class ObjectStorage<T> implements IObjectStorage<T> {
     
     private static final Logger     LOGGER   = LoggerFactory.getLogger(ObjectStorage.class);
     private final Class<?>          className;
     private final IPluginFileSystem service;
     private final XStream           X_STREAM = new XStream(new XppDriver());
-    
-    
     
     /**
      * Constructor for ObjectStorage.
@@ -45,13 +39,12 @@ public class ObjectStorage<T> implements IObjectStorage<T>
      */
     public ObjectStorage(final IPluginFileSystem _service, final Class<?> _className) {
     
-    
         service = _service;
         className = _className;
         Validate.notNull(service);
         Validate.notNull(_className);
+        
     }
-    
     
     /**
      * Method get.
@@ -62,20 +55,21 @@ public class ObjectStorage<T> implements IObjectStorage<T>
     @Override
     public synchronized T get() {
     
-    
         final String resourceName = getResource();
+        T defaultReturn = (T) ClassUtils.instantiate(className);
         try {
             
-            if (!service.existResource(resourceName)) { return null; }
+            if (!service.existResource(resourceName)) {
+                return defaultReturn;
+            }
             final InputStream open = service.open(resourceName);
             return (T) X_STREAM.fromXML(open);
         } catch (final Exception e) {
             LOGGER.error("Could not retrieve data from file {}, invalid format", resourceName, e);
         }
-        return null;
+        return defaultReturn;
         
     }
-    
     
     /**
      * Method set.
@@ -87,16 +81,13 @@ public class ObjectStorage<T> implements IObjectStorage<T>
     @Override
     public synchronized void set(final T _object) {
     
-    
         try {
             X_STREAM.toXML(_object, service.store(getResource()));
         } catch (final FileNotFoundException e) {
             throw new SerializationException(e.getMessage(), e);
         }
         
-        
     }
-    
     
     /**
      * Method getResource.
@@ -104,7 +95,6 @@ public class ObjectStorage<T> implements IObjectStorage<T>
      * @return String
      */
     private String getResource() {
-    
     
         return className.getName() + ".xml";
     }
