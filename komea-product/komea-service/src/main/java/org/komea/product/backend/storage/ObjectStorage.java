beb3db4ebedbf3f38d7674e46748f20591382a4c
Sylvain Leroy <sleroy0@gmail.com>
2014-05-06 10:59:21 +0200

    [object_storage] unit tests : 71.4% of coverage

--- komea-product/komea-service/src/main/java/org/komea/product/backend/storage/ObjectStorage.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/storage/ObjectStorage.java
@@ -1,93 +1,116 @@
+
 package org.komea.product.backend.storage;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.XppDriver;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+
+
 import org.apache.commons.lang.SerializationException;
 import org.apache.commons.lang.Validate;
 import org.komea.product.backend.service.fs.IObjectStorage;
 import org.komea.product.backend.service.fs.IPluginFileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+
+
 
 /**
  * This class implements an Object Storage.
- *
+ * 
  * @author sleroy
  * @version $Revision: 1.0 $
  */
-public class ObjectStorage<T> implements IObjectStorage<T> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ObjectStorage.class);
-    private final Class<?> className;
+public class ObjectStorage<T> implements IObjectStorage<T>
+{
+    
+    
+    private static final Logger     LOGGER   = LoggerFactory.getLogger(ObjectStorage.class);
+    private final Class<?>          className;
     private final IPluginFileSystem service;
-    private final XStream X_STREAM = new XStream(new XppDriver());
-
+    private final XStream           X_STREAM = new XStream(new XppDriver());
+    
+    
+    
     /**
      * Constructor for ObjectStorage.
-     *
-     * @param _service IPluginFileSystem
-     * @param _className Class<?>
+     * 
+     * @param _service
+     *            IPluginFileSystem
+     * @param _className
+     *            Class<?>
      */
     public ObjectStorage(final IPluginFileSystem _service, final Class<?> _className) {
-
+    
+    
         service = _service;
         className = _className;
         Validate.notNull(service);
         Validate.notNull(_className);
-
+        
     }
-
+    
+    
     /**
      * Method get.
-     *
+     * 
      * @return T
      * @see org.komea.product.backend.service.fs.IObjectStorage#get()
      */
     @Override
     public synchronized T get() {
-
+    
+    
+        T newBean = null;
         final String resourceName = getResource();
         try {
-            if (!service.existResource(resourceName)) {
-                return null;
+            if (service.existResource(resourceName)) {
+                
+                newBean = (T) X_STREAM.fromXML(service.open(resourceName));
             }
-            final InputStream open = service.open(resourceName);
-            return (T) X_STREAM.fromXML(open);
         } catch (final Exception e) {
             LOGGER.error("Could not retrieve data from file {}, invalid format", resourceName, e);
+        } finally {
+            if (newBean == null) {
+                newBean = (T) BeanUtils.instantiate(className);
+            }
         }
-        return null;
-
+        return newBean;
+        
     }
-
+    
+    
     /**
      * Method set.
-     *
-     * @param _object T
+     * 
+     * @param _object
+     *            T
      * @see org.komea.product.backend.service.fs.IObjectStorage#set(T)
      */
     @Override
     public synchronized void set(final T _object) {
-
+    
+    
         try {
-        	
-            X_STREAM.toXML(_object, service.store(getResource()));            
-        } catch (final FileNotFoundException e) {
+            
+            X_STREAM.toXML(_object, service.store(getResource()));
+        } catch (final Exception e) {
             throw new SerializationException(e.getMessage(), e);
         }
-
+        
     }
-
+    
+    
     /**
      * Method getResource.
-     *
+     * 
      * @return String
      */
     private String getResource() {
-
+    
+    
         return className.getName() + ".xml";
     }
-
+    
 }
--- komea-product/komea-service/src/test/java/org/komea/product/backend/storage/ObjectStorageTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/storage/ObjectStorageTest.java
@@ -9,6 +9,7 @@ package org.komea.product.backend.storage;
 import java.io.FileNotFoundException;
 import java.io.StringBufferInputStream;
 
+import org.apache.commons.io.output.NullOutputStream;
 import org.junit.Assert;
 import org.junit.Test;
 import org.komea.product.backend.service.fs.IPluginFileSystem;
@@ -16,6 +17,13 @@ import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import com.thoughtworks.xstream.XStream;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 
@@ -27,6 +35,11 @@ public class ObjectStorageTest
 {
     
     
+    private static final String SALUTJESUISUNECHAINE = "salutjesuisunechaine";
+    private static final String TARGET_TMP_XML       = "target/tmp.xml";
+    
+    
+    
     /**
      * Test method for
      * {@link org.komea.product.backend.storage.ObjectStorage#ObjectStorage(org.komea.product.backend.service.fs.IPluginFileSystem, java.lang.Class)}
@@ -35,7 +48,7 @@ public class ObjectStorageTest
      * @throws FileNotFoundException
      */
     @Test
-    public final void testObjectStorage() throws FileNotFoundException {
+    public final void testGet_Exist() throws FileNotFoundException {
     
     
         final IPluginFileSystem mock =
@@ -46,5 +59,40 @@ public class ObjectStorageTest
         Mockito.when(mock.open(Matchers.anyString())).thenReturn(
                 new StringBufferInputStream(new XStream().toXML("Yoda is a coward")));
         Assert.assertEquals("Yoda is a coward", objectStorage.get());
+    }
+    
+    
+    /**
+     * Test method for {@link org.komea.product.backend.storage.ObjectStorage#get()}.
+     */
+    @Test
+    public void testGet_NotExist() throws Exception {
+    
+    
+        final IPluginFileSystem mock =
+                Mockito.mock(IPluginFileSystem.class, Mockito.withSettings().verboseLogging());
+        
+        final ObjectStorage<String> objectStorage = new ObjectStorage<String>(mock, String.class);
+        final String string = objectStorage.get();
+        assertNotNull(string);
+        assertTrue(string.isEmpty());
+        
+    }
+    
+    
+    /**
+     * Test method for {@link org.komea.product.backend.storage.ObjectStorage#set(java.lang.Object)}.
+     */
+    @Test
+    public void testSet() throws Exception {
+    
+    
+        final IPluginFileSystem mock =
+                Mockito.mock(IPluginFileSystem.class, Mockito.withSettings().verboseLogging());
+        
+        final ObjectStorage<String> objectStorage = new ObjectStorage<String>(mock, String.class);
+        when(mock.store(Matchers.anyString())).thenReturn(new NullOutputStream());
+        objectStorage.set(SALUTJESUISUNECHAINE);
+        verify(mock, times(1)).store(Matchers.anyString());
     }
 }
