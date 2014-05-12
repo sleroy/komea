863155f01f5e2559b531383f88b9fdd340f5b8ef
jguidoux <jeremie.guidoux@tocea.com>
2014-05-12 14:51:43 +0200

    # [DTO] default constructor in SearchKpiALertDTO initialise lists

--- komea-product/komea-dto/src/main/java/org/komea/product/database/dto/SearchKpiAlertsDto.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/database/dto/SearchKpiAlertsDto.java
@@ -1,77 +1,117 @@
+
 package org.komea.product.database.dto;
 
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
+
 import org.komea.product.database.enums.ExtendedEntityType;
 import org.komea.product.database.enums.Severity;
 
+import com.google.common.collect.Lists;
+
 public class SearchKpiAlertsDto implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
+    
+    private static final long  serialVersionUID = 1L;
+    
     private ExtendedEntityType extendedEntityType;
-    private List<String> kpiAlertTypeKeys;
-    private List<String> entityKeys;
-    private Severity severityMin;
-    private boolean activatedOnly;
-
-    public SearchKpiAlertsDto(ExtendedEntityType entityType, List<String> kpiAlertTypeKeys,
-            List<String> entityKeys, Severity severityMin, boolean activatedOnly) {
-        this.extendedEntityType = entityType;
+    private List<String>       kpiAlertTypeKeys;
+    private List<String>       entityKeys;
+    private Severity           severityMin;
+    private boolean            activatedOnly;
+    
+    public SearchKpiAlertsDto() {
+    
+        kpiAlertTypeKeys = Lists.newArrayList();
+        entityKeys = Lists.newArrayList();
+    }
+    
+    public SearchKpiAlertsDto(final ExtendedEntityType entityType, final List<String> kpiAlertTypeKeys, final List<String> entityKeys,
+            final Severity severityMin, final boolean activatedOnly) {
+    
+        extendedEntityType = entityType;
         this.kpiAlertTypeKeys = kpiAlertTypeKeys;
         this.entityKeys = entityKeys;
         this.severityMin = severityMin;
         this.activatedOnly = activatedOnly;
     }
-
-    public SearchKpiAlertsDto() {
+    
+    public boolean addAlertKey(final String _alertKey) {
+    
+        return kpiAlertTypeKeys.add(_alertKey);
     }
-
-    public List<String> getKpiAlertTypeKeys() {
-        return kpiAlertTypeKeys;
+    
+    public boolean addAlertKeyList(final Collection<? extends String> _alertKeys) {
+    
+        return kpiAlertTypeKeys.addAll(_alertKeys);
     }
-
-    public void setKpiAlertTypeKeys(List<String> kpiAlertTypeKeys) {
-        this.kpiAlertTypeKeys = kpiAlertTypeKeys;
+    
+    public boolean addEntityKey(final String _entityKey) {
+    
+        return entityKeys.add(_entityKey);
     }
-
+    
+    public boolean addEntityKeyList(final Collection<? extends String> _entityKeys) {
+    
+        return entityKeys.addAll(_entityKeys);
+    }
+    
     public List<String> getEntityKeys() {
+    
         return entityKeys;
     }
-
-    public void setEntityKeys(List<String> entityKeys) {
-        this.entityKeys = entityKeys;
-    }
-
+    
     public ExtendedEntityType getExtendedEntityType() {
+    
         return extendedEntityType;
     }
-
-    public void setExtendedEntityType(ExtendedEntityType entityType) {
-        this.extendedEntityType = entityType;
+    
+    public List<String> getKpiAlertTypeKeys() {
+    
+        return kpiAlertTypeKeys;
     }
-
+    
     public Severity getSeverityMin() {
+    
         return severityMin;
     }
-
-    public void setSeverityMin(Severity severityMin) {
-        this.severityMin = severityMin;
-    }
-
+    
     public boolean isActivatedOnly() {
+    
         return activatedOnly;
     }
-
-    public void setActivatedOnly(boolean activatedOnly) {
+    
+    public void setActivatedOnly(final boolean activatedOnly) {
+    
         this.activatedOnly = activatedOnly;
     }
-
+    
+    public void setEntityKeys(final List<String> entityKeys) {
+    
+        this.entityKeys = entityKeys;
+    }
+    
+    public void setExtendedEntityType(final ExtendedEntityType entityType) {
+    
+        extendedEntityType = entityType;
+    }
+    
+    public void setKpiAlertTypeKeys(final List<String> kpiAlertTypeKeys) {
+    
+        this.kpiAlertTypeKeys = kpiAlertTypeKeys;
+    }
+    
+    public void setSeverityMin(final Severity severityMin) {
+    
+        this.severityMin = severityMin;
+    }
+    
     @Override
     public String toString() {
-        return "SearchKpiAlertsDto{" + "extendedEntityType=" + extendedEntityType + ", kpiAlertTypeKeys="
-                + kpiAlertTypeKeys + ", entityKeys=" + entityKeys + ", severityMin="
-                + severityMin + ", activatedOnly=" + activatedOnly + '}';
+    
+        return "SearchKpiAlertsDto{" + "extendedEntityType=" + extendedEntityType + ", kpiAlertTypeKeys=" + kpiAlertTypeKeys
+                + ", entityKeys=" + entityKeys + ", severityMin=" + severityMin + ", activatedOnly=" + activatedOnly + '}';
     }
-
+    
 }
