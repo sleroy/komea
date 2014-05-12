dac80b2676b4e2535fed45f0623c8885f24b4cc4
jguidoux <jeremie.guidoux@tocea.com>
2014-05-12 14:23:47 +0200

    * [GIT_PLUGIN] use enum insteadofString for SCM_TYPE

--- komea-product/komea-plugins/komea-git-plugin/src/main/java/org/komea/product/plugins/git/bean/GitProviderPlugin.java
+++ komea-product/komea-plugins/komea-git-plugin/src/main/java/org/komea/product/plugins/git/bean/GitProviderPlugin.java
@@ -2,20 +2,18 @@
 package org.komea.product.plugins.git.bean;
 
 
-
 import javax.annotation.PostConstruct;
 
 import org.komea.product.backend.service.ISettingService;
 import org.komea.product.backend.service.cron.ICronRegistryService;
 import org.komea.product.backend.service.entities.IPersonService;
 import org.komea.product.backend.service.esper.IEventPushService;
+import org.komea.product.plugins.repository.model.ScmType;
 import org.komea.product.plugins.scm.api.IScmRepositoryProxyFactories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
 
 /**
  * Main class to define the GIT Provider Plugin.
@@ -23,27 +21,22 @@ import org.springframework.stereotype.Service;
  * @author sleroy
  */
 @Service
-public class GitProviderPlugin
-{
-    
+public class GitProviderPlugin {
     
     /**
      * GIT Provider plugin name;
      */
     public static final String           GIT_PROVIDER_PLUGIN = "GIT Provider plugin";
     
-    public static final String           GIT_TYPE            = "GIT";
+    public static final ScmType          GIT_TYPE            = ScmType.GIT;
     
-    private static final Logger          LOGGER              = LoggerFactory
-                                                                     .getLogger("git-provider");
-    
+    private static final Logger          LOGGER              = LoggerFactory.getLogger("git-provider");
     
     @Autowired
     private ICronRegistryService         cronRegistryService;
     
     @Autowired
     private IEventPushService            esperEngine;
-    
     
     @Autowired
     private GitRepositoryProxyFactory    gitProxyFactory;
@@ -57,59 +50,45 @@ public class GitProviderPlugin
     @Autowired
     private ISettingService              registry;
     
-    
-    
     /**
      * @return the cronRegistryService
      */
     public ICronRegistryService getCronRegistryService() {
     
-    
         return cronRegistryService;
     }
-    
     
     /**
      * @return the esperEngine
      */
     public IEventPushService getEsperEngine() {
     
-    
         return esperEngine;
     }
     
-    
     public IPersonService getPersonService() {
-    
     
         return personService;
     }
     
-    
     public IScmRepositoryProxyFactories getProxyFactories() {
-    
     
         return proxyFactories;
     }
-    
     
     /**
      * @return the registry
      */
     public ISettingService getRegistry() {
     
-    
         return registry;
     }
-    
     
     @PostConstruct
     public void init() {
     
-    
         proxyFactories.registerFactory(GIT_TYPE, gitProxyFactory);
     }
-    
     
     /**
      * @param _cronRegistryService
@@ -117,10 +96,8 @@ public class GitProviderPlugin
      */
     public void setCronRegistryService(final ICronRegistryService _cronRegistryService) {
     
-    
         cronRegistryService = _cronRegistryService;
     }
-    
     
     /**
      * @param _esperEngine
@@ -128,31 +105,24 @@ public class GitProviderPlugin
      */
     public void setEsperEngine(final IEventPushService _esperEngine) {
     
-    
         esperEngine = _esperEngine;
     }
     
-    
     public void setPersonService(final IPersonService _personService) {
-    
     
         personService = _personService;
     }
     
-    
     public void setProxyFactories(final IScmRepositoryProxyFactories _proxyFactories) {
-    
     
         proxyFactories = _proxyFactories;
     }
-    
     
     /**
      * @param _registry
      *            the registry to set
      */
     public void setRegistry(final ISettingService _registry) {
-    
     
         registry = _registry;
     }
