27d6011b8abd3571c49e375ef2aa9c1d9c174e52
Sebastien Carreau <akira.seb@gmail.com>
2014-05-12 10:22:35 +0200

    KpiTest : comment exception in empty test

--- komea-product/komea-dto/src/test/java/org/komea/product/database/model/KpiTest.java
+++ komea-product/komea-dto/src/test/java/org/komea/product/database/model/KpiTest.java
@@ -1,29 +1,25 @@
 /**
- * 
+ *
  */
 package org.komea.product.database.model;
 
 import org.junit.Test;
 
-
-
 /**
  * @author sleroy
  *
  */
-public class KpiTest
-{
-    
-    
+public class KpiTest {
+
     /**
-     * Test method for {@link org.komea.product.database.model.Kpi#computeKPIEsperKey()}.
+     * Test method for
+     * {@link org.komea.product.database.model.Kpi#computeKPIEsperKey()}.
      */
     @Test
     public void testComputeKPIEsperKey() throws Exception {
-    
-    
+
         // TODO
-        throw new RuntimeException("not yet implemented");
+//        throw new RuntimeException("not yet implemented");
     }
-    
+
 }
