bdfea4c00d678e91dfbc236aa9cefbf54dc01023
jguidoux <jeremie.guidoux@tocea.com>
2014-05-12 14:54:00 +0200

    * [SERVICE] add unit test in AlertService

--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/alert/AlertServiceITest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/alert/AlertServiceITest.java
@@ -5,13 +5,25 @@
 package org.komea.product.backend.service.alert;
 
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.komea.product.database.dto.BaseEntityDto;
+import org.komea.product.database.dto.KpiAlertDto;
+import org.komea.product.database.dto.MeasureDto;
+import org.komea.product.database.dto.SearchKpiAlertsDto;
+import org.komea.product.database.enums.EntityType;
+import org.komea.product.database.enums.ExtendedEntityType;
 import org.komea.product.database.enums.Operator;
 import org.komea.product.database.enums.Severity;
 import org.komea.product.database.model.KpiAlertType;
+import org.komea.product.database.model.Measure;
+import org.komea.product.service.dto.AlertCriteria;
 import org.komea.product.test.spring.AbstractSpringDBunitIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.Lists;
 
 /**
  * @author sleroy
@@ -120,18 +132,95 @@ public class AlertServiceITest extends AbstractSpringDBunitIntegrationTest {
     @Test
     public void testIsAlertAssociatedToMeasureEntity() throws Exception {
     
-        org.junit.Assert.assertTrue("not yet implemented", false);
+        // GIVEN the alert COVERAGE_BRANCH_TOO_LOW
+        final KpiAlertType alertType = new KpiAlertType();
+        alertType.setKpiAlertKey("COVERAGE_BRANCH_TOO_LOW");
+        alertType.setIdKpi(1);
+        alertType.setId(1);
+        alertType.setOperator(Operator.LESSER);
+        alertType.setValue(60D);
+        alertType.setName("branch coverage too small");
+        alertType.setDescription("An alert is launched when the Branche coverage is < 60");
+        alertType.setSeverity(Severity.CRITICAL);
+        
+        // AND the project Komea
+        BaseEntityDto entity = new BaseEntityDto();
+        entity.setId(1);
+        entity.setEntityType(EntityType.PROJECT);
+        entity.setDescription("KPI manager");
+        entity.setName("Komea");
+        
+        // AND AlertCriteria to find on the project komea alerts of type COVERAGE_BRANCH_TOO_LOW
+        List<MeasureDto> measures = Lists.newArrayList();
+        AlertCriteria criteria = new AlertCriteria(alertType, entity, EntityType.PROJECT, measures);
+        
+        // AND a measure on theKomea project
+        Measure measure = new Measure();
+        measure.setEntity(EntityType.PROJECT, 1);
+        measure.setValue(25D);
+        
+        // WHEN I check if the mieasure corrspond to the alertCriterai
+        boolean isAssociate = alertService.isAlertAssociatedToMeasureEntity(criteria, measure);
+        
+        // THEN the result must be true
+        org.junit.Assert.assertTrue("The alert COVERAGE_BRANCH_TOO_LOW should be associate to the project Komea ", isAssociate);
     }
-    
     /**
      * Test method for
      * {@link org.komea.product.backend.service.alert.AlertService#isAlertFiltered(org.komea.product.database.dto.SearchKpiAlertsDto, org.komea.product.database.dto.KpiAlertDto)}
      * .
      */
     @Test
-    public void testIsAlertFiltered() throws Exception {
+    public void testIs_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_filtered() throws Exception {
     
-        org.junit.Assert.assertTrue("not yet implemented", false);
+        SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
+        filter.setActivatedOnly(true);
+        filter.addEntityKey("KOMEA");
+        filter.setExtendedEntityType(ExtendedEntityType.PROJECT);
+        filter.addAlertKey("COVERAGE_BRANCH_TOO_LOW");
+        
+        KpiAlertDto kpiAlert = new KpiAlertDto();
+        kpiAlert.setActivated(true);
+        
+        boolean isFiltered = alertService.isAlertFiltered(filter, kpiAlert);
+        
+        org.junit.Assert.assertTrue("The alert should be filtered ", isFiltered);
+        
+    }
+    
+    @Test
+    public void testIs_not_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_filtered() throws Exception {
+    
+        SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
+        filter.setActivatedOnly(true);
+        filter.addEntityKey("KOMEA");
+        filter.setExtendedEntityType(ExtendedEntityType.PROJECT);
+        filter.addAlertKey("COVERAGE_BRANCH_TOO_LOW");
+        
+        KpiAlertDto kpiAlert = new KpiAlertDto();
+        kpiAlert.setActivated(false);
+        
+        boolean isFiltered = alertService.isAlertFiltered(filter, kpiAlert);
+        
+        org.junit.Assert.assertFalse("The alert should not be filtered ", isFiltered);
+        
+    }
+    
+    @Test
+    public void testIs_not_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_with_no_activated_filtered() throws Exception {
+    
+        SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
+        filter.setActivatedOnly(false);
+        filter.addEntityKey("KOMEA");
+        filter.setExtendedEntityType(ExtendedEntityType.PROJECT);
+        filter.addAlertKey("COVERAGE_BRANCH_TOO_LOW");
+        
+        KpiAlertDto kpiAlert = new KpiAlertDto();
+        kpiAlert.setActivated(false);
+        
+        boolean isFiltered = alertService.isAlertFiltered(filter, kpiAlert);
+        
+        org.junit.Assert.assertTrue("The alert should be filtered ", isFiltered);
         
     }
 }
