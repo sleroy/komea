f280b6889880533606c398b314b9e812fa70478f
jguidoux <jeremie.guidoux@tocea.com>
2014-05-13 16:13:27 +0200

    * [ADMIN_WEB_GUI] fix unit test to test stat page

--- komea-product/komea-admin-webgui/src/test/java/org/komea/product/wicket/statistics/StatPageTest.java
+++ komea-product/komea-admin-webgui/src/test/java/org/komea/product/wicket/statistics/StatPageTest.java
@@ -4,11 +4,13 @@
 
 package org.komea.product.wicket.statistics;
 
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.komea.product.backend.api.IKpiValueService;
 import org.komea.product.backend.service.esper.IEventStatisticsService;
+import org.komea.product.backend.service.kpi.IStatisticsAPI;
 import org.komea.product.wicket.utils.WicketTesterMethodRule;
 import org.mockito.Mockito;
 
@@ -16,29 +18,27 @@ import org.mockito.Mockito;
  * @author sleroy
  */
 public class StatPageTest {
-
-	@Rule
-	public final WicketTesterMethodRule wicketRule = new WicketTesterMethodRule();
-
-	@Before
-	public void before() {
-
-		wicketRule.getApplicationContextMock().putBean(
-				Mockito.mock(IKpiValueService.class));
-		wicketRule.getApplicationContextMock().putBean(
-				Mockito.mock(IEventStatisticsService.class));
-
-	}
-
-	/**
-	 * Test method for
-	 * {@link org.komea.product.wicket.statistics.StatPage#StatPage(org.apache.wicket.request.mapper.parameter.PageParameters)}
-	 * .
-	 */
-	@Test
-	public final void testStatPage() throws Exception {
-
-		wicketRule.testStart(StatPage.class);
-	}
-
+    
+    @Rule
+    public final WicketTesterMethodRule wicketRule = new WicketTesterMethodRule();
+    
+    @Before
+    public void before() {
+    
+        wicketRule.getApplicationContextMock().putBean(Mockito.mock(IKpiValueService.class));
+        wicketRule.getApplicationContextMock().putBean(Mockito.mock(IEventStatisticsService.class));
+        wicketRule.getApplicationContextMock().putBean(Mockito.mock(IStatisticsAPI.class));
+        
+    }
+    
+    /**
+     * Test method for
+     * {@link org.komea.product.wicket.statistics.StatPage#StatPage(org.apache.wicket.request.mapper.parameter.PageParameters)} .
+     */
+    @Test
+    public final void testStatPage() throws Exception {
+    
+        wicketRule.testStart(StatPage.class);
+    }
+    
 }
