9827883477b019140868b150e75ecc912baeca58
jguidoux <jeremie.guidoux@tocea.com>
2014-05-14 17:14:29 +0200

    * [SERVICE] add method findKpi on IKpiService

--- komea-product/komea-service/src/main/java/org/komea/product/backend/api/IKPIService.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/api/IKPIService.java
@@ -1,4 +1,6 @@
+
 package org.komea.product.backend.api;
+
 
 import java.util.List;
 
@@ -13,56 +15,65 @@ import org.komea.product.service.dto.KpiKey;
 /**
  */
 public interface IKPIService extends IGenericService<Kpi, Integer, KpiCriteria> {
-
-	/**
-	 * Finds a KPI if existing.
-	 * 
-	 * @param _kpiKey
-	 *            KpiKey
-	 * @return the kpi./
-	 */
-	public Kpi findKPI(KpiKey _kpiKey);
-
-	/**
-	 * Finds a KPI or throws an exception
-	 * 
-	 * @param _kpiKey
-	 *            KpiKey
-	 * @return the KPI or an exception.
-	 */
-	public Kpi findKPIOrFail(KpiKey _kpiKey);
-
-	/**
-	 * @param _entityType
-	 * @return
-	 */
-	public List<Kpi> getAllKpisOfEntityType(EntityType _entityType);
-
-	/**
-	 * Creates a new KPI.
-	 * 
-	 * @param _kpi
-	 *            a new KPI.
-	 */
-	@Override
-	public void saveOrUpdate(Kpi _kpi);
-
-	/**
-	 * @param kpi
-	 */
-	void deleteKpi(Kpi kpi);
-
-	List<Kpi> getBaseKpisOfGroupKpiKeys(List<String> groupKpiKeys);
-
-	List<Kpi> getKpisForGroups(List<Kpi> simpleKpis);
-
-	List<Kpi> getKpisOfGroupKpiKeys(List<String> groupKpiKeys, List<Kpi> kpis);
-
-	/**
-	 * @param kpi
-	 * @param alertTypes
-	 * @param successFactors
-	 */
-	void saveOrUpdateKpi(Kpi kpi, List<KpiAlertType> alertTypes, List<SuccessFactor> successFactors);
-
+    
+    /**
+     * Finds a KPI if existing.
+     * 
+     * @param _kpiKey
+     *            KpiKey
+     * @return the kpi./
+     */
+    public Kpi findKPI(KpiKey _kpiKey);
+    
+    /**
+     * Finds a KPI if existing.
+     * 
+     * @param _kpiKey
+     *            KpiKey
+     * @return the kpi./
+     */
+    public Kpi findKPI(String _kpiKey);
+    
+    /**
+     * Finds a KPI or throws an exception
+     * 
+     * @param _kpiKey
+     *            KpiKey
+     * @return the KPI or an exception.
+     */
+    public Kpi findKPIOrFail(KpiKey _kpiKey);
+    
+    /**
+     * @param _entityType
+     * @return
+     */
+    public List<Kpi> getAllKpisOfEntityType(EntityType _entityType);
+    
+    /**
+     * Creates a new KPI.
+     * 
+     * @param _kpi
+     *            a new KPI.
+     */
+    @Override
+    public void saveOrUpdate(Kpi _kpi);
+    
+    /**
+     * @param kpi
+     */
+    void deleteKpi(Kpi kpi);
+    
+    List<Kpi> getBaseKpisOfGroupKpiKeys(List<String> groupKpiKeys);
+    
+    List<Kpi> getKpisForGroups(List<Kpi> simpleKpis);
+    
+    List<Kpi> getKpisOfGroupKpiKeys(List<String> groupKpiKeys, List<Kpi> kpis);
+    
+    /**
+     * @param kpi
+     * @param alertTypes
+     * @param successFactors
+     */
+    void saveOrUpdateKpi(Kpi kpi, List<KpiAlertType> alertTypes, List<SuccessFactor> successFactors);
+    
 }
