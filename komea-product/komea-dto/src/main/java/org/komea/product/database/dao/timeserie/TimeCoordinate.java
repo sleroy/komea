431f8afd8d3fb69140f72442e2be9727426dde47
jguidoux <jeremie.guidoux@tocea.com>
2014-05-14 23:52:19 +0200

    # [DTO] TimeCoordinate : fix getDate method

--- komea-product/komea-dto/src/main/java/org/komea/product/database/dao/timeserie/TimeCoordinate.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/database/dao/timeserie/TimeCoordinate.java
@@ -2,45 +2,30 @@
 package org.komea.product.database.dao.timeserie;
 
 
-
 import org.joda.time.DateTime;
 
-
-
-public class TimeCoordinate
-{
-    
+public class TimeCoordinate {
     
     private int    day;
     
-    
     private int    entityID;
-    
     
     private int    hour;
     
-    
     private int    idKpi;
     
-    
     private int    month;
-    
     
     private Double value;
     
     private int    week;
     
-    
     private int    year;
-    
-    
     
     public TimeCoordinate() {
     
-    
         super();
     }
-    
     
     /*
      * (non-Javadoc)
@@ -48,7 +33,6 @@ public class TimeCoordinate
      */
     @Override
     public boolean equals(final Object obj) {
-    
     
         if (this == obj) {
             return true;
@@ -91,7 +75,6 @@ public class TimeCoordinate
         return true;
     }
     
-    
     /**
      * Returns the period.
      * 
@@ -99,19 +82,15 @@ public class TimeCoordinate
      */
     public DateTime getDate() {
     
-    
-        final DateTime dateTime = new DateTime();
+        final DateTime dateTime = new DateTime(year, month, day, 0, 0);
         
         return dateTime;
     }
     
-    
     public int getDay() {
-    
     
         return day;
     }
-    
     
     /**
      * Returns the value of the field entityID.
@@ -120,17 +99,13 @@ public class TimeCoordinate
      */
     public int getEntityID() {
     
-    
         return entityID;
     }
     
-    
     public int getHour() {
-    
     
         return hour;
     }
-    
     
     /**
      * Returns the value of the field idKpi.
@@ -139,38 +114,28 @@ public class TimeCoordinate
      */
     public int getIdKpi() {
     
-    
         return idKpi;
     }
     
-    
     public int getMonth() {
-    
     
         return month;
     }
     
-    
     public Double getValue() {
-    
     
         return value;
     }
     
-    
     public int getWeek() {
-    
     
         return week;
     }
     
-    
     public int getYear() {
-    
     
         return year;
     }
-    
     
     /*
      * (non-Javadoc)
@@ -178,7 +143,6 @@ public class TimeCoordinate
      */
     @Override
     public int hashCode() {
-    
     
         final int prime = 31;
         int result = 1;
@@ -193,13 +157,10 @@ public class TimeCoordinate
         return result;
     }
     
-    
     public void setDay(final int _day) {
-    
     
         day = _day;
     }
-    
     
     /**
      * Sets the field entityID with the value of _entityID.
@@ -209,17 +170,13 @@ public class TimeCoordinate
      */
     public void setEntityID(final int _entityID) {
     
-    
         entityID = _entityID;
     }
     
-    
     public void setHour(final int _hour) {
-    
     
         hour = _hour;
     }
-    
     
     /**
      * Sets the field idKpi with the value of _kpiID.
@@ -229,38 +186,28 @@ public class TimeCoordinate
      */
     public void setIdKpi(final int _kpiID) {
     
-    
         idKpi = _kpiID;
     }
     
-    
     public void setMonth(final int _month) {
-    
     
         month = _month;
     }
     
-    
     public void setValue(final Double _value) {
-    
     
         value = _value;
     }
     
-    
     public void setWeek(final int _week) {
-    
     
         week = _week;
     }
     
-    
     public void setYear(final int _year) {
-    
     
         year = _year;
     }
-    
     
     /*
      * (non-Javadoc)
@@ -269,10 +216,7 @@ public class TimeCoordinate
     @Override
     public String toString() {
     
-    
-        return "TimeCoordinate [day="
-                + day + ", entityID=" + entityID + ", hour=" + hour + ", idKpi=" + idKpi
-                + ", month=" + month + ", value=" + value + ", week=" + week + ", year=" + year
-                + "]";
+        return "TimeCoordinate [day=" + day + ", entityID=" + entityID + ", hour=" + hour + ", idKpi=" + idKpi + ", month=" + month
+                + ", value=" + value + ", week=" + week + ", year=" + year + "]";
     }
 }
