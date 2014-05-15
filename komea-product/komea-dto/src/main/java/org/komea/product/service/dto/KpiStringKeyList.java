ab53a9529b0feceb2539b1d0230d39cf5b33309a
jguidoux <jeremie.guidoux@tocea.com>
2014-05-15 11:23:54 +0200

    * [DTO] add setter to StringKeyKpiList

--- komea-product/komea-dto/src/main/java/org/komea/product/service/dto/KpiStringKeyList.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/service/dto/KpiStringKeyList.java
@@ -10,59 +10,74 @@ import org.komea.product.database.enums.EntityType;
 import com.google.common.collect.Sets;
 
 public class KpiStringKeyList {
-
-    private final Set<String> kpiKey;
-    private final Set<String> entityKeys;
-    private final EntityType  entityType;
-
+    
+    private Set<String> kpiKeys;
+    private Set<String> entityKeys;
+    private EntityType  entityType;
+    
     public KpiStringKeyList() {
-
-        kpiKey = Sets.newHashSet();
+    
+        kpiKeys = Sets.newHashSet();
         entityKeys = Sets.newHashSet();
         entityType = EntityType.PROJECT;
     }
-
+    
+    public void setKpiKeys(final Set<String> _kpiKeys) {
+    
+        kpiKeys = _kpiKeys;
+    }
+    
+    public void setEntityKeys(final Set<String> _entityKeys) {
+    
+        entityKeys = _entityKeys;
+    }
+    
+    public void setEntityType(final EntityType _entityType) {
+    
+        entityType = _entityType;
+    }
+    
     public KpiStringKeyList(final Set<String> _kpiKey, final Set<String> _entityKeys, final EntityType _entityType) {
-
+    
         super();
-        kpiKey = _kpiKey;
+        kpiKeys = _kpiKey;
         entityKeys = _entityKeys;
         entityType = _entityType;
     }
-
+    
     public boolean addEntityKey(final String _entityKey) {
-
+    
         return entityKeys.add(_entityKey);
     }
-
+    
     public boolean addEntityKeys(final Collection<? extends String> _entityKeys) {
-
+    
         return entityKeys.addAll(_entityKeys);
     }
-
+    
     public void addKpiKey(final String _key) {
-
-        kpiKey.add(_key);
+    
+        kpiKeys.add(_key);
     }
-
+    
     public boolean addKpiKeys(final Collection<? extends String> _keys) {
-
-        return kpiKey.addAll(_keys);
+    
+        return kpiKeys.addAll(_keys);
     }
-
+    
     public Set<String> getEntityKeys() {
-
+    
         return entityKeys;
     }
-
+    
     public EntityType getEntityType() {
-
+    
         return entityType;
     }
-
-    public Set<String> getKpiKey() {
-
-        return kpiKey;
+    
+    public Set<String> getKpiKeys() {
+    
+        return kpiKeys;
     }
-
+    
 }
