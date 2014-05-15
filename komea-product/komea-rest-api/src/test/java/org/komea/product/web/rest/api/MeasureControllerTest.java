0c03f890095d4dcfb273eaf7b5891d2a3f1cacf1
jguidoux <jeremie.guidoux@tocea.com>
2014-05-15 16:37:52 +0200

    *  add olap dependency in scope test

--- komea-product/komea-rest-api/src/test/java/org/komea/product/web/rest/api/MeasureControllerTest.java
+++ komea-product/komea-rest-api/src/test/java/org/komea/product/web/rest/api/MeasureControllerTest.java
@@ -2,14 +2,20 @@
 package org.komea.product.web.rest.api;
 
 
-import org.easymock.Mock;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
-import org.komea.product.backend.service.kpi.IKpiAPI;
+import org.komea.product.backend.service.kpi.IMeasureService;
+import org.komea.product.database.dao.timeserie.PeriodTimeSerieOptions;
+import org.komea.product.database.dao.timeserie.TimeSerieDTO;
+import org.komea.product.service.dto.KpiStringKey;
 import org.komea.product.service.dto.KpiStringKeyList;
 import org.komea.product.service.dto.ManyHistoricalMeasureRequest;
 import org.komea.product.test.spring.AbstractSpringWebIntegrationTestCase;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -33,7 +39,7 @@ public class MeasureControllerTest extends AbstractSpringWebIntegrationTestCase 
     private MeasuresController    measureController;
     
     @Mock
-    private IKpiAPI               service;
+    private IMeasureService       service;
     
     @Before
     public void setUp() {
@@ -46,18 +52,22 @@ public class MeasureControllerTest extends AbstractSpringWebIntegrationTestCase 
     @Test
     public void testfindHistoricalMeasure() throws Exception {
     
-        // List<MeasureResult> measures = Lists.newArrayList();
-        // MeasureResult result = new MeasureResult();
-        // result.addHistoricalValue(12D, new Date());
-        // measures.add(result);
-        // Mockito.when(service.getHistoricalMeasures(Matchers.any(HistoryStringKeyList.class), Matchers.any(LimitCriteria.class)))
+        TimeSerieDTO serie = new TimeSerieDTO();
+        Mockito.when(service.findHistoricalMeasure(Matchers.any(KpiStringKey.class), Matchers.any(PeriodTimeSerieOptions.class)))
+                .thenReturn(serie);
         // .thenReturn(measures);
         
         // HistoryStringKeyList history = new HistoryStringKeyList(ExtendedEntityType.PROJECT);
         // LimitCriteria limit = LimitCriteria.createDefaultLimitCriteria();
         ManyHistoricalMeasureRequest request = new ManyHistoricalMeasureRequest();
+        PeriodTimeSerieOptions period = new PeriodTimeSerieOptions();
+        period.setFromPeriod(new DateTime(2014, 1, 1, 0, 0));
+        period.setToPeriod(new DateTime());
+        period.pickBestGranularity();
+        request.setPeriod(period);
         String jsonMessage = IntegrationTestUtil.convertObjectToJSON(request);
         System.out.println(jsonMessage);
+        
         final ResultActions httpRequest = mockMvc.perform(MockMvcRequestBuilders.post("/measures/historic")
                 .contentType(MediaType.APPLICATION_JSON).content(jsonMessage));
         
@@ -65,7 +75,6 @@ public class MeasureControllerTest extends AbstractSpringWebIntegrationTestCase 
         httpRequest.andExpect(MockMvcResultMatchers.status().isOk());
         
     }
-    
     @Test
     public void testCurrentMeasures() throws Exception {
     
