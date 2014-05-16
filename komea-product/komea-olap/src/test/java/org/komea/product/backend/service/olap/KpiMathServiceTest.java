7fc56bb1e9f46e2751f196b57c14f4417fd8acc3
jguidoux <jeremie.guidoux@tocea.com>
2014-05-16 12:20:24 +0200

    * foramt class

--- komea-product/komea-olap/src/test/java/org/komea/product/backend/service/olap/KpiMathServiceTest.java
+++ komea-product/komea-olap/src/test/java/org/komea/product/backend/service/olap/KpiMathServiceTest.java
@@ -5,34 +5,27 @@
 package org.komea.product.backend.service.olap;
 
 
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.apache.commons.math3.stat.descriptive.summary.Sum;
 import org.junit.Test;
-import org.komea.product.backend.service.olap.KpiMathService;
 import org.komea.product.database.model.Measure;
 
 import com.google.common.collect.Lists;
 
-import static org.junit.Assert.assertEquals;
-
-
-
 /**
  * @author sleroy
  */
-public class KpiMathServiceTest
-{
-    
+public class KpiMathServiceTest {
     
     /**
      * Test method for {@link org.komea.product.backend.service.olap.KpiMathService#computeAverageFromMeasures(java.util.List)}.
      */
     @Test
     public final void testComputeAverageFromMeasures() throws Exception {
-    
     
         final KpiMathService kpiMathService = new KpiMathService();
         final Measure fakeMeasure = fakeMeasure(12);
@@ -41,19 +34,15 @@ public class KpiMathServiceTest
         final double measures = kpiMathService.computeAverageFromMeasures(arrayList);
         final Mean mean = new Mean();
         
-        assertEquals(measures, mean.evaluate(new double[]
-            {
-                    12,
-                    24 }), 0.0d);
+        assertEquals(measures, mean.evaluate(new double[] {
+                12, 24 }), 0.0d);
     }
-    
     
     /**
      * Test method for {@link org.komea.product.backend.service.olap.KpiMathService#computeSumFromMeasures(java.util.List)}.
      */
     @Test
     public final void testComputeSumFromMeasures() throws Exception {
-    
     
         final KpiMathService kpiMathService = new KpiMathService();
         final Measure fakeMeasure = fakeMeasure(12);
@@ -62,15 +51,11 @@ public class KpiMathServiceTest
         final double measures = kpiMathService.computeSumFromMeasures(arrayList);
         final Sum mean = new Sum();
         
-        assertEquals(measures, mean.evaluate(new double[]
-            {
-                    12,
-                    24 }), 0.0d);
+        assertEquals(measures, mean.evaluate(new double[] {
+                12, 24 }), 0.0d);
     }
     
-    
     private Measure fakeMeasure(final double _value) {
-    
     
         final Measure measure = new Measure();
         measure.setValue(_value);
