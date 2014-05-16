f4b095eb4c84c4dfe6968cd6e20c139d5cbbdbcd
jguidoux <jeremie.guidoux@tocea.com>
2014-05-16 11:33:28 +0200

    * [SERVICE_API] add deprecated one some methods

--- komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/kpi/IKpiAPI.java
+++ komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/kpi/IKpiAPI.java
@@ -4,6 +4,7 @@
 
 package org.komea.product.backend.service.kpi;
 
+
 import java.util.Collection;
 import java.util.List;
 
@@ -11,8 +12,8 @@ import org.komea.product.database.enums.EntityType;
 import org.komea.product.database.model.Kpi;
 import org.komea.product.service.dto.HistoryStringKey;
 import org.komea.product.service.dto.HistoryStringKeyList;
-import org.komea.product.service.dto.LimitCriteria;
 import org.komea.product.service.dto.MeasureResult;
+import org.komea.product.service.dto.PeriodCriteria;
 
 /**
  * This interface defines the KPI api provided by Komea. It is recommended to
@@ -22,24 +23,24 @@ import org.komea.product.service.dto.MeasureResult;
  * @author sleroy
  */
 public interface IKpiAPI {
-
-	/**
-	 * Returns the list of kpis associated to an entity type.
-	 * 
-	 * @param _entityType
-	 *            the type of entity requested
-	 * @return the list of kpi.
-	 */
-	List<Kpi> getAllKpisOfEntityType(EntityType _entityType);
-
-	/**
-	 * @param _groupKpiKeys
-	 * @return
-	 * @deprecated TO BE REFACTORED
-	 */
-	@Deprecated
-	List<Kpi> getBaseKpisOfGroupKpiKeys(List<String> _groupKpiKeys);
-
+    
+    /**
+     * Returns the list of kpis associated to an entity type.
+     * 
+     * @param _entityType
+     *            the type of entity requested
+     * @return the list of kpi.
+     */
+    List<Kpi> getAllKpisOfEntityType(EntityType _entityType);
+    
+    /**
+     * @param _groupKpiKeys
+     * @return
+     * @deprecated TO BE REFACTORED
+     */
+    @Deprecated
+    List<Kpi> getBaseKpisOfGroupKpiKeys(List<String> _groupKpiKeys);
+    
     /**
      * This method find the historical measures of a a KPI on an entity
      *
@@ -48,8 +49,10 @@ public interface IKpiAPI {
      * @param _limit
      *            the limit of the research by entity. As the number of result of the date range
      * @return the measures
+     * @deprecated use IMeasureService.findHistoricalMeasure
      */
-    MeasureResult getHistoricalMeasure(HistoryStringKey _historyKey, LimitCriteria _limit);
+    @Deprecated
+    MeasureResult getHistoricalMeasure(HistoryStringKey _historyKey, PeriodCriteria _limit);
     
     /**
      * This method find the historical measures of a a list of KPIs on a list of entities
@@ -59,44 +62,46 @@ public interface IKpiAPI {
      * @param _limit
      *            the limit of the research by entity. As the number of result of the date range
      * @return a list of measures
+     * @deprecated use IMeasureService.findMupltipleHistoricalMeasure
      */
-    List<MeasureResult> getHistoricalMeasures(HistoryStringKeyList _historyKeys, LimitCriteria _limit);
-	/**
-	 * Returns the list of kpis for groups.
-	 * 
-	 * @param _kpis
-	 *            the list of kpis
-	 * @deprecated TO BE REFACTORED
-	 */
-	@Deprecated
-	List<Kpi> getKpisForGroups(List<Kpi> _kpis);
-
-	/**
-	 * @param _groupKpiKeys
-	 * @param _baseKpis
-	 * @return
-	 * @deprecated TO BE REFACTORED
-	 */
-	@Deprecated
-	Collection<? extends Kpi> getKpisOfGroupKpiKeys(List<String> _groupKpiKeys, List<Kpi> _baseKpis);
-
-	/**
-	 * Returns a selection of kpis filtered by their entity type and a list of
-	 * kpi keys.
-	 * 
-	 * @param _kpiType
-	 *            the kpi type
-	 * @param _kpiKeys
-	 *            the kpi keys
-	 * @return the list of kpi
-	 */
-	Collection<? extends Kpi> getSelectionOfKpis(List<String> _kpiKeys);
-
-	/**
-	 * Returns the list of KPI
-	 * 
-	 * @return the list of KPI.
-	 */
-	List<Kpi> selectAll();
-
+    @Deprecated
+    List<MeasureResult> getHistoricalMeasures(HistoryStringKeyList _historyKeys, PeriodCriteria _limit);
+    /**
+     * Returns the list of kpis for groups.
+     * 
+     * @param _kpis
+     *            the list of kpis
+     * @deprecated TO BE REFACTORED
+     */
+    @Deprecated
+    List<Kpi> getKpisForGroups(List<Kpi> _kpis);
+    
+    /**
+     * @param _groupKpiKeys
+     * @param _baseKpis
+     * @return
+     * @deprecated TO BE REFACTORED
+     */
+    @Deprecated
+    Collection<? extends Kpi> getKpisOfGroupKpiKeys(List<String> _groupKpiKeys, List<Kpi> _baseKpis);
+    
+    /**
+     * Returns a selection of kpis filtered by their entity type and a list of
+     * kpi keys.
+     * 
+     * @param _kpiType
+     *            the kpi type
+     * @param _kpiKeys
+     *            the kpi keys
+     * @return the list of kpi
+     */
+    Collection<? extends Kpi> getSelectionOfKpis(List<String> _kpiKeys);
+    
+    /**
+     * Returns the list of KPI
+     * 
+     * @return the list of KPI.
+     */
+    List<Kpi> selectAll();
+    
 }
