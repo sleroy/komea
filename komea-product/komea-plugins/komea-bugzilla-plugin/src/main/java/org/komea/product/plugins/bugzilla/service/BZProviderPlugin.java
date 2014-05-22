d38e942d200e3925c0eb1423e50da3ddfdc5535f
Sebastien Carreau <akira.seb@gmail.com>
2014-05-22 15:42:53 +0200

    Bugzilla kpis

--- komea-product/komea-plugins/komea-bugzilla-plugin/src/main/java/org/komea/product/plugins/bugzilla/service/BZProviderPlugin.java
+++ komea-product/komea-plugins/komea-bugzilla-plugin/src/main/java/org/komea/product/plugins/bugzilla/service/BZProviderPlugin.java
@@ -3,15 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.komea.product.plugins.bugzilla.service;
 
-
-
 import java.util.Arrays;
-
 import javax.annotation.PostConstruct;
-
 import org.komea.product.backend.api.IKPIService;
 import org.komea.product.backend.api.PluginAdminPages;
 import org.komea.product.backend.api.PluginMountPage;
@@ -32,12 +27,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-
 /**
  * This class defines the bugzilla provider plugin. Informations are collected
  * at runtime.
- * 
+ *
  * @author rgalerme
  * @version $Revision: 1.0 $
  */
@@ -47,107 +40,84 @@ import org.springframework.beans.factory.annotation.Autowired;
         icon = "bugzilla",
         url = "/bugzilla-provider",
         eventTypes = {})
-@PluginAdminPages(@PluginMountPage(
-        pluginName = BZProviderPlugin.BUGZILLA_PROVIDER_PLUGIN,
-        page = BugZillaPage.class))
-public class BZProviderPlugin
-{
-    
-    
-    public static final String  BUGZILLA_PROVIDER_PLUGIN = "BugZilla Provider plugin";
-    
-    private static final Logger LOGGER                   = LoggerFactory
-                                                                 .getLogger(BZProviderPlugin.class);
-    
+@PluginAdminPages(
+        @PluginMountPage(
+                pluginName = BZProviderPlugin.BUGZILLA_PROVIDER_PLUGIN,
+                page = BugZillaPage.class))
+public class BZProviderPlugin {
+
+    public static final String BUGZILLA_PROVIDER_PLUGIN = "BugZilla Provider plugin";
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(BZProviderPlugin.class);
+
     @Autowired
     private IBZConfigurationDAO bugZillaConfiguration;
-    
+
     @Autowired
-    private IEventTypeService   evenTypeService;
-    
+    private IEventTypeService evenTypeService;
+
     @Autowired
-    private IKPIService         kpiService;
-    
-    
-    
+    private IKPIService kpiService;
+
     public Kpi bzClosedBugs() {
-    
-    
-        BZBugCountKPI.create(Search.create(Filter.create("status", false, "new", "unconfirmed",
-                "onhold", "accepted", "assigned", "opened", "reopened")));
-        
         return bzKpi("bugs_status_closed", "Closed bugs", "Number of closed bugs",
                 ValueDirection.BETTER,
                 new GroovyScriptLoader("scripts/BzClosedBugScript.groovy").load());
     }
-    
-    
+
     public Kpi bzOpenBugs() {
-    
-    
+
         BZBugCountKPI.create(Search.create(Filter.create("status", false, "new", "unconfirmed",
                 "onhold", "accepted", "assigned", "opened", "reopened")));
-        
+
         return bzKpi("bugs_status_open", "Open bugs", "Number of open bugs", ValueDirection.WORST,
                 new GroovyScriptLoader("scripts/BzOpenBugScript.groovy").load());
     }
-    
-    
+
     public Kpi bzOpenBySeverityBugs(final String severity) {
-    
-    
-        final GroovyScriptLoader groovyScriptLoader =
-                new GroovyScriptLoader("scripts/BzOpenBySeverityBugs.groovy");
+
+        final GroovyScriptLoader groovyScriptLoader
+                = new GroovyScriptLoader("scripts/BzOpenBySeverityBugs.groovy");
         groovyScriptLoader.addParameter("#severity#", severity);
         return bzKpi("bugs_status_open_severity_" + severity, "Open " + severity + " bugs",
                 "Number of open bugs with " + severity + " severity", ValueDirection.WORST,
                 groovyScriptLoader.load());
     }
-    
-    
+
     public Kpi bzOpenNotFixedBugs() {
-    
-    
         return bzKpi("bugs_status_open_not_fixed", "Open NOT fixed bugs",
                 "Number of open but not fixed bugs", ValueDirection.WORST, new GroovyScriptLoader(
                         "scripts/BzOpenNotFixedBugs.groovy").load());
     }
-    
-    
+
     public Kpi bzTotalBugs() {
-    
-    
+
         return bzKpi("bugs_total", "Total bugs", "Total number of bugs", ValueDirection.WORST,
                 new GroovyScriptLoader("scripts/BzTotalBugs.groovy").load());
     }
-    
-    
+
     /**
      * @return the bugZillaConfiguration
      */
     public IBZConfigurationDAO getBugZillaConfiguration() {
-    
-    
+
         return bugZillaConfiguration;
     }
-    
-    
+
     /**
      * @return the evenTypeService
      */
     public IEventTypeService getEvenTypeService() {
-    
-    
+
         return evenTypeService;
     }
-    
-    
+
     @PostConstruct
     public void init() {
-    
-    
+
         LOGGER.info("Loading bugZilla plugin");
-        
+
         addKpi(bzTotalBugs());
         addKpi(bzOpenBugs());
         addKpi(bzOpenNotFixedBugs());
@@ -155,53 +125,43 @@ public class BZProviderPlugin
         for (final String severity : Arrays.asList("minor", "major", "critical", "blocker")) {
             addKpi(bzOpenBySeverityBugs(severity));
         }
-        
+
     }
-    
-    
+
     /**
-     * @param _bugZillaConfiguration
-     *            the bugZillaConfiguration to set
+     * @param _bugZillaConfiguration the bugZillaConfiguration to set
      */
     public void setBugZillaConfiguration(final IBZConfigurationDAO _bugZillaConfiguration) {
-    
-    
+
         bugZillaConfiguration = _bugZillaConfiguration;
     }
-    
-    
+
     /**
-     * @param _evenTypeService
-     *            the evenTypeService to set
+     * @param _evenTypeService the evenTypeService to set
      */
     public void setEvenTypeService(final IEventTypeService _evenTypeService) {
-    
-    
+
         evenTypeService = _evenTypeService;
     }
-    
-    
+
     private void addKpi(final Kpi _kpi) {
-    
-    
+
         if (!kpiService.exists(_kpi.getKpiKey())) {
             kpiService.saveOrUpdate(_kpi);
         }
     }
-    
-    
+
     private Kpi bzKpi(
             final String key,
             final String name,
             final String description,
             final ValueDirection valueDirection,
             final String formula) {
-    
-    
+
         return KpiBuilder.create().name(name).key(key).description(description)
                 .interval(0d, 100000d).dailyKPI().providerType(ProviderType.BUGTRACKER)
                 .queryScript(formula).forProject()
                 .produceValue(ValueType.INT, ValueDirection.BETTER).build();
-        
+
     }
 }
--- komea-product/komea-plugins/komea-bugzilla-plugin/src/main/resources/scripts/BzOpenBySeverityBugs.groovy
+++ komea-product/komea-plugins/komea-bugzilla-plugin/src/main/resources/scripts/BzOpenBySeverityBugs.groovy
@@ -2,6 +2,6 @@ import org.komea.product.plugins.bugzilla.core.*;
 import org.komea.product.backend.kpi.search.*;
 
 
-BZBugCountKPI.create(Search.create(Filter.create("status", false, "new",
+BZBugCountKPI.create(Search.create(Filter.create("status", true, "new",
              "unconfirmed", "onhold", "accepted", "assigned", "opened", "reopened"), Filter.create("severity", true,
              "#severity#")))
