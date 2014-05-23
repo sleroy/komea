0dda1979b9fe65d573d9864e7c88b5130fd6d69a
Sebastien Carreau <akira.seb@gmail.com>
2014-05-23 10:32:26 +0200

    jenkins kpi names

--- komea-product/komea-standard-kpis/src/main/java/org/komea/product/backend/service/standardkpi/JenkinsKPIService.java
+++ komea-product/komea-standard-kpis/src/main/java/org/komea/product/backend/service/standardkpi/JenkinsKPIService.java
@@ -1,13 +1,9 @@
 /**
- * 
+ *
  */
-
 package org.komea.product.backend.service.standardkpi;
 
-
-
 import javax.annotation.PostConstruct;
-
 import org.komea.product.backend.api.standardkpi.IStandardKpiService;
 import org.komea.product.backend.service.kpi.KpiBuilder;
 import org.komea.product.database.enums.EntityType;
@@ -25,40 +21,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-
 /**
  * @author sleroy
  */
 @Service
 @Transactional
-public class JenkinsKPIService
-{
-    
-    
+public class JenkinsKPIService {
+
     @Autowired
     public IStandardKpiService standardKpiBuilderService;
-    
-    
-    
+
     /**
      * @return
      */
     public Kpi healthRateOfUserActions() {
-    
-    
+
         return KpiBuilder.createAscending().nameAndKey("Project health influence")
                 .description("Project Health influence of a developer")
                 .providerType(ProviderType.CI_BUILD).entityType(EntityType.PERSON).dailyKPI()
                 .interval(-100d, 100d).query(ProjectBuildHealthInfluencePerUser.class).build();
-        
+
     }
-    
-    
+
     @PostConstruct
     public void initJenkinsKPI() {
-    
-    
+
         saveOrUpdate(numberOfConfigurationChanges());
         saveOrUpdate(numberSuccessBuildPerDay());
         saveOrUpdate(numberOfBuildPerDay());
@@ -68,117 +55,99 @@ public class JenkinsKPIService
         saveOrUpdate(numberOfFixedBuildPerUser());
         saveOrUpdate(jenkinsSuccessBuildRate());
         saveOrUpdate(healthRateOfUserActions());
-        
+
     }
-    
-    
+
     /**
      * @return
      */
     public Kpi jenkinsSuccessBuildRate() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("SBRJW")
+
+        return KpiBuilder.createAscending().nameAndKey("Successful builds (%)")
                 .description("Success project build rate").providerType(ProviderType.CI_BUILD)
                 .entityType(EntityType.PROJECT).dailyKPI().query(SuccessfulBuildRate.class)
                 .interval(0d, 100d).build();
-        
+
     }
-    
-    
+
     /**
      * @return
      */
     public Kpi numberOfBuildBrokenPerUser() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NBBPD")
+
+        return KpiBuilder.createAscending().nameAndKey("Broken builds / user")
                 .description("Number of broken builds per user ")
                 .providerType(ProviderType.CI_BUILD).entityType(EntityType.PROJECT).dailyKPI()
                 .interval(0d, 24 * 60 / 5d).query(NumberOfBrokenBuildPerUserPerDay.class).build();
-        
+
     }
-    
-    
+
     // KPI : Durée maximale de bon fonctionnement (tps entre deux builds success average)
     // KPI : Durée de non-fonctionnement (moyenne)
     // KPI : Durée de non-fonctionnement (maximale)
     public Kpi numberOfBuildPerDay() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NBPD").description("Number of build ")
+
+        return KpiBuilder.createAscending().nameAndKey("Builds").description("Number of build ")
                 .providerType(ProviderType.CI_BUILD).entityType(EntityType.PROJECT)
                 .query(BuildPerDay.class).dailyKPI().build();
-        
+
     }
-    
-    
+
     /**
      * @return
      */
     public Kpi numberOfConfigurationChanges() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NJCC")
+
+        return KpiBuilder.createAscending().nameAndKey("Job conf changes")
                 .description("Number of job configuration change")
                 .providerType(ProviderType.CI_BUILD).entityType(EntityType.PROJECT).dailyKPI()
                 .interval(0d, 60d).query(NumberOfJobConfigurationChangesPerWeek.class).build();
     }
-    
-    
+
     public Kpi numberOfFailedBuildPerDay() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NFBPD")
+
+        return KpiBuilder.createAscending().nameAndKey("Failed builds")
                 .description("Number of failed build ").providerType(ProviderType.CI_BUILD)
                 .entityType(EntityType.PROJECT).dailyKPI().query(NumberOfFailedBuildPerDay.class)
                 .interval(0d, 24 * 60 / 5d).build();
     }
-    
-    
+
     /**
      * @return
      */
     public Kpi numberOfFixedBuildPerUser() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NFBM")
+
+        return KpiBuilder.createAscending().nameAndKey("Fixed builds")
                 .description("Number of fixed builds").providerType(ProviderType.CI_BUILD)
                 .entityType(EntityType.PERSON).dailyKPI()
                 .query(NumberOfFixedBuildPerUserPerDay.class).build();
-        
+
     }
-    
-    
+
     /**
      * @return
      */
     public Kpi numberOfUnstableBuildPerDay() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NUBPD")
+
+        return KpiBuilder.createAscending().nameAndKey("Unstable builds")
                 .description("Number of unstable build").providerType(ProviderType.CI_BUILD)
                 .entityType(EntityType.PROJECT).dailyKPI().query(NumberOfFailedBuildPerDay.class)
                 .interval(0d, 24 * 60 / 5d).build();
     }
-    
-    
+
     public Kpi numberSuccessBuildPerDay() {
-    
-    
-        return KpiBuilder.createAscending().nameAndKey("NSBPD")
+
+        return KpiBuilder.createAscending().nameAndKey("Successful builds")
                 .description("Number of successful build ").providerType(ProviderType.CI_BUILD)
                 .entityType(EntityType.PROJECT).dailyKPI().interval(0d, 24 * 60 / 5d)
                 .query(NumberOfSuccessfulBuildPerDay.class).build();
     }
-    
-    
+
     /**
      * @param _numberSuccessBuildPerDay
      */
     public void saveOrUpdate(final Kpi _kpi) {
-    
-    
+
         standardKpiBuilderService.saveOrUpdate(_kpi);
     }
 }
