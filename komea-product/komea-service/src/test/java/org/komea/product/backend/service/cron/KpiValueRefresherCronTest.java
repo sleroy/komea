39227288aa07707131ee608956d0ab71ed925c45
Sylvain Leroy <sleroy0@gmail.com>
2014-05-23 10:59:58 +0200

    [cron, refresh] added cron to refresh values

---
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/cron/KpiValueRefresherCronTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+
+package org.komea.product.backend.service.cron;
+
+
+
+import org.junit.Test;
+import org.komea.product.backend.api.IKPIService;
+import org.komea.product.backend.service.kpi.IStatisticsAPI;
+import org.komea.product.test.spring.AbstractSpringIntegrationTestCase;
+import org.quartz.JobExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.mockito.Mockito.mock;
+
+
+
+/**
+ * @author sleroy
+ */
+public class KpiValueRefresherCronTest extends AbstractSpringIntegrationTestCase
+{
+    
+    
+    @Autowired
+    private IKPIService    kpiService;
+    @Autowired
+    private IStatisticsAPI statisticsAPI;
+    
+    
+    
+    /**
+     * Test method for {@link org.komea.product.backend.service.cron.KpiValueRefresherCron#execute(org.quartz.JobExecutionContext)}.
+     */
+    @Test
+    public final void testExecute() throws Exception {
+    
+    
+        final KpiValueRefresherCron kpiValueRefresherCron = new KpiValueRefresherCron();
+        kpiValueRefresherCron.setKpiService(kpiService);
+        kpiValueRefresherCron.setStatisticsAPI(statisticsAPI);
+        kpiValueRefresherCron.execute(mock(JobExecutionContext.class));
+        
+    }
+    
+}
--- komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/LayoutPage.java
+++ komea-product/komea-wicket-utils/src/main/java/org/komea/product/wicket/LayoutPage.java
@@ -1,11 +1,8 @@
+
 package org.komea.product.wicket;
 
-import com.googlecode.wicket.jquery.core.JQueryBehavior;
-import com.googlecode.wicket.jquery.core.Options;
-import com.googlecode.wicket.jquery.ui.widget.accordion.AccordionPanel;
-import com.googlecode.wicket.jquery.ui.widget.tabs.AjaxTab;
-import com.googlecode.wicket.jquery.ui.widget.tabs.SimpleTab;
-import static groovyjarjarantlr.Utils.error;
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -35,37 +32,47 @@ import org.komea.product.wicket.widget.RedirectPageLink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.googlecode.wicket.jquery.core.Options;
+import com.googlecode.wicket.jquery.ui.widget.accordion.AccordionPanel;
+
+
+
 /**
  * Defines the general layout for all komea pages.
- *
+ * 
  * @author sleroy
  */
-public abstract class LayoutPage extends WebPage {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(LayoutPage.class);
-
-    private static final long serialVersionUID = 1L;
+public abstract class LayoutPage extends WebPage
+{
     
-    protected AccordionPanel accordion;
     
-    public final static int COMPANY_INDEX=0;
-    public final static int KPIS_INDEX=1;
-    public final static int ADMIN_INDEX=2;
-
-//    protected final JQueryBehavior jQueryBehavior;
+    public final static int     ADMIN_INDEX      = 2;
+    
+    public final static int     COMPANY_INDEX    = 0;
+    
+    public final static int     KPIS_INDEX       = 1;
+    
+    private static final Logger LOGGER           = LoggerFactory.getLogger(LayoutPage.class);
+    private static final long   serialVersionUID = 1L;
+    protected AccordionPanel    accordion;
+    
+    
+    
+    // protected final JQueryBehavior jQueryBehavior;
     public LayoutPage(final PageParameters _parameters) {
-
+    
+    
         super(_parameters);
         add(new Label("page_title", Model.of(getTitle())));
-
-//         jQueryBehavior = new JQueryBehavior("#accordion", "accordion");
-//        this.add(jQueryBehavior);
+        
+        // jQueryBehavior = new JQueryBehavior("#accordion", "accordion");
+        // this.add(jQueryBehavior);
         addAccordingPanel(this);
-
-        final KomeaSecurityContextHolderAwareRequestWrapper securityContextHolderAwareRequestWrapper
-                = new KomeaSecurityContextHolderAwareRequestWrapper((ServletRequest) getRequest()
+        
+        final KomeaSecurityContextHolderAwareRequestWrapper securityContextHolderAwareRequestWrapper =
+                new KomeaSecurityContextHolderAwareRequestWrapper((ServletRequest) getRequest()
                         .getContainerRequest(), "");
-
+        
         if (securityContextHolderAwareRequestWrapper.isUserInRole("ADMIN")) {
             add(new WebMarkupContainer("signinpanel"));
             add(new Fragment("personalpanel", "personal", this));
@@ -78,73 +85,16 @@ public abstract class LayoutPage extends WebPage {
         // DEBUG MODE
         checkStateless();
         
-
-    }
-
-    private void addAccordingPanel(final LayoutPage page) {
-
-        // Recommended options when using dynamic content (AjaxTab) //
-        Options options = new Options();
-        options.set("heightStyle", Options.asString("content"));
-
-        // Accordion //
-       accordion = new AccordionPanel("accordion", this.newTabList(page), options) {
-            
-//            target.add(accordion.setActiveTab(accordion.getLastTabIndex()));
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void onActivate(AjaxRequestTarget target, int index, ITab tab) {
-            }
-        };
         
-        page.add(accordion);
     }
-
-    private List<ITab> newTabList(final LayoutPage page) {
-        List<ITab> tabs = new ArrayList<ITab>();
-        // tab #3, using AbstractTab //
-        AbstractTab tab1 = new AbstractTab(Model.of(getString("home.company"))) {
-            
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public WebMarkupContainer getPanel(String panelId) {
-                return new Fragment(panelId, "panel-1", page);
-            }
-        };
-        
-        tabs.add(tab1);
-
-        tabs.add(new AbstractTab(Model.of(getString("home.kpis"))) {
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public WebMarkupContainer getPanel(String panelId) {
-                return new Fragment(panelId, "panel-2", page);
-            }
-        });
-
-        tabs.add(new AbstractTab(Model.of(getString("home.administration"))) {
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public WebMarkupContainer getPanel(String panelId) {
-                return new Fragment(panelId, "panel-3", page);
-            }
-        });
-
-        return tabs;
-    }
-
+    
+    
     /**
      * Check if the page is stateless
      */
     public void checkStateless() {
-
+    
+    
         if (!isPageStateless()) {
             final Iterator<Component> iterator = this.iterator();
             while (iterator.hasNext()) {
@@ -156,69 +106,170 @@ public abstract class LayoutPage extends WebPage {
             }
         }
     }
-
+    
+    
     /**
      * Returns the bread crumb.
-     *
+     * 
      * @return the breadcrumbs
      */
     @SuppressWarnings("rawtypes")
     public final List<? extends Entry<String, Class>> getBreadCrumbs() {
-
+    
+    
         final ArrayList<Entry<String, Class>> arrayList = new ArrayList<Entry<String, Class>>();
         arrayList.add(new KomeaEntry<String, Class>("Home", HomePage.class));
         arrayList.addAll(getMiddleLevelPages());
         arrayList.add(new KomeaEntry<String, Class>(getTitle(), getClass()));
         return arrayList;
     }
-
+    
+    
     /**
      * Returns the page links in breadcrumb between home and active page.
-     *
+     * 
      * @return the list of pages
      */
     public List<? extends Entry<String, Class>> getMiddleLevelPages() {
-
+    
+    
         return Collections.emptyList();
     }
-
+    
+    
     /**
      * Provides the title to show in the panel (H1) markup.
-     *
+     * 
      * @return the title.
      */
     public String getTitle() {
-
+    
+    
         return getString("layout.title");
     }
-
+    
+    
     @Override
     public void renderHead(final IHeaderResponse response) {
-
+    
+    
         super.renderHead(response);
-
+        
         response.render(JavaScriptHeaderItem.forReference(getApplication()
                 .getJavaScriptLibrarySettings().getJQueryReference()));
     }
-
+    
+    
+    private void addAccordingPanel(final LayoutPage page) {
+    
+    
+        // Recommended options when using dynamic content (AjaxTab) //
+        final Options options = new Options();
+        options.set("heightStyle", Options.asString("content"));
+        
+        // Accordion //
+        accordion = new AccordionPanel("accordion", newTabList(page), options)
+        {
+            
+            
+            // target.add(accordion.setActiveTab(accordion.getLastTabIndex()));
+            
+            private static final long serialVersionUID = 1L;
+            
+            
+            
+            @Override
+            public void onActivate(final AjaxRequestTarget target, final int index, final ITab tab) {
+            
+            
+            }
+        };
+        
+        page.add(accordion);
+    }
+    
+    
     private void buildBreadCrumb() {
-
+    
+    
         final List<Entry<String, Class>> breadPath = (List<Entry<String, Class>>) getBreadCrumbs();
         final List<Entry<String, Class>> notActivePages = new ArrayList();
         for (int i = 0, ni = breadPath.size() - 1; i < ni; ++i) {
             notActivePages.add(breadPath.get(i));
         }
         breadPath.get(breadPath.size() - 1);
-
-        add(new ListView<Entry<String, Class>>("bread", notActivePages) {
-
+        
+        add(new ListView<Entry<String, Class>>("bread", notActivePages)
+        {
+            
+            
             @Override
             protected void populateItem(final ListItem<Entry<String, Class>> _item) {
-
+            
+            
                 _item.add(new RedirectPageLink("blink", _item.getModelObject()));
             }
-
+            
         });
         add(new BookmarkablePageLink<Void>("breadactive", getPageClass()));
+    }
+    
+    
+    private List<ITab> newTabList(final LayoutPage page) {
+    
+    
+        final List<ITab> tabs = new ArrayList<ITab>();
+        // tab #3, using AbstractTab //
+        final AbstractTab tab1 = new AbstractTab(Model.of(getString("home.company")))
+        {
+            
+            
+            private static final long serialVersionUID = 1L;
+            
+            
+            
+            @Override
+            public WebMarkupContainer getPanel(final String panelId) {
+            
+            
+                return new Fragment(panelId, "panel-1", page);
+            }
+        };
+        
+        tabs.add(tab1);
+        
+        tabs.add(new AbstractTab(Model.of(getString("home.kpis")))
+        {
+            
+            
+            private static final long serialVersionUID = 1L;
+            
+            
+            
+            @Override
+            public WebMarkupContainer getPanel(final String panelId) {
+            
+            
+                return new Fragment(panelId, "panel-2", page);
+            }
+        });
+        
+        tabs.add(new AbstractTab(Model.of(getString("home.administration")))
+        {
+            
+            
+            private static final long serialVersionUID = 1L;
+            
+            
+            
+            @Override
+            public WebMarkupContainer getPanel(final String panelId) {
+            
+            
+                return new Fragment(panelId, "panel-3", page);
+            }
+        });
+        
+        return tabs;
     }
 }
