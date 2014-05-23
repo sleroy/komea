6f3ebbae2b387946499c32b6153f3773161db9d7
Sylvain Leroy <sleroy0@gmail.com>
2014-05-23 13:16:47 +0200

    [backup-event-stats] better logging

--- komea-product/komea-service/src/main/java/org/komea/product/backend/service/esper/stats/EventStatsCron.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/service/esper/stats/EventStatsCron.java
@@ -1,7 +1,10 @@
 /**
  *
  */
+
 package org.komea.product.backend.service.esper.stats;
+
+
 
 import org.joda.time.DateTime;
 import org.komea.product.backend.service.esper.IEventStatisticsService;
@@ -14,24 +17,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+
 /**
  * @author sleroy
  */
 @DisallowConcurrentExecution
-public class EventStatsCron implements Job {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(EventStatsCron.class);
-
+public class EventStatsCron implements Job
+{
+    
+    
+    private static final Logger     LOGGER = LoggerFactory.getLogger(EventStatsCron.class);
+    
     @Autowired
     private IEventStatisticsService service;
-
+    
+    
+    
     /*
      * (non-Javadoc)
      * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
      */
     @Override
     public void execute(final JobExecutionContext _context) throws JobExecutionException {
-
+    
+    
         LOGGER.info("Backup of alert stats...");
         final long receivedAlertsIn24LastHours = service.getReceivedAlertsIn24LastHours();
         final Measure measure = new Measure();
@@ -39,17 +49,22 @@ public class EventStatsCron implements Job {
         measure.setIdKpi("-1");
         measure.setValue(new Double(receivedAlertsIn24LastHours));
         service.putHistory(new DateTime().hourOfDay().get(), measure);
-
+        LOGGER.info("Backup of alert stats finished...");
+        
     }
-
+    
+    
     public IEventStatisticsService getService() {
-
+    
+    
         return service;
     }
-
+    
+    
     public void setService(final IEventStatisticsService _service) {
-
+    
+    
         service = _service;
     }
-
+    
 }
