aedd1c702e92e5c5a952b5305278d7525f0c3b15
jguidoux <jeremie.guidoux@tocea.com>
2014-06-03 14:37:04 +0200

    * add rest apo to fond goals of a akpi on an entity

--- komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/IKpiGoalService.java
+++ komea-product/komea-service-api/src/main/java/org/komea/product/backend/service/IKpiGoalService.java
@@ -5,7 +5,6 @@
 package org.komea.product.backend.service;
 
 
-
 import java.util.List;
 
 import org.komea.product.backend.service.generic.IGenericService;
@@ -13,14 +12,10 @@ import org.komea.product.database.model.KpiGoal;
 import org.komea.product.database.model.KpiGoalCriteria;
 import org.komea.product.service.dto.KpiKey;
 
-
-
 /**
  * @author sleroy
  */
-public interface IKpiGoalService extends IGenericService<KpiGoal, Integer, KpiGoalCriteria>
-{
-    
+public interface IKpiGoalService extends IGenericService<KpiGoal, Integer, KpiGoalCriteria> {
     
     /**
      * Returns a list of kpi goals for a given entity and a kpi.
--- komea-product/komea-testspring/src/main/java/org/komea/product/test/spring/AbstractSpringWebIntegrationTestCase.java
+++ komea-product/komea-testspring/src/main/java/org/komea/product/test/spring/AbstractSpringWebIntegrationTestCase.java
@@ -2,15 +2,12 @@
 package org.komea.product.test.spring;
 
 
-
 import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-
 
 /**
  * This class defines the abstract class to perform integration tests on Spring
@@ -21,14 +18,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = {
-        "classpath:/spring/application-context-test.xml",
-        "classpath*:/spring/dispatcher-servlet-test.xml" })
+        "classpath:/spring/application-context-test.xml", "classpath*:/spring/dispatcher-servlet-test.xml" })
 @TransactionConfiguration(defaultRollback = true)
-public abstract class AbstractSpringWebIntegrationTestCase
-{
-    
+public abstract class AbstractSpringWebIntegrationTestCase {
     
     protected final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(getClass());
-    
     
 }
