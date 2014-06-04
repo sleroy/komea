67b94fb665bfe889aae11199b60c09fd92a86a2a
RaphaelTocea <rgalerme@Tera>
2014-06-04 11:48:22 +0200

    add modif cron

--- komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/cronpage/CronDialog.java
+++ komea-product/komea-admin-webgui/src/main/java/org/komea/product/wicket/cronpage/CronDialog.java
@@ -5,22 +5,22 @@
  */
 package org.komea.product.wicket.cronpage;
 
-import com.googlecode.wicket.jquery.ui.form.RadioChoice;
 import com.googlecode.wicket.jquery.ui.widget.dialog.AbstractFormDialog;
 import com.googlecode.wicket.jquery.ui.widget.dialog.DialogButton;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.wicket.Component;
+import static org.apache.wicket.Component.PARENT_PATH;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
-import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.IModel;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
 import org.komea.product.backend.service.cron.CronDetails;
-import org.komea.product.wicket.utils.DialogFactory;
+import org.komea.product.backend.service.cron.CronUtils;
+import org.komea.product.backend.service.cron.ICronRegistryService;
 import org.komea.product.wicket.utils.NameGeneric;
 import org.komea.product.wicket.widget.builders.TextFieldBuilder;
 
@@ -32,23 +32,42 @@ public abstract class CronDialog extends AbstractFormDialog<String> {
 
     private final Form form;
     private final NameGeneric cronValue;
-    private final Component cronField;
+    private final TextField<String> cronField;
     private CronDetails cronObject;
+    private final FeedbackPanel feedbackPanel;
 
     protected final DialogButton btnCancel = new DialogButton("Cancel");
     protected final DialogButton btnSelect = new DialogButton("Save"); // with a customized text
 
     public CronDialog(String id, String title) {
         super(id, title, new Model<String>(), true);
+        feedbackPanel = new FeedbackPanel("feedback");
+        feedbackPanel.setOutputMarkupId(true);
+        feedbackPanel.setOutputMarkupPlaceholderTag(true);
+        add(feedbackPanel);
+        feedbackPanel.setVisible(false);
         form = new Form<String>("formCron");
         cronValue = new NameGeneric();
-        cronField = TextFieldBuilder.<String>createRequired("name", cronValue, "name").highlightOnErrors()
-                .simpleValidator(0, 255).withTooltip(getString("cronpage.form.dialog.cronfield.tooltip")).build();
+        cronField = TextFieldBuilder.<String>createRequired("cron", cronValue, "name").highlightOnErrors()
+                .simpleValidator(0, 255).withTooltip(getString("cronpage.form.dialog.cronfield.tooltip")).buildTextField();
+        cronField.add(new IValidator<String>() {
+
+            @Override
+            public void validate(IValidatable<String> iv) {
+                String value = iv.getValue();
+                if (!CronUtils.isValidCronExpression(value)) {
+                    final ValidationError error = new ValidationError();
+                    error.setMessage(getString("cronpage.form.dialog.error.invalidexpression"));
+                    iv.error(error);
+                }
+
+            }
+        });
         cronField.setOutputMarkupId(true);
         cronField.setOutputMarkupPlaceholderTag(true);
         form.add(cronField);
-        CronDialogForm cronDialogForm = new CronDialogForm("formmakecron");
-        form.add(cronDialogForm);
+        CronDialogForm cronDialogForm = new CronDialogForm("formmakecron", this, feedbackPanel);
+        add(cronDialogForm);
         add(form);
 
     }
@@ -56,6 +75,11 @@ public abstract class CronDialog extends AbstractFormDialog<String> {
     public void setCronValue(CronDetails value, AjaxRequestTarget art) {
         cronObject = value;
         cronValue.setName(value.getCronExpression());
+        art.add(cronField);
+    }
+
+    public void setCronValue(String value, AjaxRequestTarget art) {
+        cronValue.setName(value);
         art.add(cronField);
     }
 
@@ -91,27 +115,8 @@ public abstract class CronDialog extends AbstractFormDialog<String> {
 
     @Override
     protected void onError(AjaxRequestTarget art) {
-        // 
-    }
-
-    private static class AjaxLinkImpl extends AjaxLink {
-
-        private final NameGeneric cronValue;
-        private final Component composant;
-        private final String btnValue;
-
-        public AjaxLinkImpl(String id, NameGeneric _cronValue, Component _composant, String _btnvalue) {
-            super(id);
-            cronValue = _cronValue;
-            composant = _composant;
-            btnValue = _btnvalue;
-
-        }
-
-        @Override
-        public void onClick(AjaxRequestTarget art) {
-
-        }
+        feedbackPanel.setVisible(true);
+        art.add(feedbackPanel);
     }
 
 }
