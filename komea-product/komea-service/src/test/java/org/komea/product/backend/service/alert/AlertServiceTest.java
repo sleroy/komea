71f832152b8867ef3ff0ffbffdbd8d386fde195a
jguidoux <jeremie.guidoux@tocea.com>
2014-06-05 11:15:43 +0200

    * [SERVICE] activate test in AlertServiceTest

--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/alert/AlertServiceTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/alert/AlertServiceTest.java
@@ -5,9 +5,7 @@
 package org.komea.product.backend.service.alert;
 
 
-
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.komea.product.database.dto.KpiAlertDto;
 import org.komea.product.database.dto.SearchKpiAlertsDto;
@@ -16,19 +14,12 @@ import org.komea.product.database.enums.Operator;
 import org.komea.product.database.enums.Severity;
 import org.komea.product.database.model.KpiAlertType;
 
-
-
 /**
  * @author sleroy
  */
-@Ignore
-public class AlertServiceTest
-{
-    
+public class AlertServiceTest {
     
     private final IAlertService alertService = new AlertService();
-    
-    
     
     /**
      * Test method for
@@ -37,7 +28,6 @@ public class AlertServiceTest
      */
     @Test
     public void testIs_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_filtered() throws Exception {
-    
     
         final SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
         filter.setActivatedOnly(true);
@@ -54,10 +44,8 @@ public class AlertServiceTest
         
     }
     
-    
     @Test
     public void testIs_not_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_filtered() throws Exception {
-    
     
         final SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
         filter.setActivatedOnly(true);
@@ -74,11 +62,8 @@ public class AlertServiceTest
         
     }
     
-    
     @Test
-    public void testIs_not_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_with_no_activated_filtered()
-            throws Exception {
-    
+    public void testIs_not_activeted_Alert_COVERAGE_BRANCH_TOO_LOW_with_no_activated_filtered() throws Exception {
     
         final SearchKpiAlertsDto filter = new SearchKpiAlertsDto();
         filter.setActivatedOnly(false);
@@ -95,47 +80,8 @@ public class AlertServiceTest
         
     }
     
-    
-    // @Mock
-    // private IAlertTypeService alertTypeService;
-    //
-    // @Mock
-    // private IEntityService entityService;
-    //
-    // @Mock
-    // private IKPIService kpiService;
-    // @Mock
-    // private IHistoryService measureService;
-    @Test
-    public final void testIsAlert_definition_not_activated() throws Exception {
-    
-    
-        // GIVEN an alert is launch when branch coverage is under 60%
-        final KpiAlertType alertType = new KpiAlertType();
-        alertType.setKpiAlertKey("COVERAGE_BRANCH_TOO_LOW");
-        alertType.setIdKpi(1);
-        alertType.setId(1);
-        alertType.setOperator(Operator.LESSER);
-        alertType.setValue(60D);
-        alertType.setName("branch coverage too small");
-        alertType.setDescription("An alert is launched when the Branche coverage is < 60");
-        alertType.setSeverity(Severity.CRITICAL);
-        // AND the alert definition is not activated
-        alertType.setEnabled(false);
-        // AND The current coverage is 5
-        final int coverage = 5;
-        
-        // WHEN we test if the alert is activated
-        final boolean activated = alertService.isAlertActivated(alertType, coverage);
-        
-        // The alert must be activate
-        Assert.assertFalse(activated);
-    }
-    
-    
     @Test
     public final void testIsAlert_not_activated() throws Exception {
-    
     
         // GIVEN an alert is launch when branch coverage is under 60%
         final KpiAlertType alertType = new KpiAlertType();
@@ -159,35 +105,4 @@ public class AlertServiceTest
         Assert.assertFalse(activated);
     }
     
-    
-    /**
-     * Test method for
-     * {@link org.komea.product.backend.service.alert.AlertService#isAlertActivated(org.komea.product.database.model.KpiAlertType, java.lang.Number)}
-     * .
-     */
-    @Test
-    public final void testIsAlertActivated() throws Exception {
-    
-    
-        // GIVEN an alert is launch when branch coverage is under 60%
-        final KpiAlertType alertType = new KpiAlertType();
-        alertType.setKpiAlertKey("COVERAGE_BRANCH_TOO_LOW");
-        alertType.setIdKpi(1);
-        alertType.setId(1);
-        alertType.setOperator(Operator.LESSER);
-        alertType.setValue(60D);
-        alertType.setName("branch coverage too small");
-        alertType.setDescription("An alert is launched when the Branche coverage is < 60");
-        alertType.setSeverity(Severity.CRITICAL);
-        // AND the alert definition is activated
-        alertType.setEnabled(true);
-        // AND The current coverage is 5
-        final int coverage = 5;
-        
-        // WHEN we test if the alert is activated
-        final boolean activated = alertService.isAlertActivated(alertType, coverage);
-        
-        // The alert must be activate
-        Assert.assertTrue(activated);
-    }
 }
