0ccfc3c013071ceaec88d23fc404a670ec74f71c
Sylvain Leroy <sleroy0@gmail.com>
2014-06-09 21:32:14 +0200

    [test] upgraded unit tests

--- komea-product/komea-dto/src/test/java/org/komea/product/plugins/datasource/PluginDataCustomFieldsTest.java
+++ komea-product/komea-dto/src/test/java/org/komea/product/plugins/datasource/PluginDataCustomFieldsTest.java
@@ -1,20 +1,72 @@
 /**
- * 
+ *
  */
+
 package org.komea.product.plugins.datasource;
 
+
+
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 
 
 /**
  * @author sleroy
- *
  */
 public class PluginDataCustomFieldsTest
 {
     
     
+    public static class A
+    {
+        
+        
+        private String p1;
+        private String p2;
+        public String getP1() {
+        
+        
+            return p1;
+        }
+        public void setP1(String _p1) {
+        
+        
+            p1 = _p1;
+        }
+        public String getP2() {
+        
+        
+            return p2;
+        }
+        public void setP2(String _p2) {
+        
+        
+            p2 = _p2;
+        }
+    }
+    
+    
+    
+    private PluginDataCustomFields dataCustomFields;
+    
+    
+    
+    @Before
+    public void before() {
+
+
+        dataCustomFields = new PluginDataCustomFields();
+    }
+
+
     /**
      * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#containsField(java.lang.String)}.
      */
@@ -22,10 +74,13 @@ public class PluginDataCustomFieldsTest
     public final void testContainsField() throws Exception {
     
     
-        throw new RuntimeException("not yet implemented");
+        assertFalse(dataCustomFields.containsField("p1"));
+        
+        dataCustomFields.put("p1", "v");
+        assertTrue(dataCustomFields.containsField("p1"));
     }
-    
-    
+
+
     /**
      * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#getField(java.lang.String)}.
      */
@@ -33,7 +88,9 @@ public class PluginDataCustomFieldsTest
     public final void testGetField() throws Exception {
     
     
-        throw new RuntimeException("not yet implemented");
+        assertNotNull(dataCustomFields.getField("p1"));
+        dataCustomFields.put("p1", "v");
+        assertEquals("v", dataCustomFields.getField("p1"));
     }
     
     
@@ -42,9 +99,13 @@ public class PluginDataCustomFieldsTest
      */
     @Test
     public final void testGetFieldsAsMap() throws Exception {
-    
-    
-        throw new RuntimeException("not yet implemented");
+
+
+        dataCustomFields.put("p1", "v");
+        dataCustomFields.put("p2", "v2");
+        final Map<String, String> asMap = dataCustomFields.getFieldsAsMap();
+        assertEquals("v", asMap.get("p1"));
+        assertEquals("v2", asMap.get("p2"));
     }
     
     
@@ -55,18 +116,13 @@ public class PluginDataCustomFieldsTest
     public final void testGetPojo() throws Exception {
     
     
-        throw new RuntimeException("not yet implemented");
+        dataCustomFields.put("p1", "v");
+        dataCustomFields.put("p2", "v2");
+        final A a = dataCustomFields.getPojo(A.class);
+        assertEquals("v", a.getP1());
+        assertEquals("v2", a.getP2());
+        
     }
     
-    
-    /**
-     * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#put(java.lang.String, java.lang.String)}.
-     */
-    @Test
-    public final void testPut() throws Exception {
-    
-    
-        throw new RuntimeException("not yet implemented");
-    }
     
 }
