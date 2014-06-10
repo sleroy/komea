27517652f93a3184d58ea07cb3274a58224219f6
Sylvain Leroy <sleroy0@gmail.com>
2014-06-10 14:52:51 +0200

    [regressions] fixed regressions with merge

--- komea-product/komea-service/src/test/java/org/komea/product/backend/utils/exemples/kpi/LineCoverageKPI.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/utils/exemples/kpi/LineCoverageKPI.java
@@ -2,6 +2,7 @@
 package org.komea.product.backend.utils.exemples.kpi;
 
 
+
 import org.komea.eventory.api.cache.BackupDelay;
 import org.komea.eventory.api.engine.IDynamicDataQuery;
 import org.komea.product.backend.groovy.AbstractGroovyQuery;
@@ -10,36 +11,48 @@ import org.komea.product.database.dto.KpiResult;
 import org.komea.product.database.enums.EntityType;
 import org.komea.product.service.dto.EntityKey;
 
-public class LineCoverageKPI extends AbstractGroovyQuery implements IEntityKpiFormula, IDynamicDataQuery<KpiResult> {
-    
+
+
+public class LineCoverageKPI extends AbstractGroovyQuery implements IEntityKpiFormula,
+IDynamicDataQuery<KpiResult>
+{
+
+
     public LineCoverageKPI() {
-    
-        super(EntityType.PROJECT);
+
+
+        super();
     }
-    
+
+
     @Override
     public Number evaluate(final EntityKey _entityKey) {
-    
+
+
         return 25;
     }
-    
+
+
     /*
      * (non-Javadoc)
      * @see org.komea.eventory.api.engine.IQuery#getBackupDelay()
      */
     @Override
     public BackupDelay getBackupDelay() {
-    
+
+
         return BackupDelay.DAY;
     }
-    
+
+
     /*
      * (non-Javadoc)
      * @see org.komea.eventory.api.engine.IQuery#getResult()
      */
     @Override
     public KpiResult getResult() {
-    
-        return forEachEntity(this);
+
+
+        return forEachEntity(EntityType.PROJECT, this);
     }
 }
