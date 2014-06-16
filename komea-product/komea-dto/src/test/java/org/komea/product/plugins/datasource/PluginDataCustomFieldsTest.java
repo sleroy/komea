9a54ecc5a4570926f230a31ae966ec1ff4d4f120
Sylvain Leroy <sleroy0@gmail.com>
2014-06-16 09:26:46 +0200

    [plugin] custom data fields

--- komea-product/komea-dto/src/test/java/org/komea/product/plugins/datasource/PluginDataCustomFieldsTest.java
+++ komea-product/komea-dto/src/test/java/org/komea/product/plugins/datasource/PluginDataCustomFieldsTest.java
@@ -9,12 +9,11 @@ package org.komea.product.plugins.datasource;
 import java.io.Serializable;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -24,57 +23,45 @@ import static org.junit.Assert.assertTrue;
  */
 public class PluginDataCustomFieldsTest
 {
-    
-    
+
+
     public static class A
     {
-        
-        
+
+
         private String p1;
         private String p2;
-        
-        
-        
+
+
+
         public String getP1() {
-        
-        
+
+
             return p1;
         }
-        
-        
+
+
         public String getP2() {
-        
-        
+
+
             return p2;
         }
-        
-        
+
+
         public void setP1(final String _p1) {
-        
-        
+
+
             p1 = _p1;
         }
-        
-        
+
+
         public void setP2(final String _p2) {
-        
-        
+
+
             p2 = _p2;
         }
     }
     
-    
-    
-    private PluginDataCustomFields dataCustomFields;
-    
-    
-    
-    @Before
-    public void before() {
-    
-    
-        dataCustomFields = new PluginDataCustomFields();
-    }
     
     
     /**
@@ -84,13 +71,14 @@ public class PluginDataCustomFieldsTest
     public final void testContainsField() throws Exception {
     
     
+        final PluginDataCustomFields dataCustomFields = new PluginDataCustomFields();
         assertFalse(dataCustomFields.containsField("p1"));
-        
+
         dataCustomFields.put("p1", "v");
         assertTrue(dataCustomFields.containsField("p1"));
     }
-    
-    
+
+
     /**
      * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#getField(java.lang.String)}.
      */
@@ -98,12 +86,14 @@ public class PluginDataCustomFieldsTest
     public final void testGetField() throws Exception {
     
     
-        assertNotNull(dataCustomFields.getField("p1"));
-        dataCustomFields.put("p1", "v");
-        assertEquals("v", dataCustomFields.getField("p1"));
+        final PluginDataCustomFields dataCustomFields = new PluginDataCustomFields();
+        
+        assertNull(dataCustomFields.getField("p2"));
+        dataCustomFields.put("p2", "v");
+        assertEquals("v", dataCustomFields.getField("p2"));
     }
-    
-    
+
+
     /**
      * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#getFieldsAsMap()}.
      */
@@ -111,14 +101,16 @@ public class PluginDataCustomFieldsTest
     public final void testGetFieldsAsMap() throws Exception {
     
     
+        final PluginDataCustomFields dataCustomFields = new PluginDataCustomFields();
+        
         dataCustomFields.put("p1", "v");
         dataCustomFields.put("p2", "v2");
         final Map<String, Serializable> asMap = dataCustomFields.getFieldsAsMap();
         assertEquals("v", asMap.get("p1"));
         assertEquals("v2", asMap.get("p2"));
     }
-    
-    
+
+
     /**
      * Test method for {@link org.komea.product.plugins.datasource.PluginDataCustomFields#getPojo(java.lang.Class)}.
      */
@@ -126,13 +118,15 @@ public class PluginDataCustomFieldsTest
     public final void testGetPojo() throws Exception {
     
     
+        final PluginDataCustomFields dataCustomFields = new PluginDataCustomFields();
+        
         dataCustomFields.put("p1", "v");
         dataCustomFields.put("p2", "v2");
         final A a = dataCustomFields.getPojo(A.class);
         assertEquals("v", a.getP1());
         assertEquals("v2", a.getP2());
-        
+
     }
-    
-    
+
+
 }
