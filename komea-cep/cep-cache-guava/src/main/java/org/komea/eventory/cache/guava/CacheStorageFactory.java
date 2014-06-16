e0595d500bbf64a2b0937d4d8851a7357509aa2a
Sylvain Leroy <sleroy0@gmail.com>
2014-06-16 09:54:02 +0200

    [guava] removed jdk 7 syntax

--- komea-cep/cep-cache-guava/src/main/java/org/komea/eventory/cache/guava/CacheStorageFactory.java
+++ komea-cep/cep-cache-guava/src/main/java/org/komea/eventory/cache/guava/CacheStorageFactory.java
@@ -14,17 +14,17 @@ import org.komea.eventory.api.cache.ICacheStorageFactory;
  */
 public class CacheStorageFactory implements ICacheStorageFactory
 {
-
-
+    
+    
     /*
      * (non-Javadoc)
      * @see org.komea.eventory.api.cache.ICacheStorageFactory#newCacheStorage(org.komea.product.cep.api.cache.ICacheConfiguration)
      */
     @Override
     public ICacheStorage newCacheStorage(final ICacheConfiguration _cacheConfiguration) {
-    
-    
-        return new GoogleCacheStorage<>(_cacheConfiguration);
+
+
+        return new GoogleCacheStorage(_cacheConfiguration);
     }
-    
+
 }
