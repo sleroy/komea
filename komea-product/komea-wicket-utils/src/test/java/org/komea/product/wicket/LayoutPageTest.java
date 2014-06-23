2e432f85c464eaa07339a0829c58c31989d01888
Sylvain Leroy <sleroy0@gmail.com>
2014-06-23 08:01:02 +0200

    [admin, kpi debug] relooking of tables

--- komea-product/komea-wicket-utils/src/test/java/org/komea/product/wicket/LayoutPageTest.java
+++ komea-product/komea-wicket-utils/src/test/java/org/komea/product/wicket/LayoutPageTest.java
@@ -25,72 +25,85 @@ import static org.mockito.Mockito.when;
  */
 public class LayoutPageTest
 {
-
-
+    
+    
     public static class A extends LayoutPage
     {
-
-
+        
+        
         public A() {
-
-
+        
+        
             super(new PageParameters());
         }
-
-
+        
+        
         /**
          * @param _parameters
          */
         public A(final PageParameters _parameters) {
-
-
+        
+        
             super(_parameters);
         }
-
-
+        
+        
         /*
          * (non-Javadoc)
          * @see org.komea.product.wicket.LayoutPage#getSecurityController()
          */
         @Override
         protected IKomeaSecurityController getSecurityController() {
-
-
+        
+        
             final IKomeaSecurityController mock2 = mock(IKomeaSecurityController.class);
             when(mock2.isUserInRole("ADMIN")).thenReturn(true);
             return mock2;
         }
-
-
+        
+        
         /*
          * (non-Javadoc)
          * @see org.komea.product.wicket.LayoutPage#obtainSecurityDetails()
          */
         @Override
         protected UserDetails obtainSecurityDetails() {
-
-
+        
+        
             return mock(UserDetails.class);
         }
     }
-    
-    
-    
+
+
+
     @Rule
     public final TesterMethodRule wicketRule = new TesterMethodRule();
-
-
-
+    
+    
+    
     @Test
     public void testLayoutWithAuth() throws Exception {
-
-
+    
+    
         final WicketTester newWicketTester = wicketRule.newWicketTester();
         newWicketTester.startPage(A.class);
         assertFalse(newWicketTester.hasNoErrorMessage().wasFailed());
         newWicketTester.getComponentFromLastRenderedPage("hellopanel:avatar");
-
+        
         newWicketTester.destroy();
-
+        
+    }
+    
+    
+    @Test
+    public void testLayoutWithoutAuth() throws Exception {
+    
+    
+        final WicketTester newWicketTester = wicketRule.newWicketTester();
+        newWicketTester.startPage(HomePage.class);
+        assertFalse(newWicketTester.hasNoErrorMessage().wasFailed());
+        
+        newWicketTester.destroy();
+        
     }
 }
