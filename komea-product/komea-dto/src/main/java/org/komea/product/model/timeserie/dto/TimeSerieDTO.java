9f3e53eadc94277e3205ec652c33267471100dfe
Sebastien Carreau <akira.seb@gmail.com>
2014-06-25 16:24:08 +0200

    TimeSerieDTO : add method timeSeriesToMeasureResults

--- komea-product/komea-dto/src/main/java/org/komea/product/model/timeserie/dto/TimeSerieDTO.java
+++ komea-product/komea-dto/src/main/java/org/komea/product/model/timeserie/dto/TimeSerieDTO.java
@@ -1,73 +1,52 @@
-
 package org.komea.product.model.timeserie.dto;
 
-
-
+import com.google.common.collect.Lists;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-
 import org.komea.product.database.dto.BaseEntityDto;
 import org.komea.product.database.enums.GroupFormula;
 import org.komea.product.database.model.Kpi;
 import org.komea.product.service.dto.MeasureResult;
 
-import com.google.common.collect.Lists;
+public class TimeSerieDTO implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
-
-public class TimeSerieDTO implements Serializable
-{
-
-
-    private static final long       serialVersionUID = 1L;
-
-    private List<TimeCoordinateDTO> coordinates      = Lists.newArrayList();
-    private BaseEntityDto           entity;
-    private Kpi                     kpi;
-
-
+    private List<TimeCoordinateDTO> coordinates = Lists.newArrayList();
+    private BaseEntityDto entity;
+    private Kpi kpi;
 
     public TimeSerieDTO() {
 
-
     }
-
 
     public TimeSerieDTO(
             final List<TimeCoordinateDTO> coordinates,
             final Kpi kpi,
             final BaseEntityDto entity) {
 
-
         this.coordinates = coordinates;
         this.kpi = kpi;
         this.entity = entity;
     }
 
-
     public boolean addCoordinate(final TimeCoordinateDTO _e) {
-
 
         return coordinates.add(_e);
     }
 
-
     public List<TimeCoordinateDTO> getCoordinates() {
-
 
         return coordinates;
     }
 
-
     public BaseEntityDto getEntity() {
-
 
         return entity;
     }
 
-
     public Double getGroupFormulaValue() {
-
 
         double sum = 0;
         int count = 0;
@@ -86,37 +65,35 @@ public class TimeSerieDTO implements Serializable
         return sum;
     }
 
-
     public Kpi getKpi() {
-
 
         return kpi;
     }
 
-
     public void setCoordinates(final List<TimeCoordinateDTO> coordinates) {
-
 
         this.coordinates = coordinates;
     }
 
-
     public void setEntity(final BaseEntityDto _entity) {
-
 
         entity = _entity;
     }
 
-
     public void setKpi(final Kpi _kpi) {
-
 
         kpi = _kpi;
     }
 
+    public static List<MeasureResult> timeSeriesToMeasureResults(final List<TimeSerieDTO> timeSerieDTOs) {
+        final List<MeasureResult> measureResults = new ArrayList<MeasureResult>(timeSerieDTOs.size());
+        for (final TimeSerieDTO timeSerieDTO : timeSerieDTOs) {
+            measureResults.add(timeSerieDTO.toMeasureResult());
+        }
+        return measureResults;
+    }
 
     public MeasureResult toMeasureResult() {
-
 
         final Double average = getGroupFormulaValue();
         return new MeasureResult(entity, kpi, average);
@@ -129,8 +106,7 @@ public class TimeSerieDTO implements Serializable
      */
     @Override
     public String toString() {
-    
-    
+
         return "TimeSerieDTO [\\n\\tcoordinates="
                 + coordinates + ", \\n\\tkpi=" + kpi.getKey() + ", \\n\\tentity=" + entity + "]";
     }
