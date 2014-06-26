b9c6cff74df9410ee9209cc44117ff5ea5c59c42
Sylvain Leroy <sleroy0@gmail.com>
2014-06-26 11:13:33 +0200

    [stat,page] fixed unit test

--- komea-product/komea-admin-webgui/src/test/java/org/komea/product/wicket/statistics/StatPageTest.java
+++ komea-product/komea-admin-webgui/src/test/java/org/komea/product/wicket/statistics/StatPageTest.java
@@ -1,35 +1,46 @@
 /**
- * 
+ *
  */
 
 package org.komea.product.wicket.statistics;
 
 
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.komea.product.backend.api.IKpiRefreshingScheduler;
 import org.komea.product.backend.api.IQueryService;
 import org.komea.product.backend.service.esper.IEventStatisticsService;
 import org.komea.product.backend.service.kpi.IStatisticsAPI;
 import org.komea.product.wicket.utils.WicketTesterMethodRule;
 import org.mockito.Mockito;
 
+
+
 /**
  * @author sleroy
  */
-public class StatPageTest {
+public class StatPageTest
+{
+    
     
     @Rule
     public final WicketTesterMethodRule wicketRule = new WicketTesterMethodRule();
     
+    
+    
     @Before
     public void before() {
+    
     
         wicketRule.getApplicationContextMock().putBean(Mockito.mock(IQueryService.class));
         wicketRule.getApplicationContextMock().putBean(Mockito.mock(IEventStatisticsService.class));
         wicketRule.getApplicationContextMock().putBean(Mockito.mock(IStatisticsAPI.class));
-        
+        wicketRule.getApplicationContextMock().putBean(Mockito.mock(IKpiRefreshingScheduler.class));
+
     }
+    
     
     /**
      * Test method for
@@ -38,8 +49,9 @@ public class StatPageTest {
     @Test
     public final void testStatPage() throws Exception {
     
-        wicketRule.testStart(StatPage.class);
-       
-    }
     
+        wicketRule.testStart(StatPage.class);
+        
+    }
+
 }
