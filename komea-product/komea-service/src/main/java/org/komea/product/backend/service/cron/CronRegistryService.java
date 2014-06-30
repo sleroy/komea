de31f3602987e86a35383d9bf9ad3b77b7829d98
Sylvain Leroy <sleroy0@gmail.com>
2014-06-30 22:45:02 +0200

    [bugfix] major problem with thread closing

--- komea-product/komea-service/src/main/java/org/komea/product/backend/service/cron/CronRegistryService.java
+++ komea-product/komea-service/src/main/java/org/komea/product/backend/service/cron/CronRegistryService.java
@@ -39,34 +39,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class CronRegistryService implements ICronRegistryService, ApplicationContextAware
 {
-    
-    
+
+
     private static Logger    LOGGER = LoggerFactory.getLogger(CronRegistryService.class);
-    
+
     private SchedulerFactory schedulerFactory;
-    
-    
-    
+
+
+
     public CronRegistryService() {
-    
-    
+
+
         super();
     }
-    
-    
+
+
     @PreDestroy
     public void destroy() {
-    
-    
+
+
         try {
-            
+
             schedulerFactory.getScheduler().clear();
+            schedulerFactory.getScheduler().shutdown(false); // Kill without wait
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
     }
-    
-    
+
+
     /**
      * Method disableCronTask.
      *
@@ -76,18 +77,18 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public void disableCronTask(final String _cronName) {
-    
-    
+
+
         LOGGER.info("Disabling the cron task {}", _cronName);
         try {
             schedulerFactory.getScheduler().pauseTrigger(TriggerKey.triggerKey(_cronName));
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
     }
-    
-    
+
+
     /**
      * Method enableCronTask.
      *
@@ -97,18 +98,18 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public void enableCronTask(final String _cronName) {
-    
-    
+
+
         LOGGER.info("Enabling the cron task {}", _cronName);
         try {
             schedulerFactory.getScheduler().resumeTrigger(TriggerKey.triggerKey(_cronName));
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
     }
-    
-    
+
+
     /**
      * Method existCron.
      *
@@ -119,16 +120,16 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public boolean existCron(final String _jobName) {
-    
-    
+
+
         try {
             return schedulerFactory.getScheduler().checkExists(JobKey.jobKey(_jobName));
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
     }
-    
-    
+
+
     /*
      * (non-Javadoc)
      * @see
@@ -137,17 +138,17 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public void forceNow(final String _jobName) {
-    
-    
+
+
         try {
             schedulerFactory.getScheduler().triggerJob(JobKey.jobKey(_jobName));
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
     }
-    
-    
+
+
     /**
      * Method getCronTasks.
      *
@@ -156,8 +157,8 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public List<CronDetails> getCronTasks() {
-    
-    
+
+
         final List<CronDetails> cronDetailsList = new ArrayList<CronDetails>();
         try {
             for (final JobKey jobKey : schedulerFactory.getScheduler().getJobKeys(
@@ -165,7 +166,7 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
                 schedulerFactory.getScheduler().getJobDetail(jobKey);
                 final CronDetails cronDetails = new CronDetails();
                 cronDetails.setCronName(jobKey.getName());
-                
+
                 final TriggerKey triggerKey = TriggerKey.triggerKey(jobKey.getName());
                 final Trigger trigger = schedulerFactory.getScheduler().getTrigger(triggerKey);
                 if (trigger instanceof CronTrigger) {
@@ -173,38 +174,38 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
                 }
                 cronDetails.setLastTime(trigger.getPreviousFireTime());
                 cronDetails.setNextTime(trigger.getNextFireTime());
-
+                
                 final TriggerState triggerState =
                         schedulerFactory.getScheduler().getTriggerState(triggerKey);
-
+                
                 cronDetails.setStatus(triggerState);
                 cronDetailsList.add(cronDetails);
             }
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
         return cronDetailsList;
     }
-    
-    
+
+
     public SchedulerFactory getSchedulerFactory() {
-    
-    
+
+
         return schedulerFactory;
     }
-    
-    
+
+
     @Override
     public void registerCronTask(
             final String _cronName,
             final BackupDelay _backupDelay,
             final Class<? extends Job> _runnable,
             final JobDataMap _properties) {
-    
-    
+
+
         CronScheduleBuilder cronSchedule;
-        
+
         switch (_backupDelay) {
             case DAY:
                 cronSchedule = CronScheduleBuilder.dailyAtHourAndMinute(20, 0);
@@ -222,10 +223,10 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
                 throw new UnsupportedOperationException("Enum is unknown : " + _backupDelay);
         }
         createNewCronJob(_cronName, _runnable, _properties, cronSchedule);
-        
+
     }
-    
-    
+
+
     // For more informations :
     // http://quartz-scheduler.org/documentation/quartz-2.x/tutorials/tutorial-lesson-02
     /**
@@ -248,14 +249,14 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
             final String _cronExpression,
             final Class<? extends Job> _runnable,
             final JobDataMap _properties) {
-    
-    
+
+
         final CronScheduleBuilder cronSchedule = CronScheduleBuilder.cronSchedule(_cronExpression);
         createNewCronJob(_cronName, _runnable, _properties, cronSchedule);
-        
+
     }
-    
-    
+
+
     /**
      * Method removeCronTask.
      *
@@ -265,17 +266,17 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public void removeCronTask(final String _cronName) {
-    
-    
+
+
         try {
             schedulerFactory.getScheduler().deleteJob(JobKey.jobKey(_cronName));
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
     }
-    
-    
+
+
     /*
      * (non-Javadoc)
      * @see
@@ -285,29 +286,29 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
     @Override
     public void setApplicationContext(final ApplicationContext _applicationContext)
             throws BeansException {
-    
-    
+
+
         try {
-            
+
             schedulerFactory = new StdSchedulerFactory();
             schedulerFactory.getScheduler().setJobFactory(
                     new JobInjectionFactory(_applicationContext));
             schedulerFactory.getScheduler().start();
-            
+
         } catch (final SchedulerException e) {
             throw new CronRuntimeException(e);
         }
-        
+
     }
-    
-    
+
+
     public void setSchedulerFactory(final SchedulerFactory _schedulerFactory) {
-    
-    
+
+
         schedulerFactory = _schedulerFactory;
     }
-    
-    
+
+
     /**
      * Method updateCronFrequency.
      *
@@ -319,46 +320,46 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
      */
     @Override
     public void updateCronFrequency(final String _cronName, final String _cronExpression) {
-    
-    
+
+
         try {
             final JobDetail jobDetail =
                     schedulerFactory.getScheduler().getJobDetail(JobKey.jobKey(_cronName));
-            
+
             final JobDataMap jobDataMap = jobDetail.getJobDataMap();
-            
+
             final Trigger trigger =
                     TriggerBuilder.newTrigger().forJob(_cronExpression).usingJobData(jobDataMap)
-                            .withIdentity(_cronName).startNow()
-                            .withSchedule(CronScheduleBuilder.cronSchedule(_cronExpression))
-                            .build();
+                    .withIdentity(_cronName).startNow()
+                    .withSchedule(CronScheduleBuilder.cronSchedule(_cronExpression))
+                    .build();
             schedulerFactory.getScheduler().rescheduleJob(TriggerKey.triggerKey(_cronExpression),
                     trigger);
         } catch (final SchedulerException e) {
             throw new CronRuntimeException("Impossible to update the cron frequency of the task : "
                     + _cronName, e);
         }
-        
+
     }
-    
-    
+
+
     private void createNewCronJob(
             final String _cronName,
             final Class<? extends Job> _runnable,
             final JobDataMap _properties,
             final CronScheduleBuilder cronSchedule) throws CronRuntimeException {
-    
-    
+
+
         try {
-            
+
             LOGGER.info("Registering new CRON {}", _cronName);
             final JobDetail jobDetail =
                     JobBuilder.newJob(_runnable).withIdentity(_cronName).withDescription(_cronName)
-                            .usingJobData(_properties).build();
-            
+                    .usingJobData(_properties).build();
+
             final Trigger trigger =
                     TriggerBuilder.newTrigger().forJob(jobDetail).usingJobData(_properties)
-                            .withIdentity(_cronName).withSchedule(cronSchedule).build();
+                    .withIdentity(_cronName).withSchedule(cronSchedule).build();
             schedulerFactory.getScheduler().scheduleJob(jobDetail, trigger);
         } catch (final SchedulerException e) {
             try {
@@ -369,5 +370,5 @@ public class CronRegistryService implements ICronRegistryService, ApplicationCon
             throw new CronRuntimeException("Impossible to create the cron task", e);
         }
     }
-    
+
 }
