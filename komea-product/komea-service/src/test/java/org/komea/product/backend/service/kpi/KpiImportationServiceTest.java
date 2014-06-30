3f1b648f7494de64ab09ed3ab72b21f798e4b077
Sylvain Leroy <sleroy0@gmail.com>
2014-06-30 11:03:13 +0200

    [kpi,import] upgraded unit test

--- komea-product/komea-service/src/test/java/org/komea/product/backend/service/kpi/KpiImportationServiceTest.java
+++ komea-product/komea-service/src/test/java/org/komea/product/backend/service/kpi/KpiImportationServiceTest.java
@@ -10,9 +10,12 @@ import java.io.File;
 
 import org.junit.Test;
 import org.komea.eventory.api.engine.IQuery;
+import org.komea.product.backend.service.kpi.IKpiImportationService.KpiImportator;
 import org.komea.product.backend.service.queries.IQueryWithAnnotations;
 import org.komea.product.test.spring.AbstractSpringIntegrationTestCase;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.mockito.Mockito.mock;
 
 
 
@@ -21,13 +24,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class KpiImportationServiceTest extends AbstractSpringIntegrationTestCase
 {
-
-
+    
+    
     @Autowired
     private IKpiImportationService kpiImportationService;
-    
-    
-    
+
+
+
     /**
      * Test method for
      * {@link org.komea.product.backend.service.kpi.KpiImportationService#importCatalog(java.io.File, org.komea.product.backend.service.kpi.IKpiImportationService.KpiImportator)}
@@ -35,27 +38,33 @@ public class KpiImportationServiceTest extends AbstractSpringIntegrationTestCase
      */
     @Test
     public final void testImportCatalog() throws Exception {
-    
-    
+
+
         kpiImportationService.importCatalog(new File(
                 "src/test/resources/komea-kpis-1.1.24-SNAPSHOT.jar"),
                 new IKpiImportationService.KpiImportator()
-                {
-                    
-                    
-                    @Override
-                    public void iterate(
-                            final String _file,
-                    final IQueryWithAnnotations<IQuery> _kpiDefinition,
-                    final Throwable _throwable) {
-                    
-                    
-                        System.out.println(_file);
-                        
-                        
-                    }
-                });
+        {
+
+
+            @Override
+            public void iterate(
+                    final String _file,
+                            final IQueryWithAnnotations<IQuery> _kpiDefinition,
+                            final Throwable _throwable) {
+
+
+                System.out.println(_file);
+
+
+            }
+        });
     }
-
-
+    
+    
+    @Test
+    public final void testImportCatalogFromClassLoader() {
+    
+    
+        kpiImportationService.importCatalogFromClassLoader(mock(KpiImportator.class));
+    }
 }
