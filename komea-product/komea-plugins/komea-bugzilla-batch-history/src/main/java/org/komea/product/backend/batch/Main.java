5fddcf1bfc0d829e384037ddf74c2eb7706cc9c6
Sylvain Leroy <sleroy0@gmail.com>
2014-06-30 22:39:14 +0200

    [quartz] fixed shutdown problem with quartz

--- komea-product/komea-plugins/komea-bugzilla-batch-history/src/main/java/org/komea/product/backend/batch/Main.java
+++ komea-product/komea-plugins/komea-bugzilla-batch-history/src/main/java/org/komea/product/backend/batch/Main.java
@@ -24,25 +24,25 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main
 {
-
-
+    
+    
     private static final String BATCH_CONF                                   = "batch-conf.xml";
     
     private static final String CONFIGURATION_SPRING_APPLICATION_CONTEXT_XML =
-            "configuration/spring/application-context.xml";
-
+                                                                                     "configuration/spring/application-context.xml";
+    
     private static final Logger LOGGER                                       =
-            LoggerFactory
-            .getLogger(Main.class);
-
-
+                                                                                     LoggerFactory
+                                                                                             .getLogger(Main.class);
+    
+    
     private static final int    NUMBER_ARGS                                  = 2;
-
-
-
+    
+    
+    
     public static void main(final String[] args) throws IOException {
-
-
+    
+    
         // ARG0 = ProjectName
         // ARG1 = Configuration Folder
         if (args.length != NUMBER_ARGS) {
@@ -52,40 +52,47 @@ public class Main
         final File configFolder = new File(args[1]).getAbsoluteFile();
         LOGGER.info("Configuration folder : {}", configFolder);
 
-        final InputStream inputStream = FileUtils.openInputStream(new File(args[1], BATCH_CONF));
-        LOGGER.info("Loading mybatis conf");
-        final SqlSessionFactory build = new SqlSessionFactoryBuilder().build(inputStream);
-        LOGGER.info("Opening database session");
-        final SqlSession openSession = build.openSession();
+        InputStream inputStream = null;
+        SqlSession openSession = null;
+        FileSystemXmlApplicationContext fileSystemXmlApplicationContext = null;
+        try {
+            inputStream = FileUtils.openInputStream(new File(args[1], BATCH_CONF));
+            LOGGER.info("Loading mybatis conf");
+            final SqlSessionFactory build = new SqlSessionFactoryBuilder().build(inputStream);
+            LOGGER.info("Opening database session");
+            openSession = build.openSession();
 
 
-        LOGGER.info("Opening Spring session");
-        final FileSystemXmlApplicationContext fileSystemXmlApplicationContext =
-                new FileSystemXmlApplicationContext(CONFIGURATION_SPRING_APPLICATION_CONTEXT_XML);
-        final IKpiImportationService kpiImportationService =
-                fileSystemXmlApplicationContext.getBean(IKpiImportationService.class);
-        LOGGER.info("Force importation of kpis if necessary");
-        kpiImportationService.importFolder(configFolder);
-        final ISpringService springService =
-                fileSystemXmlApplicationContext.getBean(ISpringService.class);
-        LOGGER.info("Rebuilding history for the project {}", PROJECT_NAME);
-        launchRebuilding(PROJECT_NAME, openSession, fileSystemXmlApplicationContext, springService);
-        
-        IOUtils.closeQuietly(fileSystemXmlApplicationContext);
-        IOUtils.closeQuietly(openSession);
-        IOUtils.closeQuietly(inputStream);
+            LOGGER.info("Opening Spring session");
+            fileSystemXmlApplicationContext =
+                    new FileSystemXmlApplicationContext(
+                            CONFIGURATION_SPRING_APPLICATION_CONTEXT_XML);
+            final IKpiImportationService kpiImportationService =
+                    fileSystemXmlApplicationContext.getBean(IKpiImportationService.class);
+            LOGGER.info("Force importation of kpis if necessary");
+            kpiImportationService.importFolder(configFolder);
+            final ISpringService springService =
+                    fileSystemXmlApplicationContext.getBean(ISpringService.class);
+            LOGGER.info("Rebuilding history for the project {}", PROJECT_NAME);
+            launchRebuilding(PROJECT_NAME, openSession, fileSystemXmlApplicationContext,
+                    springService);
+        } finally {
+            IOUtils.closeQuietly(fileSystemXmlApplicationContext);
+            IOUtils.closeQuietly(openSession);
+            IOUtils.closeQuietly(inputStream);
+        }
         ;
         
     }
-
-
+    
+    
     private static void launchRebuilding(
             final String PROJECT_NAME,
             final SqlSession openSession,
             final FileSystemXmlApplicationContext fileSystemXmlApplicationContext,
             final ISpringService springService) {
-
-
+    
+    
         final IRebuildHistoryService rebuildHistoryService =
                 new RebuildHistoryService(PROJECT_NAME);
         springService.autowirePojo(rebuildHistoryService);
