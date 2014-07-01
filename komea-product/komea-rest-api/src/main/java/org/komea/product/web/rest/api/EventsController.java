a98ebbf186b8cfad2d687a9c0565786905acd8e8
Sebastien Carreau <akira.seb@gmail.com>
2014-07-01 10:27:47 +0200

    Add EventsFilter class

--- komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/EventsController.java
+++ komea-product/komea-rest-api/src/main/java/org/komea/product/web/rest/api/EventsController.java
@@ -1,21 +1,14 @@
 package org.komea.product.web.rest.api;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.komea.product.backend.service.entities.IEntityService;
 import org.komea.product.backend.service.esper.IEventPushService;
 import org.komea.product.backend.service.esper.IEventViewerService;
+import org.komea.product.backend.utils.EventsFilter;
 import org.komea.product.database.alert.IEvent;
-import org.komea.product.database.dto.BaseEntityDto;
 import org.komea.product.database.dto.EventSimpleDto;
 import org.komea.product.database.dto.SearchEventDto;
-import org.komea.product.database.enums.EntityType;
-import org.komea.product.database.enums.ExtendedEntityType;
-import org.komea.product.database.enums.Severity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import com.google.common.collect.Lists;
 
 @Controller
 @RequestMapping(value = "/events")
@@ -54,15 +45,8 @@ public class EventsController {
 
         LOGGER.debug("call rest method /events/find to find events {}", _searchEvent);
         final List<IEvent> globalActivity = eventService.getGlobalActivity();
-        final List<IEvent> events = new ArrayList<IEvent>(_searchEvent.getMaxEvents());
-        final Iterator<IEvent> iterator = globalActivity.iterator();
-        while (iterator.hasNext() && events.size() < _searchEvent.getMaxEvents()) {
-            final IEvent event = iterator.next();
-            if (eventMatches(event, _searchEvent)) {
-                events.add(event);
-            }
-        }
-        return events;
+        final EventsFilter eventsFilter = new EventsFilter(_searchEvent);
+        return eventsFilter.filterEvents(globalActivity);
     }
 
     public IEventPushService getEventPushService() {
@@ -84,30 +68,4 @@ public class EventsController {
         eventPushService.sendEventDto(_event);
     }
 
-    private boolean eventMatches(final IEvent event, final SearchEventDto searchEvent) {
-
-        final Severity severity = searchEvent.getSeverityMin();
-        final List<String> eventTypeKeys = searchEvent.getEventTypeKeys();
-        final List<String> parentEntityKeys = searchEvent.getEntityKeys();
-        final ExtendedEntityType extendedEntityType = searchEvent.getEntityType();
-        final EntityType kpiType = extendedEntityType.getKpiType();
-        final List<BaseEntityDto> parentEntities = entityService.getBaseEntityDTOS(extendedEntityType.getEntityType(), parentEntityKeys);
-        final List<BaseEntityDto> entities = entityService.getSubEntities(extendedEntityType, parentEntities);
-        final List<String> entityKeys = Lists.newArrayList();
-        for (final BaseEntityDto baseEntityDto : entities) {
-            entityKeys.add(baseEntityDto.getKey());
-        }
-        if ((eventTypeKeys.isEmpty() || eventTypeKeys.contains(event.getEventType().getEventKey()))
-                && (kpiType == null || kpiType.equals(event.getEventType().getEntityType()))
-                && event.getEventType().getSeverity().compareTo(severity) >= 0) {
-            if (kpiType == null) {
-                return true;
-            }
-            final String entityKey = eventService.getEntityKey(kpiType, event);
-            if (entityKeys.isEmpty() || entityKeys.contains(entityKey)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
---
+++ komea-product/komea-service-api/src/main/java/org/komea/product/backend/utils/EventsFilter.java
@@ -0,0 +1,67 @@
+package org.komea.product.backend.utils;
+
+import com.google.common.collect.Lists;
+import java.util.Iterator;
+import java.util.List;
+import org.komea.product.backend.service.entities.IEntityService;
+import org.komea.product.backend.service.esper.IEventViewerService;
+import org.komea.product.database.alert.IEvent;
+import org.komea.product.database.dto.BaseEntityDto;
+import org.komea.product.database.dto.SearchEventDto;
+import org.komea.product.database.enums.EntityType;
+import org.komea.product.database.enums.ExtendedEntityType;
+import org.komea.product.database.enums.Severity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventsFilter {
+
+    @Autowired
+    private IEventViewerService eventService;
+    @Autowired
+    private IEntityService entityService;
+
+    final SearchEventDto searchEvent;
+
+    public EventsFilter(SearchEventDto searchEvent) {
+        this.searchEvent = searchEvent;
+    }
+
+    public List<IEvent> filterEvents(final List<IEvent> _events) {
+        final List<IEvent> result = Lists.newArrayList();
+        final Iterator<IEvent> iterator = _events.iterator();
+        while (iterator.hasNext() && _events.size() < searchEvent.getMaxEvents()) {
+            final IEvent event = iterator.next();
+            if (eventMatches(event)) {
+                _events.add(event);
+            }
+        }
+        return result;
+    }
+
+    private boolean eventMatches(final IEvent event) {
+        final Severity severity = searchEvent.getSeverityMin();
+        final List<String> eventTypeKeys = searchEvent.getEventTypeKeys();
+        final List<String> parentEntityKeys = searchEvent.getEntityKeys();
+        final ExtendedEntityType extendedEntityType = searchEvent.getEntityType();
+        final EntityType kpiType = extendedEntityType.getKpiType();
+        final List<BaseEntityDto> parentEntities = entityService.getBaseEntityDTOS(extendedEntityType.getEntityType(), parentEntityKeys);
+        final List<BaseEntityDto> entities = entityService.getSubEntities(extendedEntityType, parentEntities);
+        final List<String> entityKeys = Lists.newArrayList();
+        for (final BaseEntityDto baseEntityDto : entities) {
+            entityKeys.add(baseEntityDto.getKey());
+        }
+        if ((eventTypeKeys.isEmpty() || eventTypeKeys.contains(event.getEventType().getEventKey()))
+                && (kpiType == null || kpiType.equals(event.getEventType().getEntityType()))
+                && event.getEventType().getSeverity().compareTo(severity) >= 0) {
+            if (kpiType == null) {
+                return true;
+            }
+            final String entityKey = eventService.getEntityKey(kpiType, event);
+            if (entityKeys.isEmpty() || entityKeys.contains(entityKey)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
