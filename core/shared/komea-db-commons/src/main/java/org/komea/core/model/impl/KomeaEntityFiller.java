6eb084e096afbd2dbfcfec911e687f9f289fe789
Antoine Floch <antoine.floch@tocea.com>
2014-12-02 11:53:29 +0100

    [db,schema] use interface of storage instead of impl

--- core/shared/komea-db-commons/src/main/java/org/komea/core/model/impl/KomeaEntityFiller.java
+++ core/shared/komea-db-commons/src/main/java/org/komea/core/model/impl/KomeaEntityFiller.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.komea.core.model.IKomeaEntity;
+import org.komea.core.model.IKomeaEntityFactory;
 import org.komea.core.schema.IEntityType;
 import org.komea.core.utils.PojoToMap;
 
@@ -17,10 +18,10 @@ import org.komea.core.utils.PojoToMap;
  */
 public class KomeaEntityFiller<T> implements IKomeaEntityFiller<T> {
 
-	private final OKomeaModelFactory	oKomeaModelFactory;
+	private final IKomeaEntityFactory	oKomeaModelFactory;
 	private final IEntityType	     entityType;
 
-	public KomeaEntityFiller(final OKomeaModelFactory _oKomeaModelFactory, final IEntityType _entityType) {
+	public KomeaEntityFiller(final IKomeaEntityFactory _oKomeaModelFactory, final IEntityType _entityType) {
 		this.oKomeaModelFactory = _oKomeaModelFactory;
 		this.entityType = _entityType;
 
@@ -29,7 +30,7 @@ public class KomeaEntityFiller<T> implements IKomeaEntityFiller<T> {
 	@Override
 	public void put(final T _entity) {
 		final Map<String, Object> beanMap = new PojoToMap().convertPojoInMap(_entity);
-		final IKomeaEntity newInstance = this.oKomeaModelFactory.newInstance(this.entityType);
+		final IKomeaEntity newInstance = this.oKomeaModelFactory.create(this.entityType);
 		final Iterator<Entry<String, Object>> it = beanMap.entrySet().iterator();
 		while (it.hasNext()) {
 			final Entry<String, Object> entry = it.next();
