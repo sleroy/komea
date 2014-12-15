f9d0b45cfe0a1135fba380ad8429b9c3a131cee7
jguidoux <jeremie.guidoux@tocea.com>
2014-12-15 17:56:51 +0100

    * add possibility to create an executacle  jar with all dependencies (fatJar) for the connectors

---
+++ modules/connectors/bugzilla-connector/src/main/java/org/komea/connectors/bugzilla/launch/Launcher.java
@@ -0,0 +1,14 @@
+package org.komea.connectors.bugzilla.launch;
+
+
+/**
+ * Created by jguidoux on 12/12/14.
+ */
+public class Launcher {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello bugzilla connector");
+	}
+}
---
+++ modules/connectors/git-connector/src/main/java/org/komea/connectors/git/launch/Launcher.java
@@ -0,0 +1,9 @@
+package org.komea.connectors.git.launch;
+
+
+/**
+ * Created by jguidoux on 12/12/14.
+ */
+public class Launcher {
+	
+}
