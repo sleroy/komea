3a28314e515abc8346ed37c0ba8865d7fbedebeb
RaphaelTocea <rgalerme@Tera>
2014-12-16 18:10:01 +0100

    fix in jira api to connect to mongodb #2699

--- modules/connectors/jira-connector/src/main/java/com/tocea/jiraconnector/core/JiraServerContext.java
+++ modules/connectors/jira-connector/src/main/java/com/tocea/jiraconnector/core/JiraServerContext.java
@@ -6,10 +6,23 @@
 package com.tocea.jiraconnector.core;
 
 import com.tocea.jiraconnector.generalplugin.BadConfigurationException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.rcarz.jiraclient.BasicCredentials;
 import net.rcarz.jiraclient.JiraClient;
 import net.rcarz.jiraclient.greenhopper.GreenHopperClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.BasicClientConnectionManager;
 
 /**
  *
@@ -28,15 +41,15 @@ public class JiraServerContext {
         if ("".equals(configuration.getUrl())) {
             throw new BadConfigurationException("Map of jira configuration deos not contain the correct field");
         }
-        JiraClient client = null;
+        JiraClient client;
         if (containUser(configuration)) {
-            client = new JiraClient(configuration.getUrl(), new BasicCredentials(configuration.getLogin(), configuration.getPass()));
+            client = new JiraClient(configuration.getUrl(), new BasicCredentials(configuration.getLogin(), configuration.getPass()),true);          
         } else {
-            client = new JiraClient(configuration.getUrl());
+            client = new JiraClient(configuration.getUrl(),null,true);
         }
         return new JiraServerContext(client);
     }
-
+    
     private static boolean containUser(JiraConfiguration configuration) {
 
         boolean result;
