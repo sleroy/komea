1f70856d01ecf1fdd272efb074daec396282d2a8
Antoine Floch <antoine.floch@tocea.com>
2014-12-18 16:57:45 +0100

    [connectors, jira] the sent events now only contains partial information of the Jira issue

--- modules/connectors/jira-connector/src/main/java/org/komea/connectors/jira/JiraEventsConnector.java
+++ modules/connectors/jira-connector/src/main/java/org/komea/connectors/jira/JiraEventsConnector.java
@@ -2,13 +2,15 @@
 package org.komea.connectors.jira;
 
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import net.rcarz.jiraclient.Field;
 import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.Issue.SearchResult;
 import net.rcarz.jiraclient.JiraException;
+import net.rcarz.jiraclient.Version;
 
 import org.komea.connectors.jira.exceptions.BadConfigurationException;
 import org.komea.connectors.jira.utils.JiraServerContext;
@@ -17,18 +19,16 @@ import org.komea.event.storage.IEventStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
-
 public class JiraEventsConnector
 {
     
     private final static Logger LOGGER           = LoggerFactory.getLogger(JiraEventsConnector.class);
     
-    public static final String PROVIDER_BUG     = "jira";
+    public static final String  JIRA             = "jira";
     
-    public static final String EVENT_UPDATE_BUG = "issue_update";
+    public static final String  EVENT_UPDATE_BUG = "issue_update";
     
-    public static final String EVENT_NEW_BUG    = "issue_new";
+    public static final String  EVENT_NEW_BUG    = "issue_new";
     
     private final IEventStorage storage;
     
@@ -64,16 +64,27 @@ public class JiraEventsConnector
             sendIssues(searchIssues.issues);
             
             if (searchIssues.total > searchIssues.max) {
+                
                 for (int i = searchIssues.max; i < searchIssues.total; i = i + searchIssues.max) {
                     
-                    Issue.SearchResult parcourIssues = this.jira.getClient().searchIssues("created > \"" + format + "\"", null,
-                            JiraServerContext.GetOccurence, i);
-                    sendIssues(parcourIssues.issues);
+                    searchIssues = this.jira.getClient().searchIssues("created > \"" + format + "\"", null, JiraServerContext.GetOccurence,
+                            i);
+                    
+                    sendIssues(searchIssues.issues);
+                    log(searchIssues);
                 }
             }
         } catch (JiraException ex) {
             LOGGER.error(ex.getMessage(), ex);
         }
+    }
+    
+    private void log(final SearchResult searchIssues) {
+    
+        double percentage = ((double) searchIssues.start + searchIssues.max) / (searchIssues.total) * 100;
+        
+        LOGGER.info("Processing done at {}%", (int) percentage);
+        
     }
     
     private void importUpdateIssue(final Date date) {
@@ -101,48 +112,42 @@ public class JiraEventsConnector
     
     private void sendIssues(final List<Issue> issues) {
     
-        sendEvent(issues, EVENT_NEW_BUG);
+        for (Issue issue : issues) {
+            sendEvent(issue, EVENT_NEW_BUG);
+        }
+        
     }
     
     private void sendUpdateIssue(final List<Issue> issues) {
     
-        sendEvent(issues, EVENT_UPDATE_BUG);
-    }
-    
-    private void sendEvent(final List<Issue> issues, final String eventName) {
-    
-        ComplexEvent complexEventDto;
-        for (Issue sue : issues) {
-            
-            complexEventDto = createBugEvent(sue, eventName);
-            this.storage.storeComplexEvent(complexEventDto);
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    private ComplexEvent createBugEvent(final Issue bug, final String eventName) {
-    
-        final ComplexEvent complexEventDto = new ComplexEvent();
-        complexEventDto.setProvider(PROVIDER_BUG);
-        complexEventDto.setEventType(eventName);
-        Map<String, Serializable> properties = Maps.newHashMap();
-        
-        try {
-            java.lang.reflect.Field declaredField = Issue.class.getDeclaredField("fields");
-            declaredField.setAccessible(true);
-            properties = (Map<String, Serializable>) declaredField.get(bug);
-        } catch (NoSuchFieldException ex) {
-            LOGGER.error(ex.getMessage(), ex);
-        } catch (SecurityException ex) {
-            LOGGER.error(ex.getMessage(), ex);
-        } catch (IllegalArgumentException ex) {
-            LOGGER.error(ex.getMessage(), ex);
-        } catch (IllegalAccessException ex) {
-            LOGGER.error(ex.getMessage(), ex);
+        for (Issue issue : issues) {
+            sendEvent(issue, EVENT_UPDATE_BUG);
         }
         
-        complexEventDto.setProperties(properties);
-        return complexEventDto;
+    }
+    
+    private void sendEvent(final Issue bug, final String eventName) {
+    
+        final ComplexEvent event = new ComplexEvent();
+        event.setProvider(JIRA);
+        event.setEventType(eventName);
+        event.setDate(Field.getDate(bug.getField("created")));
+        
+        event.addField("id", bug.getId());
+        event.addField("key", bug.getKey());
+
+     
+        event.addField("description", bug.getDescription());
+        if(bug.getProject()!=null){
+            event.addField("project", bug.getProject().getName());
+        }
+        if (bug.getStatus() != null) {
+            event.addField("status", bug.getStatus().getName());
+        }
+        if (bug.getIssueType() != null) {
+            event.addField("type", bug.getIssueType().getName());
+        }
+        this.storage.storeComplexEvent(event);
     }
     
     /**
