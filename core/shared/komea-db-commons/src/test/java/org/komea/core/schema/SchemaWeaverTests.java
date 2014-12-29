b7e30cd4f75f31596a1ae52e52dafa63dad90f13
Sylvain Leroy <sleroy0@gmail.com>
2014-12-29 18:02:03 +0100

    [fixed] exceptions

--- core/shared/komea-db-commons/src/test/java/org/komea/core/schema/SchemaWeaverTests.java
+++ core/shared/komea-db-commons/src/test/java/org/komea/core/schema/SchemaWeaverTests.java
@@ -1,59 +1,51 @@
-
 package org.komea.core.schema;
-
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.komea.core.schema.impl.KomeaSchemaFactory;
 import org.komea.core.schema.impl.SchemaWeaver;
-import org.komea.core.schema.utils.YUMLExport;
 
 import com.google.common.collect.Lists;
 
-public class SchemaWeaverTests
-{
-    
-    private final IKomeaSchemaFactory factory = new KomeaSchemaFactory();
-    
-    @Test
-    public void weaverTest() throws FileNotFoundException, IOException {
-    
-        IKomeaSchema schema1 = buildSchema1();
-        IKomeaSchema schema2 = buildSchema2();
-        
-        SchemaWeaver weaver = new SchemaWeaver();
-        IKomeaSchema weaved = weaver.weave("weaved", Lists.newArrayList(schema1,schema2));
-        Assert.assertTrue(weaved.findType("Person").getProperties().size()==2);
-    }
-    
-    private IKomeaSchema buildSchema2() {
-    
-        IKomeaSchema schema = this.factory.newSchema("S2");
-        
-        IEntityType person = this.factory.newEntity("Person");
-        person.addProperty(this.factory.newAttribute("name", Primitive.STRING));
-        person.addProperty(this.factory.newAttribute("email", Primitive.STRING));
-        
-        schema.addType(person);
-        return schema;
-    }
-    
-    private IKomeaSchema buildSchema1() {
-    
-        IKomeaSchema schema = this.factory.newSchema("S1");
-        
-        IEntityType person = this.factory.newEntity("Person");
-        person.addProperty(this.factory.newAttribute("name", Primitive.STRING).enableIndexation());
-        
-        IEntityType developer = this.factory.newEntity("Developper");
-        developer.setSuperType(person);
-        
-        schema.addType(person);
-        schema.addType(developer);
-        return schema;
-    }
+public class SchemaWeaverTests {
+
+	private final IKomeaSchemaFactory	factory	= new KomeaSchemaFactory();
+
+	@Test
+	public void weaverTest() {
+
+		final IKomeaSchema schema1 = buildSchema1();
+		final IKomeaSchema schema2 = buildSchema2();
+
+		final SchemaWeaver weaver = new SchemaWeaver();
+		final IKomeaSchema weaved = weaver.weave("weaved", Lists.newArrayList(schema1, schema2));
+		Assert.assertTrue(weaved.findType("Person").getProperties().size() == 2);
+	}
+
+	private IKomeaSchema buildSchema1() {
+
+		final IKomeaSchema schema = factory.newSchema("S1");
+
+		final IEntityType person = factory.newEntity("Person");
+		person.addProperty(factory.newAttribute("name", Primitive.STRING).enableIndexation());
+
+		final IEntityType developer = factory.newEntity("Developper");
+		developer.setSuperType(person);
+
+		schema.addType(person);
+		schema.addType(developer);
+		return schema;
+	}
+
+	private IKomeaSchema buildSchema2() {
+
+		final IKomeaSchema schema = factory.newSchema("S2");
+
+		final IEntityType person = factory.newEntity("Person");
+		person.addProperty(factory.newAttribute("name", Primitive.STRING));
+		person.addProperty(factory.newAttribute("email", Primitive.STRING));
+
+		schema.addType(person);
+		return schema;
+	}
 }
