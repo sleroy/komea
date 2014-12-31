201fdc6748fbc37c46230e356df7081d60c55ee3
Sylvain Leroy <sleroy0@gmail.com>
2014-12-31 11:16:52 +0100

    [fixed] orientdbevent to handle period select

--- core/komea-events/src/main/java/org/komea/event/storage/orientdb/impl/OrientDBEventDB.java
+++ core/komea-events/src/main/java/org/komea/event/storage/orientdb/impl/OrientDBEventDB.java
@@ -2,6 +2,8 @@ package org.komea.event.storage.orientdb.impl;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
@@ -19,89 +21,89 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
 public class OrientDBEventDB implements IEventDB {
-
+	
 	/**
 	 * @author sleroy
 	 */
 	private final class EmptyResultIterator implements
-	        ResultIterator<FlatEvent> {
+	ResultIterator<FlatEvent> {
 		@Override
 		public void close() {
-			
+
 		}
-		
+
 		@Override
 		public boolean hasNext() {
 			return false;
 		}
-		
+
 		@Override
 		public FlatEvent next() {
 			return null;
 		}
-		
+
 		@Override
 		public void remove() {
-			
+
 		}
 	}
-	
-	private final class OResultIteratorImplementation implements
-	ResultIterator<FlatEvent> {
-		private final OrientDBEventIterator	iterator;
 
+	private final class OResultIteratorImplementation implements
+	        ResultIterator<FlatEvent> {
+		private final OrientDBEventIterator	iterator;
+		
 		private OResultIteratorImplementation(
-				final OrientDBEventIterator _iterator) {
+		        final OrientDBEventIterator _iterator) {
 			iterator = _iterator;
 		}
-
+		
 		@Override
 		public void close() {
 			//
 		}
-
+		
 		@Override
 		public boolean hasNext() {
-
+			
 			return iterator.hasNext();
 		}
-
+		
 		@Override
 		public FlatEvent next() {
-
+			
 			return iterator.next();
 		}
-
+		
 		@Override
 		public void remove() {
 			iterator.remove();
-
+			
 		}
 	}
-
+	
 	private final OrientSessionFactory<ODatabaseDocumentTx>	orientSessionFactory;
 	private final String	                                eventType;
 	private static final Logger	                            LOGGER	= LoggerFactory
-			.getLogger(OrientDBEventDB.class);
-
+	                                                                       .getLogger(OrientDBEventDB.class);
+	
 	public OrientDBEventDB(
-			final OrientSessionFactory<ODatabaseDocumentTx> _orientSessionFactory,
-			final String _eventType) {
+	        final OrientSessionFactory<ODatabaseDocumentTx> _orientSessionFactory,
+	        final String _eventType) {
 		orientSessionFactory = _orientSessionFactory;
 		eventType = _eventType;
 	}
-
+	
 	@Override
 	public void close() throws IOException {
 		// Do not close the osf.
-
+		
 	}
-
+	
 	@Override
 	public long count() {
 		try {
 			return orientSessionFactory.getOrCreateDB().countClass(
-					eventType);
+			        eventType);
 		} catch (final Exception e) {
 			if (LOGGER.isDebugEnabled()) {
 				LOGGER.warn(e.getMessage(), e);
@@ -109,21 +111,21 @@ public class OrientDBEventDB implements IEventDB {
 		}
 		return 0L;
 	}
-
+	
 	@Override
 	public ResultIterator<FlatEvent> loadAll() {
 		try {
 			final ORecordIteratorClass<ODocument> browseClass = OrientDBEventDB.this.orientSessionFactory
-					.getOrCreateDB()
-					.browseClass(OrientDBEventDB.this.eventType);
+			        .getOrCreateDB()
+			        .browseClass(OrientDBEventDB.this.eventType);
 			return new OResultIteratorImplementation(new OrientDBEventIterator(
-					browseClass.iterator()));
+			        browseClass.iterator()));
 		} catch (final Exception e) {
 			LOGGER.warn(e.getMessage(), e);
 			return new EmptyResultIterator();
 		}
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * @see
@@ -132,28 +134,45 @@ public class OrientDBEventDB implements IEventDB {
 	 */
 	@Override
 	public ResultIterator<FlatEvent> loadOnPeriod(final DateInterval _period) {
+		List<ODocument> result = Collections.emptyList();
 		if (_period.isCompleteInterval()) {
+			final String query = "SELECT * FROM " + eventType
+			        + " WHERE date BETWEEN ? AND ?";
+			result = orientSessionFactory.getOrCreateDB()
+			        .query(
+			                new OSQLSynchQuery<>(query),
+			                _period.getFrom(), _period.getTo());
 			
 		} else if (_period.hasFrom()) {
-			
+			final String query = "SELECT * FROM " + eventType
+			        + " WHERE date > ?";
+			result = orientSessionFactory.getOrCreateDB()
+			        .query(
+			                new OSQLSynchQuery<>(query),
+			                _period.getFrom());
 		} else if (_period.hasTo()) {
-			
+			final String query = "SELECT * FROM " + eventType
+			        + " WHERE date < ?";
+			result = orientSessionFactory.getOrCreateDB()
+			        .query(
+			                new OSQLSynchQuery<>(query),
+			                _period.getTo());
 		}
-		orientSessionFactory.getOrCreateDB().query(new OSQLSynchQuery<>(query),
-		        _period);
-		return null;
+		return new OResultIteratorImplementation(new OrientDBEventIterator(
+		        result.iterator()));
+		
 	}
-
+	
 	@Override
 	public void put(final FlatEvent _flatEvent) {
 		Validate.isTrue(eventType.equals(_flatEvent.getEventType()));
 		final ODocument newInstance = orientSessionFactory.getOrCreateDB()
-				.newInstance(eventType);
+		        .newInstance(eventType);
 		newInstance.fields((Map) _flatEvent.getProperties());
 		newInstance.save();
-
+		
 	}
-
+	
 	@Override
 	public void putAll(final Collection<FlatEvent> _values) {
 		orientSessionFactory.getOrCreateDB().begin();
@@ -161,17 +180,17 @@ public class OrientDBEventDB implements IEventDB {
 			put(event);
 		}
 		orientSessionFactory.getOrCreateDB().commit();
-
+		
 	}
-	
+
 	@Override
 	public void removeAll() {
 		if (!orientSessionFactory.getOrCreateDB().getMetadata().getSchema()
-				.existsClass(eventType)) { return; }
+		        .existsClass(eventType)) { return; }
 		for (final ODocument event : orientSessionFactory.getOrCreateDB()
-				.browseClass(eventType)) {
+		        .browseClass(eventType)) {
 			event.delete();
 		}
-
+		
 	}
 }
