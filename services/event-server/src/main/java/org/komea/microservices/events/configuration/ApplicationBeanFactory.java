a3f0777a4a7a10aa0c3796ed2d909a295aa577f4
Sylvain Leroy <sleroy0@gmail.com>
2015-01-16 12:47:52 +0100

    [sprint] removed problems

--- services/event-server/src/main/java/org/komea/microservices/events/configuration/ApplicationBeanFactory.java
+++ services/event-server/src/main/java/org/komea/microservices/events/configuration/ApplicationBeanFactory.java
@@ -1,25 +1,8 @@
 package org.komea.microservices.events.configuration;
 
-import org.komea.event.storage.IEventTypeSchemaUpdater;
-import org.komea.event.storage.impl.EventTypeSchemaUpdater;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orientdb.orm.session.IOrientSessionFactory;
 
 @Configuration
 public class ApplicationBeanFactory {
 
-	/**
-	 * Initializes a bean with Event Query manager.
-	 *
-	 * @param _factory
-	 * @return
-	 */
-	@ConditionalOnMissingBean(EventTypeSchemaUpdater.class)
-	@Bean
-	public IEventTypeSchemaUpdater eventTypeSchemaUpdater(final IOrientSessionFactory _factory) {
-		return new EventTypeSchemaUpdater(_factory);
-
-	}
 }
--- services/event-server/src/main/java/org/komea/microservices/events/rest/controllers/StorageController.java
+++ services/event-server/src/main/java/org/komea/microservices/events/rest/controllers/StorageController.java
@@ -8,7 +8,6 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.komea.event.model.beans.BasicEvent;
 import org.komea.event.model.beans.ComplexEvent;
 import org.komea.event.storage.IEventStorage;
-import org.komea.event.storage.IEventTypeSchemaUpdater;
 import org.komea.microservices.events.database.model.ValueEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,21 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class StorageController {
 
 	@Autowired
-	private IEventStorage	        eventStorageService;
-	@Autowired
-	private IEventTypeSchemaUpdater	eventSchemaUpdater;
+	private IEventStorage eventStorageService;
 
 	@Transactional
 	@RequestMapping(method = RequestMethod.GET, value = "/clear/{eventType}")
 	@ResponseStatus(value = HttpStatus.OK)
 	public void clearEvent(@PathVariable final String eventType) {
-		this.eventStorageService.clearEventsOfType(eventType);
+		eventStorageService.clearEventsOfType(eventType);
 
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/push/{provider}/{eventName}/{value}")
 	@ResponseStatus(value = HttpStatus.OK)
-	public void pushGetEvent(@PathVariable final String provider, @NotEmpty @PathVariable final String eventName,
+	public void pushGetEvent(@PathVariable final String provider,
+			@NotEmpty @PathVariable final String eventName,
 			@NotNull @PathVariable final Double value) {
 
 		final ValueEvent valueEvent = new ValueEvent();
@@ -48,24 +46,21 @@ public class StorageController {
 		valueEvent.setValue(value);
 		valueEvent.setProvider(provider);
 		valueEvent.setDate(new Date());
-		this.eventSchemaUpdater.updateSchemaWithEvent(valueEvent.getEventType());
-		this.eventStorageService.storeEvent(valueEvent);
+		eventStorageService.storeEvent(valueEvent);
 
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/push_complex")
 	@ResponseStatus(value = HttpStatus.OK)
 	public void pushPostComplexEvent(@RequestBody final ComplexEvent _event) {
-		this.eventSchemaUpdater.updateSchemaWithEvent(_event.getEventType());
-		this.eventStorageService.storeComplexEvent(_event);
+		eventStorageService.storeComplexEvent(_event);
 
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/push")
 	@ResponseStatus(value = HttpStatus.OK)
 	public void pushPostEvent(@RequestBody final BasicEvent _event) {
-		this.eventSchemaUpdater.updateSchemaWithEvent(_event.getEventType());
-		this.eventStorageService.storeBasicEvent(_event);
+		eventStorageService.storeBasicEvent(_event);
 
 	}
 }
