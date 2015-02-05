8b932a9ad392b857b26955e8763bc70e71f3c618
Sebasten Carreau <sebastien.carreau@tocea.com>
2015-02-05 15:09:11 +0100

    Kryo : fix serialization problems

--- core/komea-sql/src/main/java/org/komea/event/utils/kryo/impl/KryoByteArrayToObjectConverter.java
+++ core/komea-sql/src/main/java/org/komea/event/utils/kryo/impl/KryoByteArrayToObjectConverter.java
@@ -3,7 +3,6 @@ package org.komea.event.utils.kryo.impl;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.google.common.base.Function;
-import java.io.ByteArrayInputStream;
 
 /**
  * Created by Richard on 4/9/14.
@@ -14,7 +13,6 @@ public class KryoByteArrayToObjectConverter<T> implements Function<byte[], T> {
      * Kryo valueObjectConverter/valueSerializer
      */
     private final Kryo kryo;
-    private static final int DEFAULT_SIZE = 1024;
     /**
      * Type of class you are reading/writing.
      */
@@ -30,7 +28,7 @@ public class KryoByteArrayToObjectConverter<T> implements Function<byte[], T> {
         if (value == null || value.length == 0) {
             return null;
         }
-        Input input = new Input(new ByteArrayInputStream(value), DEFAULT_SIZE);
+        Input input = new Input(value);
         return this.kryo.readObject(input, this.type);
     }
 }
--- core/komea-sql/src/main/java/org/komea/event/utils/kryo/impl/KryoObjectToByteArrayConverter.java
+++ core/komea-sql/src/main/java/org/komea/event/utils/kryo/impl/KryoObjectToByteArrayConverter.java
@@ -3,7 +3,6 @@ package org.komea.event.utils.kryo.impl;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
 import com.google.common.base.Function;
-import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Richard on 4/9/14.
@@ -19,9 +18,8 @@ public class KryoObjectToByteArrayConverter<T> implements Function<T, byte[]> {
 
     @Override
     public byte[] apply(final T value) {
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        final Output streamOut = new Output(outputStream, DEFAULT_SIZE);
+        final Output streamOut = new Output(DEFAULT_SIZE);
         this.kryo.writeObject(streamOut, value);
-        return outputStream.toByteArray();
+        return streamOut.toBytes();
     }
 }
